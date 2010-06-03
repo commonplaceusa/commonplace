3b4bb3ce149be678b5289f2e2763c6a567166fdc
Max Tilford <maxtilford@gmail.com>
2010-06-03 15:16:24 -0500

    cucumber and rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,12 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+config.gem "factory_girl"
+config.gem "forgery"
+config.gem "cucumber"
+config.gem "cucumber-rails", :lib => false
+config.gem "pickle"
+config.gem "email_spec"
