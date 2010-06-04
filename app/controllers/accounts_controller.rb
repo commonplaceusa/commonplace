b390180e601f31e14ccbfbe595101ebc5c94a49c
Max Tilford <maxtilford@gmail.com>
2010-06-04 11:45:02 -0400

    fixed accounts update page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -22,7 +22,7 @@ class AccountsController < ApplicationController
 
   def update
     if current_user.update_attributes(params[:user])
-      redirect_to community_page
+      redirect_to root_url
     else
       render :edit
     end
