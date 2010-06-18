bab8d8116991faff040032129d3eb6f32740ae56
Max Tilford <maxtilford@gmail.com>
2010-06-18 11:16:56 -0400

    example use of error message on

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ActiveRecord::Base
 
   acts_as_taggable_on :interests
 
-  validates_presence_of :name
+  validates_presence_of :name, :message => "nice message"
 
   has_many :sponsorships
   has_many :events, :through => :sponsorships
