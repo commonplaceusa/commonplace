e4ae235c6eadabd0cb5f0f36b91f60cf47319b2a
Max Tilford <maxtilford@gmail.com>
2010-06-18 19:44:01 -0400

    fix event display on nil start/end_times and changed event.location to event.address

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,29 +12,32 @@ module ApplicationHelper
   end
   
   def human_date(it)
-    now = DateTime.now
-    diff = (it.to_i - now.to_i)/86400
+    if it
+      now = DateTime.now
+      diff = (it.to_i - now.to_i)/86400
+      
+      #if it.strftime("%I").include? "0"
+      #  hour = it.strftime("%I").slice[1]
     
-    #if it.strftime("%I").include? "0"
-    #  hour = it.strftime("%I").slice[1]
-    
-    #return hour
-    
-    if diff < -6
-      return it.strftime("%A, %B %d, %Y at %I:%M%p")
-    elsif diff >= -6 and diff < -1
-      return "Last " + it.strftime("%A") + " at " + it.strftime("%I:%M%p")
-    elsif diff >= -1 and diff < 0
-      return "Yesterday at " + it.strftime("%I:%M%p")
-    elsif diff == 0
-      return "Today at " + it.strftime("%I:%M%p")
-    elsif diff >= 1 and diff < 0
-      return "Tomorrow at " + it.strftime("%I:%M%p")
-    elsif diff > 1 and diff <= 6
-      return "This " + it.strftime("%A") + " at " + it.strftime("%I:%M%p")
-    else
-      return it.strftime("%A, %B %d, %Y at %I:%M%p")
+      #return hour
+      
+      if diff < -6
+        return it.strftime("%A, %B %d, %Y at %I:%M%p")
+      elsif diff >= -6 and diff < -1
+        return "Last " + it.strftime("%A") + " at " + it.strftime("%I:%M%p")
+      elsif diff >= -1 and diff < 0
+        return "Yesterday at " + it.strftime("%I:%M%p")
+      elsif diff == 0
+        return "Today at " + it.strftime("%I:%M%p")
+      elsif diff >= 1 and diff < 0
+        return "Tomorrow at " + it.strftime("%I:%M%p")
+      elsif diff > 1 and diff <= 6
+        return "This " + it.strftime("%A") + " at " + it.strftime("%I:%M%p")
+      else
+        return it.strftime("%A, %B %d, %Y at %I:%M%p")
+      end
     end
+    else
+    ""
   end
-  
 end
