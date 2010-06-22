12365939d4c53dceaf606922c5a616ffd7eff537
Max Tilford <maxtilford@gmail.com>
2010-06-22 10:53:35 -0400

    add post creation feature

---
+++ features/post.feature
@@ -0,0 +1,13 @@
+Feature: posting
+  In order to communicate with my community
+  As a user
+  I want to create a post
+
+  Scenario: Create a post
+    Given I am logged in
+    And I am on the home page
+    When I fill in "post_body" with "Anybody have a ladder?"
+    And I press "Create Post"
+    Then I should be on the home page
+
+
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,4 +1,25 @@
 
+def user
+  @user ||= Factory(:user)
+end
+
+
+def login
+  user
+  visit path_to("the homepage")
+  fill_in "Email", :with => user.email
+  fill_in "Password", :with => user.password
+  click_button "Log in"
+end
+
 When /^I submit the "([^\"]*)" form$/ do |id|
   submit_form(id)
+end
+
+Given /^I am a registered user$/ do
+  user
+end
+
+Given /^I am logged in$/ do
+  login
 end
