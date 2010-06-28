5cf7df0fcb9ae663a3afaf300d4eb63eb9be35ad
Max Tilford <maxtilford@gmail.com>
2010-06-28 11:10:30 -0400

    render correct autocomplete template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def index
     if params[:term]
       @users = User.all(:conditions => ['first_name LIKE ? OR last_name LIKE ?', "%#{params[:q]}%", "%#{params[:term]}%"], :limit => 5)
-      render 'autocomplete'
+      render 'autocomplete', :layout => false
     else
       @users = User.all
     end
