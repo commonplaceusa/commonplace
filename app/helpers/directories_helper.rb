48e050c83cb4213d5482398585ec6a5e21f8e878
Max Tilford <maxtilford@gmail.com>
2010-07-06 16:29:59 -0400

    remove unused entry partial and helper

--- app/helpers/directories_helper.rb
+++ app/helpers/directories_helper.rb
@@ -1,9 +1,2 @@
 module DirectoriesHelper
-
-  def entry_options_for(thing)
-    options = {"data-info" => render(thing.class.name.pluralize.downcase + "/" + "info_box", :organization => thing)};
-    options["data-marker"] = "{\"info_html\": \"#{thing.name}\", \"lat\": \"#{thing.lat}\", \"lng\": \"#{thing.lng}\"}" if thing.lat and thing.lng
-    options
-  end
-
 end
