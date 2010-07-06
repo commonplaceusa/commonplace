61119fcc2733f0c68ba736f105a0bc7c0e237094
Max Tilford <maxtilford@gmail.com>
2010-07-06 16:22:14 -0400

    full accounts controller specs

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -14,16 +14,28 @@ describe AccountsController do
     assigns[:account].should_not be_nil
   end
 
-  context 'logged in' do
-    before :each do 
-      activate_authlogic
-      UserSession.new(Factory(:user))
-    end
-    
-    it "should render the edit template" do
-      get :edit
-      response.should render_template('edit')
-    end
+  it "should render more_info on a successful create" do
+    UserSession.stub!(:find).and_return(user_session({},{}))
+    Account.stub!(:new).and_return(mock_model(Account, :save => true))
+    post :create
+    response.should render_template('more_info')
+  end
 
+  it "should render the edit template" do
+    login
+    get :edit
+    response.should render_template('edit')
+  end
+  
+  it "should re-render edit on a failed update" do
+    login({},{:update_attributes, false})
+    put :update
+    response.should render_template('edit')
+  end
+
+  it "should redirect to root on a successful update" do
+    login({},{:update_attributes, true})
+    put :update
+    response.should redirect_to('/')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,22 @@ Spork.prefork do
     config.use_transactional_fixtures = true
   end
 
+  def current_user(stubs = {})
+  @current_user ||= mock_model(User, stubs)
+  end
+  
+  def user_session(stubs = {}, user_stubs = {})
+    @current_user ||= mock_model(UserSession, {:user => current_user(user_stubs)}.merge(stubs))
+  end
+  
+  def login(session_stubs = {}, user_stubs = {})
+    UserSession.stub!(:find).and_return(user_session(session_stubs, user_stubs))
+  end
+  
+  def logout
+    @user_session = nil
+  end
+  
 end
 
 Spork.each_run do
