fdfbafc475f735267749a93738f93b5b950e212b
Max Tilford <maxtilford@gmail.com>
2010-07-07 08:44:44 -0400

    remove dead code in event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,31 +2,16 @@ class Event < ActiveRecord::Base
   
   validates_presence_of :name
 
-  has_many :attendances
-  has_many :users, :through => :attendances
-  
-  has_many :sponsorships
-
-  has_many :businesses, :through => :sponsorships, :source => :business,
-                        :conditions => "sponsorships.sponsor_type = 'Business'"
-
-  has_many :organization, :through => :sponsorships, :source => :organization,
-                        :conditions => "sponsorships.sponsor_type = 'Organization'"
-
   has_many :referrals
 
   before_save :update_lat_and_lng, :if => "address_changed?"
 
   named_scope :upcoming, :conditions => ["? < start_time", Time.now]
   named_scope :past, :conditions => ["start_time < ?", Time.now]
-  def sponsors
-    self.businesses + self.organizations
-  end
 
   def search(term)
     Event.all
   end
-
 
   def update_lat_and_lng
     if address.blank?
