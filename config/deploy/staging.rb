e15fd4b8176559602a2bffa6db45ca6de23a9edf
Max Tilford <maxtilford@gmail.com>
2010-07-09 11:05:12 -0400

    added capistrano, and set up staging server

---
+++ config/deploy/staging.rb
@@ -0,0 +1,9 @@
+
+set :application, "staging.commonplace.in"
+set :deploy_to, "/home/staging/commonplace"
+set :user, "staging"`
+
+role :app, "staging.commonplace.in"
+role :web, "staging.commonplace.in"
+role :db, "staging.commonplace.in"
+
