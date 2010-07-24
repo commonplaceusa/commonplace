2954fadb63dbbb9f816cacd4b55929b682c908d8
Max Tilford <maxtilford@gmail.com>
2010-07-24 17:43:31 -0400

    add organizer/profile controller and show view

---
+++ spec/controllers/organizer/profiles_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Organizer::ProfilesController do
+
+  #Delete this example and add some real ones
+  it "should use Organizer::ProfilesController" do
+    controller.should be_an_instance_of(Organizer::ProfilesController)
+  end
+
+end
