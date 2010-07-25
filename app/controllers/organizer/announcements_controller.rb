40b1fe0a0ae27a7ea2e57643c0b7ce90270dd0b3
Max Tilford <maxtilford@gmail.com>
2010-07-24 21:43:25 -0400

    submitting announcement and event forms with correct values now works

--- app/controllers/organizer/announcements_controller.rb
+++ app/controllers/organizer/announcements_controller.rb
@@ -5,9 +5,17 @@ class Organizer::AnnouncementsController < ApplicationController
 
   def index 
     @announcements = @organization.announcements
-    @announcement = @organization.announcements.build
+    @announcement = Announcement.new
   end
 
+  def create
+    @announcement = @organization.announcements.build(params[:announcement])
+    if @announcement.save
+      redirect_to organizer_announcements_url(@organization)
+    else
+      render :new
+    end
+  end
   protected
   def load_organization
     @organization = Organization.find(params[:organizer_id])
