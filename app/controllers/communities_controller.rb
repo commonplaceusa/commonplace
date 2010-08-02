4e50014362298a0a44f4dc33b4c14b575bec6bdc
Max Tilford <maxtilford@gmail.com>
2010-08-02 08:18:31 -0400

    we show the community show page as the front page, with a public version of the wire on the left and registration on the right

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,19 +1,11 @@
 class CommunitiesController < ApplicationController
   
   layout 'community'
-2
 
   def show
-    load_community
     @post = Post.new
   end
 
-  protected
-  
-  def load_community
-    unless @community = Community.find_by_name(params[:id])
-      render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
-    end
-  end
+
 
 end
