a4d12e229d92d7ac67db60df84cc5af2096b7c9e
Max Tilford <maxtilford@gmail.com>
2010-08-05 10:13:19 -0400

    add nested_layouts gem create communities layout, and render it inside of application layout all pages having the filters now use the communities layout

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+
+  config.gem 'nested_layouts'
   
   config.gem 'aasm'
   
