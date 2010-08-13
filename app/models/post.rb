606de0fa894c3c8eb073ed4409e708d48ad9fa0a
Max Tilford <maxtilford@gmail.com>
2010-08-13 12:20:32 -0400

    get rid of 'Neighborhood' qualifier before the model.class.human_name in wire item headers, except for neighborhood posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,9 @@ class Post < ActiveRecord::Base
   validates_presence_of :user
   validates_presence_of :body, :message => "Please enter some text for your post"
 
+  def self.human_name
+    "Neighborhood Post"
+  end
   
   def time
     help.post_date self.created_at
