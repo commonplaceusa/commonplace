e454a8e1b23399e973c0fcea2080828f7917615f
Max Tilford <maxtilford@gmail.com>
2010-08-20 23:20:13 -0400

    add tagged_with_aliases instance method

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,18 @@
 class OrganizationsController < CommunitiesController
   
   def index
-    @subscribed_organizations = current_user.organizations
-    @suggested_organizations = []
-    @community_organizations = current_community.organizations.all(:conditions => ["id NOT IN (?)", @subscribed_organizations + @suggested_organizations + [0]])
     respond_to do |format|
-      format.json
-      format.html
+      if params[:q]
+        @results = current_community.organizations.tagged_with_aliases(params[:q], :any => true)
+        format.json { render :search }
+        format.html { render :search }
+      else
+        @subscribed_organizations = current_user.organizations
+        @suggested_organizations = []
+        @community_organizations = current_community.organizations.all(:conditions => ["id NOT IN (?)", @subscribed_organizations + @suggested_organizations + [0]])
+        format.json
+        format.html
+      end
     end
   end
 
