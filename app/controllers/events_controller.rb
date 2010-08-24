97a9b99f318e6cf257c5bb4cecfccaedb653e7a6
Max Tilford <maxtilford@gmail.com>
2010-08-24 19:16:07 -0400

    remove suggested organizations and events until we test on postgres

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < CommunitiesController
    
   def index
     @subscribed_events = current_user.organizations.map(&:events).flatten
-    @suggested_events = current_user.related_of_class(Event).all(:conditions => ["events.id NOT IN (?)", @subscribed_events])
+    @suggested_events = []
     @community_events = current_community.events.all(:conditions => ["events.id NOT IN (?)", @subscribed_events + @suggested_events])
     respond_to do |format|
       format.json
