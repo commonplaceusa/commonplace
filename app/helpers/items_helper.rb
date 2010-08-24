02c919760e42b93dec8e14356b7414ca62bb9e56
Max Tilford <maxtilford@gmail.com>
2010-08-24 15:44:18 -0400

    Re-refactor wire item views
    The _item partial is nice in theory, but when we need to show
    organizations and users in line with events, posts,
    and announcements, it all falls apart. Each model now relies on
    it's _model partial again. There is some managable html duplication,
    some of which can still be abstracted.

---
+++ app/helpers/items_helper.rb
@@ -0,0 +1,9 @@
+module ItemsHelper
+  
+  def item_tag(item, html_options = {}, &block)
+    html_options[:class] = [html_options[:class], 'styled_box', dom_class(item)].join(" ")
+    html_options[:id] = dom_id(item)
+    content_tag(:li, html_options, &block)
+  end
+
+end
