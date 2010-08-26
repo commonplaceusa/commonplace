5cac69eb44e448168e22462b996cbc86535364ed
Jonathan Levine <jonathan@jonathan-levines-macbook-pro.local>
2010-08-26 02:17:39 -0400

    wire style stuff

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
   
   def item_tag(item, html_options = {}, &block)
-    html_options[:class] = [html_options[:class], 'styled_box', dom_class(item)].join(" ")
+    html_options[:class] = [html_options[:class], dom_class(item)].join(" ")
     html_options[:id] = dom_id(item)
     content_tag(:li, html_options, &block)
   end
