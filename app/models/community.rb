1509812997f21c51598b71fb93298201f832a004
Max Tilford <maxtilford@gmail.com>
2010-09-12 11:40:30 -0400

    fix .users method for communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,9 +1,14 @@
 class Community < ActiveRecord::Base
-  has_many :users
   has_many :organizations
+  has_many :neighborhoods
   has_many :events, :through => :organizations
   has_many :announcements, :through => :organizations
+
   def self.find_by_name(name)
     find(:first, :conditions => ["LOWER(name) = ?", name.downcase])
+  end
+
+  def users
+    neighborhoods.map(&:users).flatten
   end
 end
