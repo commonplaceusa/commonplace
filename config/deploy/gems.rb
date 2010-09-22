7fe4f262cdef5c0e82f4c9daa840605304b2972a
Max Tilford <maxtilford@gmail.com>
2010-09-22 13:57:49 -0400

    don't try to install gems

--- config/deploy/gems.rb
+++ config/deploy/gems.rb
@@ -1,6 +1,7 @@
 namespace :gems do
   task :install, :roles => :app do
-    run "cd #{current_path} && bundle install vendor --without test development"
+    # For when we use bundler
+    #run "cd #{current_path} && bundle install vendor --without test development"
   end
 end
 after "deploy:update_code", "gems:install"
