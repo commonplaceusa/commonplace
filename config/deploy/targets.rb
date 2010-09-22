2e837a29adf474ea874b9d3c9114bb039d1dcc93
Max Tilford <maxtilford@gmail.com>
2010-09-21 22:47:25 -0400

    set deploy targets

--- config/deploy/targets.rb
+++ config/deploy/targets.rb
@@ -1,18 +1,18 @@
 task :production do
-  role :web, "69.164.215.169"
-  role :app, "69.164.215.169"
-  role :db,  "69.164.215.169", :primary => true
-  set :web_server, :nginx
-  set :nginx_conf_dir, '/etc/nginx/conf'
-  set :domain, "commonplace.co cambridge.commonplace.co"
-  set :web_port, "80"
-end
-
-task :staging do
   role :web, "69.164.212.22"
   role :app, "69.164.212.22"
   role :db,  "69.164.212.22", :primary => true
   set :web_server, :nginx
-  set :domain, "staging.commonplace.in cambridge.staging.commonplace.in"
+  set :nginx_conf_dir, '/etc/nginx/conf'
+  set :domain, "westroxbury.commonplace.co"
+  set :web_port, "80"
+end
+
+task :staging do
+  role :web, "69.164.215.169"
+  role :app, "69.164.215.169"
+  role :db,  "69.164.215.169", :primary => true
+  set :web_server, :nginx
+  set :domain, "westroxbury.staging.commonplaceusa.com"
   set :web_port, "80"
 end
