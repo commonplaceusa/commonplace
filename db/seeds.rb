a9a22cb6e84e6fa81e5e6f13252594867c2b0710
Max Tilford <maxtilford@gmail.com>
2010-10-02 08:20:34 -0400

    Update db/seeds.rb and spec/factories.rb to reflect current schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,31 +9,31 @@
 require 'factory_girl'
 require 'forgery'
 
+puts %[-- creating community]
+communities = [Factory(:community, :name => "West Roxbury", :slug => "westroxbury")]
 
+puts %[-- creating neighborhoods]
+neighborhoods = Array.new(5) { Factory(:neighborhood, :community => communities[0])}
 
-communities = [Factory(:community), Factory(:community)]
-
-user = Factory(:user, :community => communities[0],
+puts %[-- creating user (:email => "test@example.com", :password => "toast")]
+user = Factory(:user, :neighborhood => neighborhoods[0],
                :email => "test@example.com",
                :password => "toast")
 
-users = [Factory(:user, :community => communities[0]),
-         Factory(:user, :community => communities[0]),
-         Factory(:user, :community => communities[0]),
-         Factory(:user, :community => communities[0])]
-         
+puts %[-- creating users]
+users = Array.new(5) {|i|Array.new(4) {Factory(:user, :neighborhood => neighborhoods[i])}}.flatten
 
+puts %[-- creating posts]
 posts = [Factory(:post, :user => users[0]),
          Factory(:post, :user => users[0]),
          Factory(:post, :user => users[1]),
          Factory(:post, :user => users[2]),
          Factory(:post, :user => users[3])]
 
-organizations = [Factory(:organization, :community => communities[0]),
-                 Factory(:organization, :community => communities[0]),
-                 Factory(:organization, :community => communities[0]),
-                 Factory(:organization, :community => communities[0])]
+puts %[-- creating organizations]
+organizations = Array.new(4){Factory(:organization, :community => communities[0])}
 
+puts %[-- creating events]
 events = [Factory(:event, :organization => organizations[0]),
           Factory(:event, :organization => organizations[0]),
           Factory(:event, :organization => organizations[1]),
@@ -43,6 +43,7 @@ events = [Factory(:event, :organization => organizations[0]),
           Factory(:event, :organization => organizations[3]),
          ]
 
+puts %[-- creating announcements]
 announcements = [Factory(:announcement, :organization => organizations[0]),
                  Factory(:announcement, :organization => organizations[0]),
                  Factory(:announcement, :organization => organizations[0]),
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,19 @@ Factory.define :community do |f|
   f.name { Forgery(:address).city }
 end
 
+Factory.define :neighborhood do |f|
+  f.name { Forgery(:address).street_name }
+  f.north_bound { rand(180) - 90 }
+  f.south_bound {|n| n.north_bound - rand(5)}
+  f.east_bound { rand(360) - 180 }
+  f.west_bound {|n| n.east_bound - rand(5)}
+  f.about Forgery(:lorem_ipsum).paragraphs(1)
+  f.association :community 
+end
+
+Factory.define :avatar do |f|
+end
+
 Factory.define :user do |f|
   f.first_name { Forgery(:name).first_name }
   f.last_name { Forgery(:name).last_name }
@@ -12,9 +25,9 @@ Factory.define :user do |f|
   f.password_confirmation {|u| u.password }
   f.address { "#{Forgery(:address).street_address}, #{Forgery(:address).city}, #{Forgery(:address).state}" }
   f.about { Forgery(:lorem_ipsum).paragraphs(1) }
-  f.association :community
+  f.association :avatar
+  f.association :neighborhood
 end
-
 
 Factory.define :post do |f|
   f.body { Forgery(:lorem_ipsum).paragraphs(1) }
@@ -24,7 +37,8 @@ end
 Factory.define :event do |f|
   f.name { Forgery(:lorem_ipsum).words(2) }
   f.description { Forgery(:lorem_ipsum).paragraph }
-  f.start_time { Time.now + 1.week }
+  f.date { Time.now + rand(6).week }
+  f.start_time { Time.parse("#{rand(24)}:#{rand(60)}") }
   f.association :organization
   f.address { "#{Forgery(:address).street_address}, #{Forgery(:address).city}, #{Forgery(:address).state}" }
 end
@@ -39,6 +53,8 @@ Factory.define :organization do |f|
   f.name { Forgery(:name).company_name }
   f.website { "http://" + Forgery(:internet).domain_name }
   f.address { "#{Forgery(:address).street_address}, #{Forgery(:address).city}, #{Forgery(:address).state}" }
+  f.association :avatar
+  f.after_build {|o| o.profile_fields = [] }
 end
   
 Factory.define :message do |f|
