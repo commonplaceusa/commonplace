cd683b031e5a3f778d54b804dc734c854ea01ae6
Max Tilford <maxtilford@gmail.com>
2010-10-03 15:11:48 -0400

    order announcements latest first

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,7 +1,7 @@
 class AnnouncementsController < CommunitiesController
   load_and_authorize_resource
   def index
-    @announcements = current_community.announcements
+    @announcements = current_community.announcements.all(:order => "created_at DESC")
     respond_to do |format|
       format.json
       format.html
