dc6db1856ac87689af0d2d462cfc9233539a9725
Max Tilford <maxtilford@gmail.com>
2010-10-14 19:23:31 -0400

    resurrect the wire

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,7 +1,12 @@
 class CommunitiesController < ApplicationController
   before_filter :current_community
   before_filter :authorize_current_community
+  
+  layout 'zone'
+  
   def show
+    @items = current_user.wire
+    render :layout => params[:partial] ? 'zone' : 'communities'
   end
 
 end
