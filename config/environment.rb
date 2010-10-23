94713723436be2d00f1b78017b014366c2dfaf19
Max Tilford <maxtilford@gmail.com>
2010-10-23 04:26:48 -0400

    set actionmailer default content type to text/html

--- config/environment.rb
+++ config/environment.rb
@@ -55,6 +55,8 @@ Rails::Initializer.run do |config|
   config.gem "resque"
 
   config.gem "SystemTimer", :lib => "system_timer"
+
+  config.action_mailer.default_content_type = "text/html"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -62,7 +64,7 @@ Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
+ 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
