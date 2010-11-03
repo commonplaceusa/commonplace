d2a7ce50af8277e1c3e14010308fd8a056e3427a
Max Tilford <maxtilford@gmail.com>
2010-11-03 16:23:31 -0400

    fix url in password reset email

--- app/models/passwords_mailer.rb
+++ app/models/passwords_mailer.rb
@@ -6,7 +6,7 @@ class PasswordsMailer < ActionMailer::Base
     from "passwords@commonplaceusa.com"
     subject "CommonPlace password reset"
     body <<END
-<p>Follow this link to reset your password: #{edit_password_reset_url(user.perishable_token, :host => "ourcommonplace.com") }</p>
+<p>Follow this link to reset your password: #{edit_password_reset_url(user.perishable_token, :host => "#{user.community.slug}.ourcommonplace.com") }</p>
 <br>
 --
 #{ user.community.name }'s CommonPlace: #{user.community.slug + ".ourcommonplace.com"}
