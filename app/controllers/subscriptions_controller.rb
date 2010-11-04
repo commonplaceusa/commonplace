3edbcafc2619f794f317bf2123c3d957174f3a34
Max Tilford <maxtilford@gmail.com>
2010-11-04 17:39:46 -0400

    fix organization subscripiton tooltips

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -14,7 +14,7 @@ class SubscriptionsController < ApplicationController
   def create
     @organization = Organization.find params[:organization_id]
     current_user.organizations << @organization
-    flash[:win] = "You've subscribed to #{ @organization.name }."
+    flash[:message] = "You've subscribed to #{ @organization.name }."
     redirect_to @organization
   end
   
@@ -22,7 +22,7 @@ class SubscriptionsController < ApplicationController
     @organization = Organization.find params[:organization_id]
     current_user.organizations.delete @organization
     current_user.save
-    flash[:win] = "You've unsubscribed from #{ @organization.name }."
+    flash[:message] = "You've unsubscribed from #{ @organization.name }."
     redirect_to @organization
   end
 end
