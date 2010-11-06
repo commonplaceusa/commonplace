ddebb9988581a610ec755073d0a8b1d62f809ef2
Max Tilford <maxtilford@gmail.com>
2010-11-06 17:06:58 -0400

    use community layout in first post

--- app/controllers/first_posts_controller.rb
+++ app/controllers/first_posts_controller.rb
@@ -2,7 +2,6 @@ class FirstPostsController < ApplicationController
 
   def new
     @post = Post.new
-    render :layout => false
   end
 
   def create
@@ -12,7 +11,7 @@ class FirstPostsController < ApplicationController
       flash[:message] = "Post Created!"
       redirect_to posts_url
     else
-      render :new, :layout => false
+      render :new
     end
   end
 
