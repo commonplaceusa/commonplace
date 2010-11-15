12af7c8164694f2584f485b13e79193b825f25de
Max Tilford <maxtilford@gmail.com>
2010-11-14 19:34:32 -0500

    sort users by last_name

--- app/controllers/neighborhood/people_controller.rb
+++ app/controllers/neighborhood/people_controller.rb
@@ -2,7 +2,7 @@ class Neighborhood::PeopleController < CommunitiesController
    
   def index
     authorize! :read, User
-    @items = current_user.neighborhood.users
+    @items = current_user.neighborhood.users.all(:order => "last_name ASC")
     @render_args = Proc.new { |u| ['users/user', {:user => u}] }
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < CommunitiesController
   load_and_authorize_resource
 
   def index
-    @items = current_community.users
+    @items = current_community.users.sort_by(&:last_name)
   end
 
   def show
