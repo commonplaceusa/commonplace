02352c222e4344b20234128cabfbfabe7ed78d1c
Max Tilford <maxtilford@gmail.com>
2010-11-24 17:42:53 -0500

    only reposition #modal-content if it exists

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -85,19 +85,21 @@ $(function() {
   $('#org_url').textTruncate(140);
 
   $(window).bind('resize.modal', function () {
-    var $m = $("#modal-content"),
-    w = $m.width(),
-    h = $m.height(),
-    $b = $(window),
-    bw = $b.width(),
-    bh = $b.height();
-    
-    $m.css({top: (bh - h) / 2,
-            left: (bw - w) / 2
-           });
+    var $m = $("#modal-content")
+    if ($m.get(0)) {
+      var w = $m.width(),
+      h = $m.height(),
+      $b = $(window),
+      bw = $b.width(),
+      bh = $b.height();
+      
+      $m.css({top: (bh - h) / 2,
+              left: (bw - w) / 2
+             });
+    }
   });
   $(window).trigger('resize.modal');
-
+  
   renderMaps();
 });
 
