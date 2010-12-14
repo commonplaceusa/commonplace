28f30725f2675c744e20b736f4b4df5c5c3764f6
Max Tilford <maxtilford@gmail.com>
2010-12-14 15:58:49 -0500

    fix management feed event creation

--- app/controllers/management/feeds/events_controller.rb
+++ app/controllers/management/feeds/events_controller.rb
@@ -12,7 +12,8 @@ class Management::Feeds::EventsController < ManagementController
     if @event.save
       redirect_to management_event_url(@event)
     else
-      @events = @feeds.events
+      @past = @feed.events.past(:order => "date DESC")
+      @upcoming = @feed.events.upcoming(:order => "date DESC")
       render :index
     end
   end
