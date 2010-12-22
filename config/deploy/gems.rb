5a44a2c15f462f6fe6b9eb88fd704e52dc8505c6
Jason Berlinsky <jason@jasonberlinsky.com>
2010-12-22 17:17:54 -0500

    Re-added Bundler support to Capistrano

--- config/deploy/gems.rb
+++ config/deploy/gems.rb
@@ -1,7 +1,7 @@
 namespace :gems do
   task :install, :roles => :app do
     # For when we use bundler
-    #run "cd #{current_path} && bundle install vendor --without test development"
+    run "cd #{current_path} && bundle install vendor --without test development"
   end
 end
 after "deploy:update_code", "gems:install"
