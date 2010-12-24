115f9243c246b3e461cef29a340acff07041c82b
Max Tilford <maxtilford@gmail.com>
2010-12-24 11:15:19 -0500

    clean up commented out code

--- app/controllers/email_parse_controller.rb
+++ app/controllers/email_parse_controller.rb
@@ -9,30 +9,5 @@ class EmailParseController < ApplicationController
     end
     render :nothing => true
   end
-      
-      
-  #   reply = Reply.new(
-  #   # Pull the data from the e-mail
-  #   text = params[:text]
-  #   from = params[:from]
-  #   to = params[:to]
-  #   # Assume that the user is sending an e-mail to post-ID@commonplaceusa.com
-  #   post_id = to.gsub(/[post\-,\@commonplaceusa\.com]/,'').to_i
-    
-  #   # Ensure that the user exists
-  #   user = User.find_by_email(from)
-  #   if (!user)
-  #     # Ideally, this will send an e-mail back, complaining
-  #     return
-  #   end
-    
-  #   reply = Reply.new(:body => text, :repliable_id => post_id, :user_id => user.id)
-  #   reply.repliable = Post.find(post_id)
-  #   if !reply.save!
-  #     puts "ERROR SAVING!"
-  #   end
-  #   puts reply.inspect
-    
-  #   render :layout => false
-  # end
+
 end
