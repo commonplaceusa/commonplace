8a9eca287ec057d665af335c19cb02652289161b
Max Tilford <maxtilford@gmail.com>
2010-12-24 12:20:24 -0500

    full log in staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,3 +7,4 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
+ActiveRecord::Base.logger.level = Logger::DEBUG
