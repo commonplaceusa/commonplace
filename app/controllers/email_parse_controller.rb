728b80a8f5da06f0f10508bc7205b1ba4cf4cdb6
Jason Berlinsky <jason@jasonberlinsky.com>
2010-12-25 01:23:28 -0500

    Added filtering of reply text in e-mails

--- app/controllers/email_parse_controller.rb
+++ app/controllers/email_parse_controller.rb
@@ -3,8 +3,33 @@ class EmailParseController < ApplicationController
     user = User.find_by_email(TMail::Address.parse(params[:from]).spec)
     post = Post.find(TMail::Address.parse(params[:to]).spec.match(/post-(\d+)/)[1].to_i)
     if user && post
-      RAILS_DEFAULT_LOGGER.error("\n Original data: #{params[:text].split('=20').inspect} \n"}
-      Reply.create(:body => params[:text].split("=20")[0],
+      RAILS_DEFAULT_LOGGER.error("\n Original data: #{params[:text].inspect} \n"}
+      # Strip any replies from the text
+      
+      text = params[:text]
+      
+      # Check for key phrases
+      phrases = ['-- \n','--\n','-----Original Message-----','________________________________','From: ','Sent from my ',TMail::Address.parse(params[:to]).spec]
+      
+      index = text.length + 1
+      
+      phrases.each { |phrase| 
+        newIndex = text.index(phrase)
+        if newIndex && newIndex < index
+          index = newIndex
+        end
+      }
+      
+      # Erase everything before the key phrase
+      text = text[0,index]
+      
+      # Find the last \n character in the remaining text and erase everything after it
+      
+      text = text[0,text.rindex("\n")]
+      
+      RAILS_DEFAULT_LOGGER.error("\n Result: #{text.inspect} \n"}
+      
+      Reply.create(:body => text,
                    :repliable => post,
                    :user => user)
     end
