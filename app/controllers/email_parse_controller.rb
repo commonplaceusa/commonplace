a0fc7f9cdd1994d069a8f10f94e5943c811db8f9
Jason Berlinsky <jason@jasonberlinsky.com>
2010-12-24 22:47:37 -0500

    Distinguish between an e-mailed response and the original message

--- app/controllers/email_parse_controller.rb
+++ app/controllers/email_parse_controller.rb
@@ -3,7 +3,8 @@ class EmailParseController < ApplicationController
     user = User.find_by_email(TMail::Address.parse(params[:from]).spec)
     post = Post.find(TMail::Address.parse(params[:to]).spec.match(/post-(\d+)/)[1].to_i)
     if user && post
-      Reply.create(:body => params[:text],
+      RAILS_DEFAULT_LOGGER.error("\n #{params[:text].split('=20').inspect \n"}
+      Reply.create(:body => params[:text].split("=20")[0],
                    :repliable => post,
                    :user => user)
     end
