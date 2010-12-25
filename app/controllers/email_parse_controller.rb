f57d503ee8dc1892035fe0e5cfac36956e03a3a4
Jason Berlinsky <jason@jasonberlinsky.com>
2010-12-24 22:49:13 -0500

    Typo in e-mail controller.

--- app/controllers/email_parse_controller.rb
+++ app/controllers/email_parse_controller.rb
@@ -3,7 +3,7 @@ class EmailParseController < ApplicationController
     user = User.find_by_email(TMail::Address.parse(params[:from]).spec)
     post = Post.find(TMail::Address.parse(params[:to]).spec.match(/post-(\d+)/)[1].to_i)
     if user && post
-      RAILS_DEFAULT_LOGGER.error("\n #{params[:text].split('=20').inspect \n"}
+      RAILS_DEFAULT_LOGGER.error("\n Original data: #{params[:text].split('=20').inspect} \n"}
       Reply.create(:body => params[:text].split("=20")[0],
                    :repliable => post,
                    :user => user)
