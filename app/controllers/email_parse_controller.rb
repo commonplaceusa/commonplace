ad89493130b2aada799588845f6a65dab06f8031
Jason Berlinsky <jason@jasonberlinsky.com>
2010-12-25 15:49:37 -0500

    Refactored EmailParseController.strip() and added test to reply-stripping.

--- app/controllers/email_parse_controller.rb
+++ app/controllers/email_parse_controller.rb
@@ -1,31 +1,38 @@
 class EmailParseController < ApplicationController
+  
+  def EmailParseController.strip(text,to)
+    # Strip any replies from the text
+    
+    # Check for key phrases
+    phrases = ['-- \n','--\n','-----Original Message-----','________________________________','From: ','Sent from my ',TMail::Address.parse(to).spec,"#{TMail::Address.parse(to).spec.match(/post-(\d+)/)[1]}-replies"]
+    
+    index = text.length + 1
+    
+    phrases.each { |phrase| 
+      newIndex = text.index(phrase)
+      if newIndex && newIndex < index
+        index = newIndex
+      end
+    }
+    
+    # Erase everything before the key phrase
+    text = text[0,index]
+    
+    # Find the last \n character in the remaining text and erase everything after it
+    
+    index = text.rindex("\n")
+    if index
+      text[0,text.rindex("\n")]
+    else
+      text
+    end
+  end
+  
   def parse
     user = User.find_by_email(TMail::Address.parse(params[:from]).spec)
     post = Post.find(TMail::Address.parse(params[:to]).spec.match(/post-(\d+)/)[1].to_i)
     if user && post
-      #RAILS_DEFAULT_LOGGER.error("\n Original data: #{params[:text].inspect} \n")
-      # Strip any replies from the text
-      
-      text = params[:text]
-      
-      # Check for key phrases
-      phrases = ['-- \n','--\n','-----Original Message-----','________________________________','From: ','Sent from my ',TMail::Address.parse(params[:to]).spec,"#{TMail::Address.parse(params[:to]).spec.match(/post-(\d+)/)[1]}-replies"]
-      
-      index = text.length + 1
-      
-      phrases.each { |phrase| 
-        newIndex = text.index(phrase)
-        if newIndex && newIndex < index
-          index = newIndex
-        end
-      }
-      
-      # Erase everything before the key phrase
-      text = text[0,index]
-      
-      # Find the last \n character in the remaining text and erase everything after it
-      
-      text = text[0,text.rindex("\n")]
+      text = EmailParseController.strip(params[:text],params[:to])
       
       Reply.create(:body => text,
                    :repliable => post,
