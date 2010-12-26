25ec6abdcf16c6505dfc9992014705ace1da17b7
Max Tilford <maxtilford@gmail.com>
2010-12-26 15:59:07 -0500

    get rid of puts logging in spec

--- spec/controllers/email_parse_controller_spec.rb
+++ spec/controllers/email_parse_controller_spec.rb
@@ -5,7 +5,6 @@ describe EmailParseController do
   describe "POST parse" do
     before :each do 
       @post = mock()
-      puts @post.inspect
       @user = mock()
       @text = "Lorem ipsum..."
       stub(Post).find(20) { @post }
@@ -14,14 +13,6 @@ describe EmailParseController do
       stub(Reply).create
     end
 
-    #it "should be able to find the user"
-    #  User.find_by_email("test@example.com").email.should match "test@example.com"
-    #end
-    
-#    it "should return the same text, since nothing is in the reply section" do
-#      EmailParseController.strip(@text,"post-20-reply@commonplaceusa.com").should == @text
-#    end
-#
     it "should create a reply to a post when the email is to post-id@.*" do
       get :parse, :text => @text, :from => "test@example.com", :to => "MjA1@replies.commonplaceusa.com"
       Reply.should have_received.create(hash_including(:body => @text,
