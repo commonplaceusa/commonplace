d554a562398469f293d3a434abaf682602e4fa2b
Max Tilford <maxtilford@gmail.com>
2010-12-31 15:08:43 -0500

    add missing post.save in 'add_neighborhood_id_to_posts' migration

--- db/migrate/20101229214718_add_neighborhood_id_to_posts.rb
+++ db/migrate/20101229214718_add_neighborhood_id_to_posts.rb
@@ -3,6 +3,7 @@ class AddNeighborhoodIdToPosts < ActiveRecord::Migration
     add_column :posts, :neighborhood_id, :integer
     Post.all.each do |post|
       post.neighborhood_id = post.user.neighborhood_id
+      post.save
     end
   end
 
