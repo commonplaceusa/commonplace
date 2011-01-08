af131a3da533e2b64a402c6c6716221fbb8be540
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-08 12:20:36 -0500

    Fixed EventfulImporter to convert to Markdown instead of HTML, and refactored code slightly.

--- lib/EventfulImporter.rb
+++ lib/EventfulImporter.rb
@@ -3,47 +3,51 @@ class EventfulImporter
   require 'eventful/api'
 
   def self.import_event(event,community)
+    # The only properly formed input is a Hash
     if event.is_a?(Array)
       return
     end
+    
     begin
       e = Event.new()
       if !event || event.nil?
         return
       end
 
+      # Ensure that all necessary fields are present
       required_keys = ['title', 'description', 'start_time', 'postal_code', 'venue_address', 'id']
       required_keys.each do |key|
         if !event[key].present?
           return
         end
       end
+      
+      # If this event has already been imported, skip it
       if Event.find_by_source_feed_id(event['id'])
         return
       end
 
-      location = Location.new(:zip_code => event['postal_code'], :street_address => event['venue_address'])
-
       e.name = event['title']
 
+      # Convert from HTML to Markdown
       mccbean = McBean.fragment event['description']
-      e.description = mccbean.to_html
+      e.description = mccbean.to_markdown
 
       e.date = event['start_time'].split(" ")[0]
       e.start_time = event['start_time']
-      e.end_time = event['stop_time']
+      
+      if event['stop_time'].present?
+        e.end_time = event['stop_time']
+      end
 
-      e.location = location
+      e.location = Location.new(:zip_code => event['postal_code'], :street_address => event['venue_address'])
 
-      # Instead of using one Feed for all Eventful events for a given community, make each result its own Feed
       e.owner_id = Feed.find_or_create_by_name_and_website_and_community_id("Eventful", "http://www.eventful.com/", community).id
       e.owner_type = "Feed"
       e.source_feed_id = event['id']
-      e.save!
-      puts "Saved"
+      e.save
     rescue TypeError => e
       puts "Failed with TypeError on Community with zipcode #{community.zip_code}!"
-
     end
   end
 
@@ -54,23 +58,16 @@ class EventfulImporter
 
     Community.find(:all).each do |community|
       results = eventful.call 'events/search',
-      :location => community.zip_code,
-      :page_size => 10,
-      :mature => 'safe'
-      # if results['events'].length == 1
-      #   results['events'] = ['event' => results['events']]
-      # end
+        :location => community.zip_code,
+        :page_size => 10,
+        :mature => 'safe'
+        
       if results['events'].nil? then
         next
       end
-      #if results['events'].length == 1
-      # self.import_event(results['events']['event'],community.zip_code)
-      #else
       results['events']['event'].each do |event|
         self.import_event(event,community)
       end
-      #end
     end
-
   end
 end
