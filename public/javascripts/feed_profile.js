3d05261a1f7058c424f75aa88219b9936aab6182
Jack Wink <jackwink@umich.edu>
2011-01-15 18:31:48 -0500

    Semi-updated Js for feed profile
    Signed-off-by: Jack Wink <jackwink@umich.edu>

--- public/javascripts/feed_profile.js
+++ public/javascripts/feed_profile.js
@@ -1,23 +1,9 @@
 $(document).ready(function(){
-	$('#new_event').hide();
-	$('form').queue();
-	$('#post_tabs li:first-child').next().addClass('inactive').next().addClass('inactive');
-	$('#post_tabs li').click(function(){
-			$('form').hide();
-			
-			if($(this).text()==$('#post_tabs li:first-child').text()){
-				$('#new_announcement').fadeIn();
-			}
-			else if($(this).text()==$('#post_tabs li:last-child').text()){
-				alert('soon');
-			}
-			else{
-				$('#new_event').fadeIn();
-			}
-    	$('#post_tabs li').each(function(){
-			$(this).addClass('inactive');			
-		});
-		$(this).removeClass('inactive');
-	});
+	$('#post-to-feed h2 nav li:last-child').hide();
+	$('#post-to-feed h2 nav ul').hover(function(){
+		$('#post-to-feed h2 nav li:last-child').show();
+	}, function(){
+		$('#post-to-feed h2 nav li:last-child').hide();	
+	})
 });
 	
