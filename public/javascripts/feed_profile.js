4f7fd5a05845f98641fa814b3c42e1a34a825748
Jack Wink <jackwink@umich.edu>
2011-01-15 20:44:00 -0500

    Minor JS edit for IE7
    Signed-off-by: Jack Wink <jackwink@umich.edu>

--- public/javascripts/feed_profile.js
+++ public/javascripts/feed_profile.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('#post-to-feed h2 nav li:last-child').hide();
+	$('#post-to-feed h2 nav li:last-child a').css({background-image: "none !important"});
 	$('#post-to-feed h2 nav ul').hover(function(){
 		$('#post-to-feed h2 nav li:last-child').show();
 	}, function(){
