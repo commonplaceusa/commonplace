1f4565736b437c6b05d758c471f68799bb140dcd
Jack Wink <jackwink@umich.edu>
2011-01-15 21:05:52 -0500

    Js changes didn't go through?
    Signed-off-by: Jack Wink <jackwink@umich.edu>

--- public/javascripts/feed_profile.js
+++ public/javascripts/feed_profile.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	$('#post-to-feed h2 nav li:last-child').hide();
-	$('#post-to-feed h2 nav li:last-child a').css("background", "none !important");
 	$('#post-to-feed h2 nav ul').hover(function(){
 		$('#post-to-feed h2 nav li:last-child').show();
 	}, function(){
