09673b92ed54ca5f25ff4796cb67fee33964bde5
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-17 20:03:14 -0500

    Facebook Connect login is now working.

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "facebooker"
+ # config.gem "facebooker"
 
   config.gem "facebooker"
 
