a7fae9a21b35250399519a334967c794baefae8e
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-17 20:16:35 -0500

    Prepared for staging server

---
+++ public/javascripts/facebook_connect_staging.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  FB.init({
+    appId:***REMOVED***, cookie:true, 
+    status:true, xfbml:true 
+  });
+});
+function userExistsWithFacebookUID(uid)
+{
+  $.post()
+}
+function facebookUserWantsToLogIn(user)
+{
+  FB.api('/me', function(user) { 
+    // Send the user to the User Sessions controller to handle the login
+    window.location = '/user_sessions/create_from_facebook';
+  });
+}
+function facebookUserLoggedIn(user)
+{
+  FB.api('/me', function(user) { 
+    $('#user_full_name').val(user.name);
+    $('#user_email').val(user.email);
+    $('#user_facebook_uid').val(user.id);
+    $('#user_address').val(user.address.street);
+  });
+}
