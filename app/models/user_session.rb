37788076399f430487794ec4acfb2ba786892e0b
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-19 12:44:22 -0500

    Added ability to post to Facebook Connect users wall

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,5 +2,5 @@ class UserSession < Authlogic::Session::Base
   oauth2_client_id     CONFIG["facebook_api_key"]
   oauth2_client_secret CONFIG["facebook_secret_key"]
   oauth2_site          "https://graph.facebook.com"
-  oauth2_scope         "email,user_hometown"
+  oauth2_scope         "email,user_hometown,publish_stream"
 end
