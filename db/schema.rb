6b98d3d1b322fcdf67b72cf28a5e6814c4218d5e
Jack Wink <jackwink@umich.edu>
2011-01-20 12:29:31 -0500

    Centers grey "main" content again
    Signed-off-by: Jack Wink <jackwink@umich.edu>

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,19 @@ ActiveRecord::Schema.define(:version => 20110118164820) do
     t.text     "signup_message"
   end
 
+  create_table "conversation_memberships", :force => true do |t|
+    t.integer  "user_id",         :null => false
+    t.integer  "conversation_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "conversations", :force => true do |t|
+    t.string   "subject",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "events", :force => true do |t|
     t.string   "name",            :null => false
     t.text     "description",     :null => false
@@ -74,6 +87,13 @@ ActiveRecord::Schema.define(:version => 20110118164820) do
     t.string   "source_feed_id"
     t.string   "address"
     t.string   "venue"
+  end
+
+  create_table "feedbacks", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.string   "contents",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "feeds", :force => true do |t|
@@ -106,6 +126,15 @@ ActiveRecord::Schema.define(:version => 20110118164820) do
     t.text     "body"
     t.string   "inviter_type"
     t.integer  "invitee_id"
+  end
+
+  create_table "links", :force => true do |t|
+    t.integer  "linkable_id",   :null => false
+    t.string   "linkable_type", :null => false
+    t.integer  "linker_id",     :null => false
+    t.string   "linker_type",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "locations", :force => true do |t|
@@ -152,6 +181,29 @@ ActiveRecord::Schema.define(:version => 20110118164820) do
     t.string   "notified_type"
   end
 
+  create_table "organizations", :force => true do |t|
+    t.string   "name",                :null => false
+    t.string   "address"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "lat"
+    t.decimal  "lng"
+    t.text     "about"
+    t.string   "phone"
+    t.string   "website"
+    t.integer  "community_id"
+    t.string   "category"
+  end
+
+  create_table "platform_updates", :force => true do |t|
+    t.string   "subject",    :null => false
+    t.text     "body",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", :force => true do |t|
     t.text     "body",            :null => false
     t.integer  "user_id",         :null => false
@@ -187,6 +239,13 @@ ActiveRecord::Schema.define(:version => 20110118164820) do
     t.datetime "updated_at"
     t.string   "repliable_type"
     t.boolean  "official",       :default => false, :null => false
+  end
+
+  create_table "roles", :force => true do |t|
+    t.integer  "user_id",         :null => false
+    t.integer  "organization_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "slugs", :force => true do |t|
