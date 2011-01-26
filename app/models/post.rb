24c531586b02af60efecc43943243f4d1b303013
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-26 16:07:58 -0500

    Fixed bug in Base64-encoding that would cause issues with IDs of form 2^n

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,7 +23,7 @@ class Post < ActiveRecord::Base
     long_id = Base64.b64encode(self.id.to_s)
      m = long_id.match(/[A-Za-z0-9]*(=*)/)
     
-    if m[1]
+    if m[1].present?
       long_id = long_id.gsub(m[1],m[1].length.to_s)
     end
     long_id.gsub("\n","")
