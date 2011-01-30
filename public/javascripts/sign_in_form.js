7b10cb68228d08c42156db06fb22d2ed03e33f44
Jack Wink <jackwink@umich.edu>
2011-01-29 23:26:19 -0500

    Put login behind a click.  Could probably use a little more styling (use color rather than white/grey)
    Signed-off-by: Jack Wink <jackwink@umich.edu>

---
+++ public/javascripts/sign_in_form.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('#sign_in_button').click(function(){
+    	if($('#sign_in_button').hasClass('border')){
+        	$('#sign_in_button').removeClass('border').addClass('remove_border');
+        	$('#new_user_session').slideDown(300);  
+    	}
+    	else if($('#sign_in_button').hasClass('remove_border')){
+       		$('#new_user_session').slideUp(300, function(){
+            	$('#sign_in_button').removeClass('remove_border').addClass('border');
+        	});
+    	}
+	});
+});
+
+function drop_login_form()
+{
+	$('#sign_in_button').removeClass('border').addClass('remove_border');
+	$('#new_user_session').show(); 
+}
