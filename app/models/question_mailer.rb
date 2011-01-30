a7f4e3c8809e6b52d93ae5d577e7ae9bfffc4906
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-29 22:04:39 -0500

    Hooked up FAQ page. Need to test on staging

---
+++ app/models/question_mailer.rb
@@ -0,0 +1,20 @@
+require 'smtp_api_header.rb'
+
+class QuestionMailer < ActionMailer::Base
+  
+  helper :text
+  helper_method :url
+  include TextHelper
+
+  include Resque::Mailer
+  @queue = :notifications
+  
+  def faq(from, body)
+    recipients "jason@jasonberlinsky.com"
+    header = SmtpApiHeader.new
+    @headers['X-SMTPAPI'] = header.asJSON
+    subject "New Question for you!"
+    from "CommonPlace <do-not-reply@commonplaceusa.com>"
+  end
+  
+end
