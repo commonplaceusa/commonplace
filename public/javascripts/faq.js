b27db81b3e8046ca19ea3b61e75e3ce8e8184625
Jack Wink <jackwink@umich.edu>
2011-01-29 20:01:04 -0500

    Fixed js issue.  Ready to merge, but the FAQ still needs to be revised for content...  Unless the answer to "who is commonplace?" really is "CommonPlace is stellar team made up of ambitious young entrepreneurs, developers, and designers. Did I mention we're all good looking and single?"
    Signed-off-by: Jack Wink <jackwink@umich.edu>

--- public/javascripts/faq.js
+++ public/javascripts/faq.js
@@ -1,20 +1,45 @@
 $(document).ready(function(){	
 	var sidebar = $('#sections_picker');
 	var top = sidebar.offset().top;
-	$(window).scroll(function (event) {
+	var main;
+	$(window).resize(function (event){
 		var ypos = $(this).scrollTop();
-		if (ypos >= top-20) {
-        	if(-180+ypos<=1864){
-            	sidebar.css({top: (-180+ypos)+"px"});
-            }
-            else{ 
-               	sidebar.css({top: "1864px"});
-            }
+		main = $('#main').offset().left;
+		main += $('#main').width();
+		main -= sidebar.width();
+		main += 45;
+	  if(ypos <= 1952){
+		if (ypos+40 >= top) {
+            sidebar.css({position: "fixed", left: main, top: "30px"});
 		}
         else{
-            sidebar.css({top: ""});
+            sidebar.css({position: "absolute", left: "", top: "30px"});
         }
+      }
+      else{
+		sidebar.css({position: "absolute", top: "1802px", left:""})
+	  }	
+	});
+	$(window).scroll(function (event) {
+		var ypos = $(this).scrollTop();
+		main = $('#main').offset().left;
+		main += $('#main').width();
+		main -= sidebar.width();
+		main += 45;
+	  if(ypos <= 1952){
+		if (ypos+40 >= top) {
+            sidebar.css({position: "fixed", left: main, top: "30px"});
+		}
+        else{
+            sidebar.css({position: "absolute", left: "", top: "30px"});
+        }
+      }
+      else{
+		sidebar.css({position: "absolute", top: "1802px", left:""})
+	  }
 	});
 
 	$.localScroll();
+	
+
 });
