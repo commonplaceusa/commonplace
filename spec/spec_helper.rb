2071a03596d81feeff3e30e52d3aa79ce9fb4632
Max Tilford <maxtilford@gmail.com>
2011-02-01 14:24:11 -0800

    set up current_user and current_community mocks for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,23 @@ Spork.prefork do
   end
   
   def current_user
-    @current_user ||= User.new
+    unless @current_user
+      @current_user = mock_model(User)
+      stub(UserSession).find.stub!.user { @current_user }
+    end
+    @current_user
   end
   
   def current_user_session
     @current_user_session ||= stub(UserSession.new).user { stub(current_user).role_symbols { [:user] } }
+  end
+  
+  def current_community
+    unless @current_community
+      @current_community = mock_model(Community)
+      stub(Community).find_by_slug { @current_community }
+    end
+    @current_community
   end
   
   def login
