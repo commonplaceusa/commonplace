8b897a56caf933b75244ec2d349bf105fd69fe80
Max Tilford <maxtilford@gmail.com>
2011-02-01 22:54:14 -0800

    refactors messages_controller_spec

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,25 +3,23 @@ require 'spec_helper'
 describe MessagesController do
 
   describe "#create" do
+    
     before :each do
       current_community
-      @message = mock_model(Message)
-      stub(current_user).messages.stub!.build { @message }
-    end
-    
-    it "should try to create a message" do
-      mock(@message).save
-      post :create
+      current_user
+      @message = mock_model(Message, 
+                            :messagable => stub!.name { "Messagable Name" })
+      stub(NotificationsMailer).deliver_message
+      stub(Message).new { @message }
     end
     
     context "on successful save" do
-      before :each do 
+      
+      before :each do
         stub(@message).save { true }
-        stub(@message).messagable.stub!.name { "Messagable Name" }
-        stub(NotificationsMailer).deliver_message
         post :create
       end
-
+      
       it "delivers a notification to the messagable" do
         NotificationsMailer.should have_received.deliver_message(@message.id) 
       end
