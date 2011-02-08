4cf37f86faf51f00f8ea43d6346682eeea61f92e
Max Tilford <maxtilford@gmail.com>
2011-02-07 17:34:31 -0800

    [Finishes #9666037] implement inbox design

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -2,5 +2,7 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(:default =
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :default => "%I:%M %p",
-  :date_time12  => "%m/%d/%Y %I:%M%p"
+  :date_time12  => "%m/%d/%Y %I:%M%p",
+  :month_day_at_time => "%B %d at %I:%M%p",
+  :month_day_year => "%B %d, %Y"
 )
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,33 +1,49 @@
 require 'spec_helper'
 
 describe Message do
+  describe "#valid?" do
+    before :each do 
+      @message = Message.new
+    end
 
-  before :each do 
-    @message = Message.new
+    it "requires a subject" do
+      @message.valid?
+      @message.should have(1).errors_on(:subject)
+    end
+    
+    it "requires a body" do
+      @message.valid?
+      @message.should have(1).errors_on(:body)
+    end
+    
+    it "requires a user (owner)" do
+      @message.valid?
+      @message.should have(1).errors_on(:user)
+    end
+    
+    it "requires a messagable" do
+      @message.valid?
+      @message.should have(1).errors_on(:messagable)
+    end
   end
 
-  it "should require a subject" do
-    @message.valid?
-    @message.should have(1).errors_on(:subject)
-  end
+  describe "#most_recent_body" do
+    let(:message) do
+      mock_model(Message, :body => Forgery(:basic).text)
+    end
+    context "given a message with no replies" do
+      it "returns the message body" do
+        stub(message).replies { [] }
+        message.most_recent_body.should == message.body
+      end
+    end
 
-  it "should require a body" do
-    @message.valid?
-    @message.should have(1).errors_on(:body)
+    context "given a message with replies" do
+      it "returns the most recent reply's body" do
+        replies = Array.new(3) { |i| mock_model(Reply, :body => "body #{i}")}
+        stub(message).replies { replies }
+        message.most_recent_body.should == replies.last.body
+      end
+    end
   end
-
-  it "should require a user (owner)" do
-    @message.valid?
-    @message.should have(1).errors_on(:user)
-  end
-
-  it "should require a messagable" do
-    @message.valid?
-    @message.should have(1).errors_on(:messagable)
-  end
-
-  it "should be repliable" do
-    @message.replies.build.should be_a(Reply)
-  end
-
 end
