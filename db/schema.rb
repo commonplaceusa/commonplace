ecfa9e6c727226de62a42a8e1a2d28635dc67efb
Jason Berlinsky <jason@jasonberlinsky.com>
2011-02-12 01:14:47 -0500

    Fixed stupid typo and added 'To: ' prefix

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 ActiveRecord::Schema.define(:version => 20110208185254) do
 
   create_table "announcements", :force => true do |t|
-    t.string   "subject",                                :null => false
-    t.text     "body",                                   :null => false
-    t.integer  "feed_id",                                :null => false
+    t.string   "subject",                       :null => false
+    t.text     "body",                          :null => false
+    t.integer  "feed_id",                       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "private",    :default => false,          :null => false
-    t.string   "type",       :default => "Announcement"
+    t.boolean  "private",    :default => false, :null => false
+    t.string   "type"
     t.string   "url"
   end
 
@@ -40,6 +40,19 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.text     "signup_message"
   end
 
+  create_table "conversation_memberships", :force => true do |t|
+    t.integer  "user_id",         :null => false
+    t.integer  "conversation_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "conversations", :force => true do |t|
+    t.string   "subject",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "events", :force => true do |t|
     t.string   "name",            :null => false
     t.text     "description",     :null => false
@@ -54,8 +67,15 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.string   "source_feed_id"
     t.string   "address"
     t.string   "venue"
-    t.string   "type"
     t.string   "host_group_name"
+    t.string   "type"
+  end
+
+  create_table "feedbacks", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.string   "contents",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "feeds", :force => true do |t|
@@ -77,6 +97,7 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.string   "address"
     t.string   "hours"
     t.string   "slug"
+    t.integer  "owner_id"
     t.string   "twitter_name"
   end
 
@@ -88,6 +109,15 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.text     "body"
     t.string   "inviter_type"
     t.integer  "invitee_id"
+  end
+
+  create_table "links", :force => true do |t|
+    t.integer  "linkable_id",   :null => false
+    t.string   "linkable_type", :null => false
+    t.integer  "linker_id",     :null => false
+    t.string   "linker_type",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "messages", :force => true do |t|
@@ -113,6 +143,29 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "bounds"
+  end
+
+  create_table "organizations", :force => true do |t|
+    t.string   "name",                :null => false
+    t.string   "address"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "lat"
+    t.decimal  "lng"
+    t.text     "about"
+    t.string   "phone"
+    t.string   "website"
+    t.integer  "community_id"
+    t.string   "category"
+  end
+
+  create_table "platform_updates", :force => true do |t|
+    t.string   "subject",    :null => false
+    t.text     "body",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", :force => true do |t|
@@ -142,6 +195,13 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.datetime "updated_at"
     t.string   "repliable_type"
     t.boolean  "official",       :default => false, :null => false
+  end
+
+  create_table "roles", :force => true do |t|
+    t.integer  "user_id",         :null => false
+    t.integer  "organization_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "subscriptions", :force => true do |t|
@@ -200,10 +260,11 @@ ActiveRecord::Schema.define(:version => 20110208185254) do
     t.boolean  "admin",                                         :default => false
     t.string   "state"
     t.string   "avatar_file_name"
-    t.string   "address"
     t.integer  "facebook_uid",                     :limit => 8
+    t.string   "address"
     t.string   "oauth2_token"
     t.integer  "community_id"
+    t.boolean  "facebook_app"
   end
 
   add_index "users", ["oauth2_token"], :name => "index_users_on_oauth2_token"
