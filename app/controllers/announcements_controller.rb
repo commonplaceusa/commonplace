9e5ffeef92da1af652fabb01a996e844fdf406ee
Jason Berlinsky <jason@jasonberlinsky.com>
2011-02-17 11:18:30 -0500

    Switched sorting in syndicate to updated_at

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,7 +2,7 @@ class AnnouncementsController < CommunitiesController
   load_and_authorize_resource
 
   def index
-    @items = current_community.announcements.all(:order => "created_at DESC")
+    @items = current_community.announcements.all(:order => "updated_at DESC")
   end
   
   def subscribed
