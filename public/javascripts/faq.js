b710feaa4b0ba5fb2d53856824868c07f6f7cbac
Jack Wink <jackwink@umich.edu>
2011-02-27 12:57:38 -0500

    Updated FAQ.

--- public/javascripts/faq.js
+++ public/javascripts/faq.js
@@ -1,48 +1,3 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
 $(document).ready(function(){   
     var sidebar = $('#sections_picker');
     var top = sidebar.offset().top;
@@ -53,7 +8,7 @@ $(document).ready(function(){
         main += $('#main').width();
         main -= sidebar.width();
         main += 45;
-      if(ypos <= 1952){
+      if(ypos <= 3772){
         if (ypos+40 >= top) {
             sidebar.css({position: "fixed", left: main, top: "30px"});
         }
@@ -62,7 +17,7 @@ $(document).ready(function(){
         }
       }
       else{
-        sidebar.css({position: "absolute", top: "1802px", left:""})
+        sidebar.css({position: "absolute", top: "3637px", left:""})
       } 
     });
     $(window).scroll(function (event) {
@@ -71,7 +26,7 @@ $(document).ready(function(){
         main += $('#main').width();
         main -= sidebar.width();
         main += 45;
-      if(ypos <= 1952){
+      if(ypos <= 3772){
         if (ypos+40 >= top) {
             sidebar.css({position: "fixed", left: main, top: "30px"});
         }
@@ -80,7 +35,7 @@ $(document).ready(function(){
         }
       }
       else{
-        sidebar.css({position: "absolute", top: "1802px", left:""})
+        sidebar.css({position: "absolute", top: "3637px", left:""})
       }
     });
  
