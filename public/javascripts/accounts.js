dd83165133f14bebc1071cfa35ff8cdecd5576b3
Max Tilford <maxtilford@gmail.com>
2011-03-01 10:41:00 -0800

    use accounts.js, rm file_upload_fix.js and who_is_bubble_shower.js

--- public/javascripts/accounts.js
+++ public/javascripts/accounts.js
@@ -15,22 +15,31 @@ $(document).ready(function(){
 	//Style fix for the photoupload stuff
 	var style_fix = '<div id="file_input_fix"><input type="text" name="file_fix" id="file_style_fix"></input><div id="browse_button">Browse...</div></div>';
 	var take_photo_button = '<div id="take_a_photo" onClick="load_modal();">Take a photo</div>';
-	$('#user_avatar_input').append(style_fix).append(take_photo_button).css("min-height", "54px");
+	$('#user_avatar_input').append(style_fix)
+  // .append(take_photo_button)
+          .css("min-height", "54px");
+
 	$('#user_avatar').css("opacity", 0);
 	$('#user_avatar').css("z-index", 2); 
 	$('#user_avatar').css("position", "absolute")
 	$('#user_avatar').css("top", "25px")
 	$('#user_avatar').css("height", "30px");
+
+  $('#user_avatar').change(function() {
+    $("#file_input_fix input").val($(this).val().replace(/^.*\\/,""));
+  });
+
+
 	
 	//Changes css of clicked 
-	$('.unchecked').click(function(){
+  $('.unchecked').click(function(){
     if($(this).hasClass('checked')){
         $(this).removeClass('checked');
     }
     else{
        $(this).addClass('checked');
     }
-	});
+  });
 });
 
 function load_modal(){
