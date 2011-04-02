da05cbb5d8555988537b37070343ae2ae1b665b1
Jack Wink <jackwink@umich.edu>
2011-04-02 00:37:21 -0400

    Added overlays to the slideshow on the starter site.

--- public/javascripts/starter_site/slideshow.js
+++ public/javascripts/starter_site/slideshow.js
@@ -17,11 +17,17 @@ function startSlideshow(){
   if(play){
     if($('.active').next('img').length != 0){
       $('.active').removeClass('active').animate({left: "-420px"}, 500).next('img').animate({left: "0px"}, 500).addClass('active');
+      $('.slide_info').fadeOut(300, function(){
+        $('.slide_info').delay(400).html($('.active').attr('alt')).delay(200).fadeIn();
+      });
     }
     else{
      $('#slides img:first-child').addClass('back').appendTo('#slides');
      $('.back').css({left: "420px"}).removeClass('back');
      $('.active').removeClass('active').animate({left: "-420px"}, 500).next('img').animate({left: "0px"}, 500).addClass('active');
+     $('.slide_info').fadeOut(300, function(){
+        $('.slide_info').delay(400).html($('.active').attr('alt')).delay(200).fadeIn();
+      });
     }
   }
   setTimeout("startSlideshow()", 5000);
