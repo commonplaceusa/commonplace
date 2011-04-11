aecc3b3fbf514366ee0a9de2b95ae6dd2ac493b1
Max Tilford <maxtilford@gmail.com>
2011-04-10 22:44:57 -0700

    reset User column information before in user middle_name migration

--- db/migrate/20110409180957_add_middle_name_to_user.rb
+++ db/migrate/20110409180957_add_middle_name_to_user.rb
@@ -1,6 +1,9 @@
 class AddMiddleNameToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :middle_name, :string
+    
+    User.reset_column_information
+
     User.find(:all).each do |user|
       if user.last_name.include? " "
         user.full_name = user.first_name.to_s + " " + user.last_name.to_s
