999d4422289a90d1d8e85ec55a34bc9b6b7aecad
Max Tilford <maxtilford@gmail.com>
2011-04-18 17:23:17 -0700

    add feed subscriptions to settings page

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,10 @@
 class Subscription < ActiveRecord::Base
-
+  attr_accessible :receive_method
   belongs_to :user
   belongs_to :feed
+
+  def self.receive_methods
+    ["Live", "Daily", "Never"]
+  end
 
 end
