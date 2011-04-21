7dd0674f0301490f9893836d490eaa397a875a1b
Max Tilford <maxtilford@gmail.com>
2011-04-21 14:10:11 -0700

    the login form goes down, but doesn't go back up

--- public/javascripts/sign_in_form.js
+++ public/javascripts/sign_in_form.js
@@ -1,23 +1,7 @@
-function sign_in_click(){
-	$('#sign_in_button').click(function(){
-    	if($('#sign_in_button').hasClass('border')){
-        	$('#sign_in_button').removeClass('border').addClass('remove_border');
-        	$('#new_user_session').slideDown(300);  
-    	}
-    	else if($('#sign_in_button').hasClass('remove_border')){
-       		$('#new_user_session').slideUp(300, function(){
-            	$('#sign_in_button').removeClass('remove_border').addClass('border');
-        	});
-    	}
-	});
-}
-
-function drop_login_form()
-{
-	$('#sign_in_button').removeClass('border').addClass('remove_border');
-	$('#new_user_session').show(); 
-}
 
 $(function() {
-  sign_in_click();
+  $('#sign_in_button').click(function() {
+    $(this).addClass("open");
+    $("#new_user_session").slideDown(300);
+  });
 });
