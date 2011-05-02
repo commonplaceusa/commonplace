0b623f4d80edb7beb160cbb3abdc83bf714c451f
Max Tilford <maxtilford@gmail.com>
2011-05-02 08:14:03 -0700

    move last emails to new mail system

---
+++ mail/password_reset.rb
@@ -0,0 +1,30 @@
+class PasswordReset < MailBase
+
+  def initialize(user_id)
+    @user = User.find(user_id)
+  end
+
+  def subject
+    "CommonPlace password reset"
+  end
+
+  def from
+    "passwords@commonplaceusa.com"
+  end
+
+  def user
+    @user
+  end
+
+  def reset_url
+    url("/password_resets/#{user.perishable_token}")
+  end
+
+  def community_name
+    community.name
+  end
+
+  def community
+    user.community
+  end
+end
