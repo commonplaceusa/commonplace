ec3f463aa90d997bc143cae401485b13cbe2bbdd
Max Tilford <maxtilford@gmail.com>
2011-05-05 13:31:15 -0700

    use the rss feed item's date for rss announcements

--- lib/rss_importer.rb
+++ lib/rss_importer.rb
@@ -2,29 +2,24 @@ class RSSImporter
   require 'rss'
   
   def self.perform
-    
-    subject_tag     = CONFIG['rss_subject_tag'] || 'title'
-    body_tag        = CONFIG['rss_body_tag'] || 'description'
-    story_url_tag   = CONFIG['rss_story_url_tag'] || 'link'
-    
-    
+    RSSAnnouncement.record_timestamps = false    
     Feed.find(:all, :conditions => ["feed_url != ?", "" ]).each do |feed|
+
       RSS::Parser.parse(open(feed.feed_url).read, false).items.each do |item|
         
-        # If the item hasn't been added yet
-        unless RSSAnnouncement.exists?(:url => item.send(story_url_tag))
-            
-          # Pull the values from the Item object, using arbitrary tag names 
+        unless RSSAnnouncement.exists?(:url => item.link)
           RSSAnnouncement.create(:owner => feed,
-                                 :subject => item.send(subject_tag),
-                                 :url => item.send(story_url_tag),
+                                 :subject => item.title,
+                                 :url => item.link,
                                  :community_id => feed.community_id,
-                                 :body => McBean.fragment(item.send(body_tag)).to_markdown)
-          
+                                 :body => McBean.fragment(item.description).to_markdown,
+                                 :created_at => item.date.to_datetime,
+                                 :updated_at => item.date.to_datetime)
+
         end
       end
-      
     end
+    RSSAnnouncement.record_timestamps = true
     
   end
 end
