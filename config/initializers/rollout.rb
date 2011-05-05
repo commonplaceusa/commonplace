10a77f684061b39a041267509521721d48634ced
Max Tilford <maxtilford@gmail.com>
2011-05-05 07:15:22 -0700

    add harrisonburg and fallschurch rollout groups

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -5,3 +5,11 @@ $rollout = Rollout.new($redis)
 $rollout.define_group(:alpha) do |user|
   user.community.slug == "CommonPlace"
 end
+
+$rollout.define_group(:harrisonburg) do |user|
+  user.community.slug == "harrisonburg"
+end
+
+$rollout.define_group(:fallschurch) do |user|
+  user.community.slug == "fallschurch" 
+end
