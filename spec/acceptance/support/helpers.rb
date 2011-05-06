08ed62b51b9627b73c44e5fd4d322c6bebbfd8d5
Max Tilford <maxtilford@gmail.com>
2011-05-06 12:59:54 -0700

    refactor community and user creation in acceptance specs

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -9,6 +9,23 @@ module Helpers
     end
   end
 
+  def create_community
+    community = Factory(:community, :slug => "testing", :zip_code => "02321")
+    Neighborhood.create(:community_id => community.id, :name => "foo", :bounds => [[0,0]])
+    Capybara.app_host = "http://testing.smackaho.st:#{Capybara.server_port}"
+    community
+  end
+
+
+  def create_user(community)
+    User.create!(:first_name => "test", :last_name => "dev",
+                 :email => "test@example.com", :address => "420 Baker St.",
+                 :password => "password", :neighborhood_id => community.neighborhoods.first.id,
+                 :community_id => community.id)
+    
+  end
+  
+
 
 end
 
