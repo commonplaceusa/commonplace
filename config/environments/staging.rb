4d8a61b28047699cfcf2cdc465a56b8a9defdeb7
Max Tilford <maxtilford@gmail.com>
2011-05-09 13:20:29 -0700

    remove no longer existant config option from environments/staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,7 +7,6 @@ Commonplace::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.action_controller.consider_all_requests_local = false
   config.action_controller.perform_caching             = true
-  config.action_view.cache_template_loading            = true
   config.log_level = :debug
   
   
