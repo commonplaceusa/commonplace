2fe376e54ca2acd529f1fd99cd2e0b591bd79632
Max Tilford <maxtilford@gmail.com>
2011-05-10 07:17:03 -0700

    set a timezone for already existing communities in the migration (:default won't do that)

--- db/migrate/20110510034428_add_timezone_to_community.rb
+++ db/migrate/20110510034428_add_timezone_to_community.rb
@@ -1,6 +1,13 @@
 class AddTimezoneToCommunity < ActiveRecord::Migration
   def self.up
     add_column :communities, :time_zone, :string, :default => "Eastern Time (US & Canada)"
+
+    Community.reset_column_information
+
+    Community.find_each do |community|
+      community.time_zone = "Eastern Time (US & Canada)"
+      community.save!
+    end
   end
 
   def self.down
