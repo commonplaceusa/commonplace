c3c74d3fdb91e498e8d5d9673437f48a728729da
Max Tilford <maxtilford@gmail.com>
2011-05-11 02:30:16 -0700

    use --binstubs on bundle install deploy

--- config/deploy/gems.rb
+++ config/deploy/gems.rb
@@ -1,7 +1,7 @@
 namespace :gems do
   task :install, :roles => :app do
     # For when we use bundler
-    run "cd #{current_path} && bundle install --deployment"
+    run "cd #{current_path} && bundle install --deployment --binstubs"
   end
 end
 after "deploy:update_code", "gems:install"
