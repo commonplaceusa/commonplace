08e8743f0d9b3809d4fada167806988bf43ef3e3
Max Tilford <maxtilford@gmail.com>
2011-05-13 10:49:14 -0700

    migrate neighborhoods from bounds to centers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110510204754) do
+ActiveRecord::Schema.define(:version => 20110513171458) do
 
   create_table "announcements", :force => true do |t|
     t.string   "subject",                                  :null => false
