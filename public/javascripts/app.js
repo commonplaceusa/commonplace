4ef50d4e361a7e4e157832b0e0dca5e2d3bc0bd1
Max Tilford <maxtilford@gmail.com>
2011-05-15 21:31:46 -0700

    move say-something and whats-happening js to backbone

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,60 +1,107 @@
-$(function() {
+var CommonPlace = {};
+
+CommonPlace.WhatsHappening = Backbone.View.extend({
+  tagName: "div",
   
+  id: "whats-happening",
+
+  events: {
+    "click #zones a, #syndicate h3 a" : "navigate"
+  },
+
+  navigate: function(e) {
+    var self = this,
+        $link = $(e.currentTarget);
+    
+    e.preventDefault();
+    self.$('#zones a').removeClass('selected_nav')
+      .filter('.' + $link.attr('class'))
+      .addClass('selected_nav');
+
+    $.get($link.attr('href'), function(response) {
+      if (response) {
+        self.$('#syndicate').replaceWith($(window.innerShiv(response,false)).find("#syndicate"));
+
+        $("#syndicate form.new_reply").submit(function(e) {
+          e.preventDefault();
+          var that = this;
+          $.post($(that).attr("action"), $(that).serialize(), 
+                 function(response) {
+                   if (response) {
+                     var $replies = $(that).closest("div.replies");
+                     $replies.replaceWith($(window.innerShiv(response,false)).filter("#" + $replies.attr('id')));
+                   }
+                 });
+        });
+
+        $("#syndicate .replies a.all-replies").click(function(e) {
+          e.preventDefault();
+          $(this).hide();
+          $(this).siblings("ul").children("li").show();
+        });
+
+        $("#syndicate .item a.show-reply-form").click(function(e) {
+          e.preventDefault();
+          $(this).hide();
+          $(this.parent().siblings("div.replies").show();
+        });
+
+
+        $("#whats-happening li.item").hoverIntent(function(){
+          $.get($(this).find('div').first().data('href'),
+                function(response) {
+                  if (response) {
+                    $('#community-profiles').replaceWith($(window.innerShiv(response,false)).find("#community-profiles"));
+                    setInfoBoxPosition();
+                  }
+                });
+        }, $.noop);
+      }
+    });
+  }
+});
+
+CommonPlace.SaySomething = Backbone.View.extend({
+  tagName: "div",
+  id: "say-something",
+  
+  events: {
+    "click nav a": "navigate"
+  },
+  
+  navigate: function(e) {
+    e.preventDefault();
+    var self = this,
+        $link = $(e.currentTarget);
+    
+    self.$("nav a").removeClass("current")
+      .filter("." + $link.attr('class')).addClass("current");
+
+
+    $.get($link.attr('href'),
+          function(response) {
+            if (response) {
+              self.$("form").replaceWith($(window.innerShiv(response,false)).find("#say-something form"));
+              self.$("h2").replaceWith($(window.innerShiv(response, false)).find("#say-something h2"));
+      
+              $('input.date').datepicker({
+                prevText: '&laquo;',
+                nextText: '&raquo;',
+                showOtherMonths: true,
+                defaultDate: null
+              });
+            }
+          });    
+    
+  }
+  
+});
+
+$(function() {
   $.preLoadImages("/images/loading.gif");
+  new CommonPlace.WhatsHappening({el: $("#whats-happening")});
+  new CommonPlace.SaySomething({el: $("#say-something")});
 
-  $("#say-something nav a").live('click', function(e) {
-    e.preventDefault();
-    $.get($(this).attr('href'),
-          function(response) {
-            if (response) {
-              $("#say-something").replaceWith($(window.innerShiv(response,false)).find("#say-something"));
-              $('input.date').datepicker({
-                prevText: '&laquo;',
-                nextText: '&raquo;',
-                showOtherMonths: true,
-                defaultDate: null
-              });
-            }
-          });
-  });
-
-  $("#whats-happening nav#zones a, #syndicate h3 a").live('click', function(e) {
-    e.preventDefault();
-    $.get($(this).attr('href'),
-          function(response) {
-            if (response) {
-              $('#whats-happening').replaceWith($(window.innerShiv(response,false)).find("#whats-happening"));
-              $("#say-something").replaceWith($(window.innerShiv(response,false)).find("#say-something"));
-              $('input.date').datepicker({
-                prevText: '&laquo;',
-                nextText: '&raquo;',
-                showOtherMonths: true,
-                defaultDate: null
-              });
-              $("#whats-happening li.item").hoverIntent(function(){
-                $.get($(this).find('div').first().data('href'),
-                      function(response) {
-                        if (response) {
-                          $('#community-profiles').replaceWith($(window.innerShiv(response,false)).find("#community-profiles"));
-                          setInfoBoxPosition();
-                        }
-                      });
-              }, $.noop);
-            }
-          });
-  });
-
-  $("#syndicate form.new_reply").live("submit", function(e) {
-    e.preventDefault();
-    var that = this;
-    $.post($(that).attr("action"), $(that).serialize(), 
-           function(response) {
-             if (response) {
-               var $replies = $(that).closest("div.replies");
-               $replies.replaceWith($(window.innerShiv(response,false)).filter("#" + $replies.attr('id')));
-             }
-           });
-  });
 
   $('form.formtastic.feed input:text, form.formtastic.feed textarea').keydown(function(e) {
     var $input = $(e.currentTarget);
@@ -64,29 +111,6 @@ $(function() {
         .html($input.val());
     }, 10);
   });
-
-  $("#syndicate .replies a.all-replies").live("click", function(e) {
-    e.preventDefault();
-    $(this).hide();
-    $(this).siblings("ul").children("li").show();
-  });
-
-  $("#syndicate .item a.show-reply-form").live("click", function(e) {
-    e.preventDefault();
-    $(this).hide();
-    $(this).parent().siblings("div.replies").show();
-  });
-
-  $("#whats-happening li.item").hoverIntent(function(){
-    $.get($(this).find('div').first().data('href'),
-          function(response) {
-            if (response) {
-              $('#community-profiles').replaceWith($(window.innerShiv(response,false)).find("#community-profiles"));
-              setInfoBoxPosition();
-            }
-          });
-  }, $.noop);
-
 
   $('a.new_subscription').live('click', function(e) {
     e.preventDefault();
@@ -100,8 +124,6 @@ $(function() {
       }
     });
   });
-
-
 
   $('a.unsubscribe').live('click', function(e) {
     e.preventDefault();
@@ -148,5 +170,5 @@ $(function() {
                                     $(window).trigger('resize.modal');
                                   });
                          });
-  
+
 });
