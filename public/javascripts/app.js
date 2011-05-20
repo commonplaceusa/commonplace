d63ce73fe4d1a539fad82c79f168e0325e47ea9c
Max Tilford <maxtilford@gmail.com>
2011-05-20 07:12:37 -0700

    Add a Backbone app to handle main platform interface with alacrity

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,106 +1,12 @@
-var CommonPlace = {};
+var CommonPlace = CommonPlace || {};
 
-CommonPlace.WhatsHappening = Backbone.View.extend({
-  tagName: "div",
-  
-  id: "whats-happening",
+Mustache.template = function(templateString) {
+  return templateString;
+};
 
-  events: {
-    "click #zones a, #syndicate h3 a" : "navigate"
-  },
-
-  navigate: function(e) {
-    var self = this,
-        $link = $(e.currentTarget);
-    
-    e.preventDefault();
-    self.$('#zones a').removeClass('selected_nav')
-      .filter('.' + $link.attr('class'))
-      .addClass('selected_nav');
-
-    $.get($link.attr('href'), function(response) {
-      if (response) {
-        self.$('#syndicate').replaceWith($(window.innerShiv(response,false)).find("#syndicate"));
-
-        $("#syndicate form.new_reply").submit(function(e) {
-          e.preventDefault();
-          var that = this;
-          $.post($(that).attr("action"), $(that).serialize(), 
-                 function(response) {
-                   if (response) {
-                     var $replies = $(that).closest("div.replies");
-                     $replies.replaceWith($(window.innerShiv(response,false)).filter("#" + $replies.attr('id')));
-                   }
-                 });
-        });
-
-        $("#syndicate .replies a.all-replies").click(function(e) {
-          e.preventDefault();
-          $(this).hide();
-          $(this).siblings("ul").children("li").show();
-        });
-
-        $("#syndicate .item a.show-reply-form").click(function(e) {
-          e.preventDefault();
-          $(this).hide();
-          $(this.parent().siblings("div.replies").show();
-        });
-
-
-        $("#whats-happening li.item").hoverIntent(function(){
-          $.get($(this).find('div').first().data('href'),
-                function(response) {
-                  if (response) {
-                    $('#community-profiles').replaceWith($(window.innerShiv(response,false)).find("#community-profiles"));
-                    setInfoBoxPosition();
-                  }
-                });
-        }, $.noop);
-      }
-    });
-  }
-});
-
-CommonPlace.SaySomething = Backbone.View.extend({
-  tagName: "div",
-  id: "say-something",
-  
-  events: {
-    "click nav a": "navigate"
-  },
-  
-  navigate: function(e) {
-    e.preventDefault();
-    var self = this,
-        $link = $(e.currentTarget);
-    
-    self.$("nav a").removeClass("current")
-      .filter("." + $link.attr('class')).addClass("current");
-
-
-    $.get($link.attr('href'),
-          function(response) {
-            if (response) {
-              self.$("form").replaceWith($(window.innerShiv(response,false)).find("#say-something form"));
-              self.$("h2").replaceWith($(window.innerShiv(response, false)).find("#say-something h2"));
-      
-              $('input.date').datepicker({
-                prevText: '&laquo;',
-                nextText: '&raquo;',
-                showOtherMonths: true,
-                defaultDate: null
-              });
-            }
-          });    
-    
-  }
-  
-});
 
 $(function() {
   $.preLoadImages("/images/loading.gif");
-  new CommonPlace.WhatsHappening({el: $("#whats-happening")});
-  new CommonPlace.SaySomething({el: $("#say-something")});
 
 
   $('form.formtastic.feed input:text, form.formtastic.feed textarea').keydown(function(e) {
