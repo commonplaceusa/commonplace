c1f7ef735693a5215526f7b252e7f99ef15b3284
Max Tilford <maxtilford@gmail.com>
2011-05-21 08:24:22 -0700

    reorganize jquery document.ready callbacks

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -77,4 +77,123 @@ $(function() {
                                   });
                          });
 
+
+  $('#sign_in_button').click(function() {
+    $(this).addClass("open");
+    $("form.user_session").slideDown(300);
+  });
+
+
+  $(window).bind('resize.modal', function () {
+    var $m = $("#modal-content")
+    if ($m.get(0)) {
+      var w = $m.width(),
+      h = $m.height(),
+      $b = $(window),
+      bw = $b.width(),
+      bh = $b.height();
+      
+      $m.css({top: (bh - h) / 2,
+              left: (bw - w) / 2 - 20
+             });
+    }
+  });
+
+  $("#modal-close").live('click', function(e) {
+    $("#modal").html("");
+    e.preventDefault();
+  });
+
+  $("body").bind("#modal", function(e, content) {
+    if (content) {
+      $("#modal").replaceWith(window.innerShiv(content, false));
+    }
+   $(window).trigger('resize.modal');
+   setTimeout(function(){$(window).trigger("resize.modal");}, 500);
+  });
+
+  $("body").trigger("#modal");
+
+
+  var didscroll = false;  
+  $(window).scroll(function() { didscroll = true; });
+
+  setInterval(function() {
+    if (didscroll) {
+      didscroll = false;
+      setInfoBoxPosition();
+    }
+  }, 100); 
+    
+
+  $("body").bind("#information", function(e, content) {
+    if (content) {
+      $("#information").replaceWith(window.innerShiv(content, false));
+    }
+    
+    renderMaps();    
+    
+    setInfoBoxPosition();    
+    
+    $("#file_uploader").change(function() {
+      $(this).trigger('image.inline-form');
+    });
+
+    initInlineForm();
+
+  });
+
+  $("body").trigger("#information");
+
+
+  // Feed Profile
+  $('#post-to-feed h2 nav li:last-child').hide();
+  $('#post-to-feed h2 nav ul').hover(function(){
+    $('#post-to-feed h2 nav li:last-child').show();
+  }, function(){
+    $('#post-to-feed h2 nav li:last-child').hide();	
+  })
+
+
+  // Accounts
+  //Does the who is bubble stuff
+  $('#who_is_info_bubble h3').click(function(){
+    var re = new RegExp(/blue-arrow-down/)
+    if($('#who_is_info_bubble h3.toggle').css("background-image").match(re)){
+      $('#who_is_info_bubble h3.toggle').css("background-image","url(/images/blue-arrow-up.png)")
+    }
+    else{
+      $('#who_is_info_bubble h3.toggle').css("background-image","") 
+    }
+    	$('#who_is_info').slideToggle();
+  });
+
+  //Tool tips for main page:
+//  $('#user_address').tipsy({delayIn: 1000, delayOut: 1000, fade: true, gravity: 'n', trigger: 'focus', live: true, offset: -20 ,fallback: "We need your address so we can place you in a neighborhood within Falls Church!"});
+  
+  //Style fix for the photoupload stuff
+  var style_fix = '<div id="file_input_fix"><input type="text" name="file_fix" id="file_style_fix"></input><div id="browse_button">Browse...</div></div>';
+  var take_photo_button = '<div id="take_a_photo" onClick="load_modal();">Take a photo</div>';
+	$('#user_avatar_input').append(style_fix)
+  // .append(take_photo_button)
+    .css("min-height", "54px");
+  
+	$('#user_avatar').css("opacity", 0);
+	$('#user_avatar').css("z-index", 2); 
+	$('#user_avatar').css("position", "absolute")
+	$('#user_avatar').css("top", "25px")
+	$('#user_avatar').css("height", "30px");
+
+  $('#user_avatar').change(function() {
+    $("#file_input_fix input").val($(this).val().replace(/^.*\\/,""));
+  });
+  
+  
+  // accounts/add_feeds
+  $('.add_groups .group, .add_feeds .feed').click(function(){
+    $('div', this).toggleClass('checked');
+    var $checkbox = $("input:checkbox", this);
+    $checkbox.attr("checked", 
+                   $checkbox.is(":checked") ? false : "checked");
+  });
 });
