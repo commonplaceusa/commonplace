4e6b670911cb79f5b37a62de884d280511960832
Jason Berlinsky <jason@jasonberlinsky.com>
2011-05-27 01:17:18 -0400

    Added diagnostic output

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -1,7 +1,16 @@
+require 'net/http'
+require 'url'
+
 class FacebookCanvasController < ApplicationController
   
   def index
     @request_id = params[:request_ids]
+    url = URI.parse("http://graph.facebook.com/" + @request_id + "/")
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    @data = res.body
     render :layout => nil
   end
 
