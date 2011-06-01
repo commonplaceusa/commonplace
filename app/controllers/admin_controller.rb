1cac20c7da23992684c59b89bd4fa91141a106d5
Jason Berlinsky <jberlinsky@gmail.com>
2011-06-01 16:00:29 -0400

    Added mixpanel funnel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,10 @@ class AdminController < ApplicationController
 
   def overview
     @communities = Community.all
+    @completed_registrations = User.where("created_at < updated_at")
+    @incomplete_registrations = User.where("created_at >= updated_at")
+    @emails_opened_today = 1
+    @emails_opened = 1
     render :layout => nil
   end
 end
