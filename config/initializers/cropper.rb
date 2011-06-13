adb8600a5631f7db92de00432f3e79a0b43bb27e
Max Tilford <maxtilford@gmail.com>
2011-06-13 11:55:06 -0700

    fixes the imagemagick part of image cropping
    * don't crop the croppable style
    * we need to scale by a horizontal ration *and* a vertical ratio
      in order to properly match the croppable to the original

--- config/initializers/cropper.rb
+++ config/initializers/cropper.rb
@@ -1,19 +1,11 @@
 module Paperclip
   class Cropper < Thumbnail
     def transformation_command
-      if crop_command
-        #crop_command + super.join(" ").sub(/ -crop \S+/, '')
-        crop_command + super
-      else
-        super
-      end
-    end
-    
-    def crop_command
       target = @attachment.instance
       if target.cropping?
-       # ["-crop","#{target.w.to_i}x#{target.h.to_i}+#{target.x.to_i}+#{target.y.to_i}"]
-        " -crop '#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}'"
+        ['-crop', "'#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}'"] + super
+      else
+        super
       end
     end
   end
