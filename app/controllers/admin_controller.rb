80b4cf6af7da0521d4fa40eb338e8549c1661fa8
Jason Berlinsky <jberlinsky@gmail.com>
2011-06-14 21:26:50 -0400

    Reverse order of admin controller; Highest to lowest

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -14,7 +14,7 @@ class AdminController < ApplicationController
     @start_year = date.strftime("%Y")
     @start_month = date.strftime("%m")
     @start_day = date.strftime("%d")
-    @communities = Community.all.select{|c| c.users.count > 0 and c.households != nil and c.households > 1 and c.core}.sort{|a,b| a.users.count <=> b.users.count}
+    @communities = Community.all.select{|c| c.users.count > 0 and c.households != nil and c.households > 1 and c.core}.sort{|a,b| a.users.count <=> b.users.count}.reverse
     @completed_registrations = User.where("created_at < updated_at")
     @incomplete_registrations = User.where("created_at >= updated_at")
     @emails_opened_today = 1
