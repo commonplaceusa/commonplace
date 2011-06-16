9305a8d18d34cee0065dc37fea1023e41e37736b
Jason Berlinsky <jberlinsky@gmail.com>
2011-06-16 01:10:39 -0400

    Modified Facebook jump page to recognize different environments

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -4,7 +4,7 @@ class FacebookCanvasController < ApplicationController
   
   def index
     @request_id = params[:request_ids]
-    url = "https://graph.facebook.com/oauth/access_token?&client_id=179741908724938&client_secret=4d1d96dc9b402ca6779f77bc9e88b89a&grant_type=client_credentials"
+    url = "https://graph.facebook.com/oauth/access_token?&client_id=#{$FacebookConfig['app_id']}&client_secret=#{$FacebookConfig['app_secret']}&grant_type=client_credentials"
     @application_token = open(url).read.gsub("access_token=","")
     render :layout => nil
   end
