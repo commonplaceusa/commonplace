79ce31de4ad6630d4ae97c375109e8f2d727b23b
Jason Berlinsky <jberlinsky@gmail.com>
2011-06-16 02:00:47 -0400

    Added diagnostic output

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -5,6 +5,7 @@ class FacebookCanvasController < ApplicationController
   def index
     @request_id = params[:request_ids]
     url = "https://graph.facebook.com/oauth/access_token?&client_id=#{$FacebookConfig['app_id']}&client_secret=#{$FacebookConfig['app_secret']}&grant_type=client_credentials"
+    puts url
     @application_token = open(url).read.gsub("access_token=","")
     render :layout => nil
   end
