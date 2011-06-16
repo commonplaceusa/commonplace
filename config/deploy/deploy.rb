5820b094fa50ddd57fffaac9c4c5140a6c1e70f0
Jason Berlinsky <jberlinsky@gmail.com>
2011-06-16 02:40:00 -0400

    Using rails_env

--- config/deploy/deploy.rb
+++ config/deploy/deploy.rb
@@ -8,12 +8,17 @@ namespace :deploy do
   task :symlink do
   end
 
+  desc "Flush the Rails cache"
+  task :flush do
+    run "cd #{current_path} ; bundle exec rake cache:clear"
+  end
+
   task :update_code do
     commands = ["cd #{current_path}",
                 "git fetch origin",
                 "git reset --hard #{branch}",
                 "git submodule update --init",
-                "bundle exec rake cache:clear"]
+                "bundle exec rake cache:clear RAILS_ENV=#{rails_env}"]
     run commands.join("; ")
   end
   
