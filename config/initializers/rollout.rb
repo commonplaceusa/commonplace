6dddf99197f56e3d5b377ec8da7dc5ec6893b5aa
Jason Berlinsky <jberlinsky@gmail.com>
2011-07-04 12:25:31 -0400

    Activate alpha group for localhost testing

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -3,7 +3,7 @@ uri = URI.parse(ENV["REDISTOGO_URL"] || "localhost:6379")
 $rollout = Rollout.new(Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true))
 
 $rollout.define_group(:alpha) do |user|
-  user.community.slug == "CommonPlace"
+  user.community.slug == "CommonPlace" or user.community.slug == "test"
 end
 
 $rollout.define_group(:harrisonburg) do |user|
