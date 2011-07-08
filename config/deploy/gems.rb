303b2c7f126245a715dba9f26f70d32135897101
Max Tilford <maxtilford@gmail.com>
2011-07-08 02:12:45 -0700

    Spring Cleaning: removes dead files
    * in config
    * in lib
    * in app/controllers (possibly incomplete)
    * in app/views (incomplete)

--- config/deploy/gems.rb
+++
@@ -1,7 +0,0 @@
-namespace :gems do
-  task :install, :roles => :app do
-    # For when we use bundler
-    run "cd #{current_path} && bundle install --deployment --binstubs"
-  end
-end
-after "deploy:update_code", "gems:install"
