57c8549d3318b61d54b2e4aa078a9300ef321bc4
Max Tilford <maxtilford@gmail.com>
2011-07-09 15:28:20 -0700

    Revert "Changes to invitation page"
    This reverts commit 89e9ce83955524ab3797689ce5a43a63b9f18362.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -144,7 +144,11 @@ class AccountsController < CommunitiesController
 
   def subscribe_to_groups
     current_user.group_ids = params[:group_ids]
-    redirect_to root_url
+    if $rollout.active?(:facebook_invite, current_user)
+      redirect_to :action => "facebook_invite"
+    else
+      redirect_to root_url
+    end
   end
 
   def update
