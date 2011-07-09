61a0f731a767b00ada98c0f6794032fa0ec5d8c8
Max Tilford <maxtilford@gmail.com>
2011-07-09 15:47:13 -0700

    brings back reverted commits without jquery.validation
    * "Changes to invitatation page"
    * "Javascript to correct invitation message"
    * "And add "Post to Twitter" and "Post to Facebook" to invitations"

---
+++ public/javascripts/facebook.js
@@ -0,0 +1,59 @@
+function fbEnsureInit(callback) {
+    if(!window.fbApiInit) {
+        console.log("FB Not Initialized");
+        setTimeout(function() {fbEnsureInit(callback);}, 50);
+    } else {
+        console.log("FB initialized");
+        if(callback) {
+            callback();
+        }
+    }
+}
+
+
+function facebook_pass(session) {
+    return session.uid;
+}
+
+function facebook_share() {
+  u=location.href;
+  t=document.title;
+  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
+  return false;
+}
+
+function facebook_invite() {
+    FB.ui({
+        method: 'apprequests',
+        message: 'Invitation Text Here',
+        data: 'tracking_data'
+      });
+}
+
+function inviteLoginCallback(fb_message, fb_slug) {
+    console.log("Logged in successfully");
+    FB.ui({
+        method: 'apprequests',
+        message: fb_message,
+        data: fb_slug,
+        display: 'iframe'
+    });
+}
+
+function invite_friends(fb_message, fb_slug) {
+    console.log("Inviting friends...");
+    fbEnsureInit(
+        function(){
+            FB.login(
+                function() {
+                    inviteLoginCallback(
+                        fb_message,
+                        fb_slug
+                    );
+                },
+                {perms:'read_stream,publish_stream,offline_access'}
+            );
+        }
+    );
+}
+
