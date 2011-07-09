85133ede3343dafb2d6a0d8c1291779cc985dc34
Max Tilford <maxtilford@gmail.com>
2011-07-09 15:28:20 -0700

    Revert "Changes to invitation page"
    This reverts commit 1fa2a21eaf637c4f7f35796cbea19975d4b58829.

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,1 +1,1 @@
-$FacebookConfig = {"app_id" => ENV["facebook_app_id"] || "12345", "app_secret" => ENV["facebook_app_secret"]}
+$FacebookConfig = {"app_id" => ENV["facebook_app_id"], "app_secret" => ENV["facebook_app_secret"]}
