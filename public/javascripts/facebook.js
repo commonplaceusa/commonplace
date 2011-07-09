9249602a1318d27cd4962c8c1aa94786c166b0a3
Max Tilford <maxtilford@gmail.com>
2011-07-09 15:28:17 -0700

    Revert "Add "Post to Twitter" and "Post to Facebook" to invitations"
    This reverts commit 7d4f70f32db716c3d638749ddc85ae29de566b71.

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -15,13 +15,6 @@ function facebook_pass(session) {
     return session.uid;
 }
 
-function facebook_share() {
-  u=location.href;
-  t=document.title;
-  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');
-  return false;
-}
-
 function facebook_invite() {
     FB.ui({
         method: 'apprequests',
