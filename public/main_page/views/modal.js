594d2e0ee736c51e71aa7bb1e3e5c54b0c89a997
Jason Berlinsky <jberlinsky@gmail.com>
2011-07-16 14:00:51 -0400

    Add modal view for post editing, and generic modal caller. Also disables avatars locally.

--- public/main_page/views/modal.js
+++ public/main_page/views/modal.js
@@ -11,15 +11,12 @@ CommonPlace.NewMessage = Backbone.View.extend({
   render: function() {
     var self = this;
     if (CommonPlace.community.users.length === 0 && !this.options.loading) {
-        console.log("Fetching users...");
         CommonPlace.community.users.fetch(
             {success: function() {
                                      new CommonPlace.NewMessage({person_id: self.options.person_id, loading: true}).render();
             }});
         return;
     }
-    console.log("Got " + CommonPlace.community.users.length + " users");
-    console.log("Getting info for user " + this.options.person_id);
     $(this.el).addClass("not_empty");
     $(this.el).append('<div id="modal-overlay"></div>');
     $(this.el).append('<div id="modal-content"></div>');
@@ -40,4 +37,35 @@ CommonPlace.NewMessage = Backbone.View.extend({
     _.delay(function(){CommonPlace.app.notify("Your message has been sent.");},200);
   }
 
+});
+
+CommonPlace.EditView = Backbone.View.extend({
+  id: "modal",
+  tagname: "div",
+
+  events: { 
+    "click #modal-close": "remove"
+    //"submit form#edit_" + this.options.model_type: "submitEdit"
+  },
+
+  render: function() {
+    $(this.el).addClass("not_empty");
+    $(this.el).append('<div id="modal-overlay"></div>');
+    $(this.el).append('<div id="modal-content"></div>');
+    this.$("#modal-content").append('<img src="/images/modal-close.png" id="modal-close">');
+    this.$("#modal-content").append(CommonPlace.render("edit_" + this.options.model_type + "_form", {model: this.options.model}));
+    $("#main").append(this.el);
+    $(window).trigger('resize.modal');
+    return this;
+  },
+
+  submitMessage: function(e) {
+    e.preventDefault();
+    $.post("/api/people/" + this.options.person_id + "/messages",
+           JSON.stringify({ subject: this.$("form input#message_subject").val(),
+                            body: this.$("form textarea#message_body").val() }),
+           function() {});
+    this.remove();
+    _.delay(function(){CommonPlace.app.notify("Your " + this.options.model_type + " has been edited.");},200);
+  }
 });
