3e9bf728a97b19d05025cc9e647a115575f40799
Jason Berlinsky <jberlinsky@gmail.com>
2011-08-04 14:02:29 -0400

    Make college localization less hacky using a database field

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -14,11 +14,4 @@ $rollout.define_group(:fallschurch) do |user|
   user.community.slug == "fallschurch" 
 end
 
-$rollout.define_group(:umw) do |user|
-  user.community.slug == "Vienna"
-end
-
-# TODO: UMW should be a subgroup of College
-$rollout.activate_group(:custom_locale, :umw)
-
 $rollout.activate_group(:facebook_invite, :alpha)
