0625c18a273c43a4635acf1f5c2c9b410c229b5e
Max Tilford <maxtilford@gmail.com>
2011-08-04 21:04:02 -0700

    add support for client-side localization, and add a college locale

---
+++ public/main_page/text/college.js
@@ -0,0 +1,61 @@
+CommonPlace.text = CommonPlace.text || {};
+CommonPlace.text.college = { 
+  "eventinfo": {
+  },
+  "replies": {},
+  "edit_post_form": {},
+  "group": {},
+  "event": {},
+  "feed": {},
+  "post_form": {
+    "h2": "Say something to your fellow students"
+  },
+  "edit_event_form": {},
+  "edit_announcement_form": {},
+  "post_tour": {
+    "p1": "Here's where you go whenever you want to share with the campus.",
+
+    "ex1": "\"Post to your hall\" to share requests, goods, recommendations, questions, updates, etc. with your fellow students, like \"I need to borrow a ladder,\" \"I lost my cat,\" or \"Anybody know a good babysitter?\"",
+
+    "ex2": "\"Publicize an announcement\" to advertise things to the whole campus, like \"upcoming volunteer drive\" or \"Free tacos this Thursday at our store.\"",
+
+    "ex3": "\"Post an Event\" to announce an upcoming event to the campus",
+
+    "ex4": "\"Post to a Discussion Group\" to share specific information with interest groups in your town -- dog lovers, moms, sports, and more",
+
+    "p2": "Get a feel for how it works by saying \"Hi!\" to your fellow students."
+  },
+  "user": {},
+  "group_post_form": {},
+  "wire_tour": {
+    "p1": "Here's the CommonPlace \"Campus Wire.\" Here you'll find needs, events and announcements written by your fellow students and local organizations in town."
+  },
+  "post_confirmation_form": {},
+  "groupinfo": {},
+  "say_something_nav": {},
+  "group_post": {},
+  "index_nav": {},
+  "announcement_form": {
+    "h2": "Send an announcement to your whole campus"
+  },
+  "announcement": {},
+  "delete_confirmation": {},
+  "message_form": {},
+  "feedinfo": {},
+  "profile_tour": {
+    "p1": "This is your \"civic profile.\" Your fellow students will see this profile when you post to the site. You can always add to it by clicking \"edit\" in the top right-hand corner."
+  },
+  "wire": {},
+  "post": {},
+  "edit_group_post_form": {},
+  "tour_welcome": {
+    "use1": "Share needs, announcements and events with the town",
+    "use2": "Meet your fellow students and find great local organizations",
+    "use3": "Discover ways to get involved in the campus"
+  },
+  "userinfo": {},
+  "index": {},
+  "event_form": {
+    "h2": "Post an event to your campus"
+  }
+};
