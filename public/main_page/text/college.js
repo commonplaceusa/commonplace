17962c7ec679d31478437108a4b63c347fc3806b
pdavis <pdavis@ubuntu.ubuntu-domain>
2011-08-07 17:35:39 -0400

    Pete made changes to our codebase!?!??!?!?! But seriously...text changes

--- public/main_page/text/college.js
+++ public/main_page/text/college.js
@@ -13,49 +13,50 @@ CommonPlace.text.college = {
   "edit_event_form": {},
   "edit_announcement_form": {},
   "post_tour": {
-    "p1": "Here's where you go whenever you want to share with the campus.",
+    "p1": "Here's where you go whenever you want to share with the campus community.",
 
-    "ex1": "\"Post to your hall\" to share requests, goods, recommendations, questions, updates, etc. with your fellow students, like \"I need to borrow a ladder,\" \"I lost my cat,\" or \"Anybody know a good babysitter?\"",
+    "ex1": "\"Post to your hall\" to share requests, goods, recommendations, questions, updates, etc. with your fellow students, like \"I need to borrow a stapler,\" \"I lost phone, has anyone seen it?\" or \"I made too many brownies and there's extra's free for the taking in the common room.\"",
 
-    "ex2": "\"Publicize an announcement\" to advertise things to the whole campus, like \"upcoming volunteer drive\" or \"Free tacos this Thursday at our store.\"",
+    "ex2": "\"Publicize an announcement\" to advertise things to the whole campus, like \"Upcoming volunteer drive\" or \"Free t-shirts being given out by my club this week.\"",
 
     "ex3": "\"Post an Event\" to announce an upcoming event to the campus",
 
-    "ex4": "\"Post to a Discussion Group\" to share specific information with interest groups in your town -- dog lovers, moms, sports, and more",
+    "ex4": "\"Post to a Discussion Group\" to share specific information with those interested on campus -- sports, environment, dorm life, academic, and more",
 
-    "p2": "Get a feel for how it works by saying \"Hi!\" to your fellow students."
+    "p2": "Get a feel for how it works by saying \"Hi!\" to your fellow dormmates."
   },
   "user": {},
   "group_post_form": {},
   "wire_tour": {
-    "p1": "Here's the CommonPlace \"Campus Wire.\" Here you'll find needs, events and announcements written by your fellow students and local organizations in town."
+    "p1": "Here's the CommonPlace \"Campus Wire.\" Here you'll find needs, events and announcements written by your fellow students and local organizations on campus."
   },
   "post_confirmation_form": {},
+
   "groupinfo": {},
   "say_something_nav": {},
   "group_post": {},
   "index_nav": {},
   "announcement_form": {
-    "h2": "Send an announcement to your whole campus"
+    "h2": "Send an announcement to the whole campus"
   },
   "announcement": {},
   "delete_confirmation": {},
   "message_form": {},
   "feedinfo": {},
   "profile_tour": {
-    "p1": "This is your \"civic profile.\" Your fellow students will see this profile when you post to the site. You can always add to it by clicking \"edit\" in the top right-hand corner."
+    "p1": "This is your \"campus profile.\" Your fellow students will see this profile when you post to the site. You can always add to it by clicking \"edit\" in the top right-hand corner."
   },
   "wire": {},
   "post": {},
   "edit_group_post_form": {},
   "tour_welcome": {
-    "use1": "Share needs, announcements and events with the town",
-    "use2": "Meet your fellow students and find great local organizations",
-    "use3": "Discover ways to get involved in the campus"
+    "use1": "Share needs, announcements and events with campus",
+    "use2": "Meet your fellow students and find great campus organizations",
+    "use3": "Discover ways to get involved with the campus community"
   },
   "userinfo": {},
   "index": {},
   "event_form": {
-    "h2": "Post an event to your campus"
+    "h2": "Post an event to the campus community"
   }
 };
