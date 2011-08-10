e9e4a062a18faa552dfd48d121e5427159c849fc
Jason Berlinsky <jberlinsky@gmail.com>
2011-08-10 16:35:10 -0400

    User input for organizer application

--- app/controllers/organizer_controller.rb
+++ app/controllers/organizer_controller.rb
@@ -4,4 +4,16 @@ class OrganizerController < ApplicationController
     @data_points = current_community.organizers.map(&:organizer_data_points).flatten.select { |p| p.present? }
     @center_zip_code = current_community.zip_code
   end
+
+  def add
+    @data_point = OrganizerDataPoint.new
+    @data_point.organizer_id = current_user.id
+
+    @data_point.address = params[:address]
+    @data_point.status = params[:status]
+
+    @data_point.save
+    redirect_to :action => :map
+  end
+
 end
