c4881bace1dabfe6fa88353158de041acb5fb662
Jason Berlinsky <jberlinsky@gmail.com>
2011-08-16 12:07:17 -0400

    Revert "Rotate between two Google Maps API keys"
    This reverts commit 52273b7c4fede869441cf0f69acbeb9bf793e728.

--- app/helpers/organizer_helper.rb
+++ app/helpers/organizer_helper.rb
@@ -1,7 +1,2 @@
 module OrganizerHelper
-  def google_maps_api_key
-    [
-      'ABQIAAAAgdPfd74v_0Ttup6W0tJHtxRNtGYSdxHAX2hqsWz4YGYENJAGdhSzZ0AeIOf9BGJ4oPgPhrzNInJrQQ',
-      'ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSosDVG8KKPE1-m51RBrvYughuyMxQ-i1QfUnH94QxWIa6N4U6MouMmBA'
-    ].shuffle.first
 end
