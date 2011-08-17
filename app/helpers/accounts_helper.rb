a586aa733dd0aa1d7c088f81f32f6a5b4b2aef02
Jason Berlinsky <jberlinsky@gmail.com>
2011-08-17 17:14:51 -0400

    Selector fixes

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,7 +2,7 @@ module AccountsHelper
   def college_dorms_for_school(community)
     # HACK
     if community.is_college
-      ['Select your residence hall', '-----', community.neighborhoods.map(&:name)].flatten
+      options_for_select(['Select your residence hall', '-----', community.neighborhoods.map(&:name)].flatten, 'Select your residence hall')
     else
       []
     end
