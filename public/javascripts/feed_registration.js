667f5842f901d6fc7249fb133e120691979ba5a7
Max Tilford <maxtilford@gmail.com>
2011-09-02 11:50:21 -0700

    add avatar cropping to feed registration

--- public/javascripts/feed_registration.js
+++ public/javascripts/feed_registration.js
@@ -8,4 +8,23 @@ $(function() {
   $('#feed_avatar').change(function() {
     $("#file_input_fix input").val($(this).val().replace(/^.*\\/,""));
   });
+
+
+  // Avatar crop
+  var updateCrop = function(coords) {
+    $("#crop_x").val(coords.x);
+    $("#crop_y").val(coords.y);
+    $("#crop_w").val(coords.w);
+    $("#crop_h").val(coords.h);
+  };
+
+  $("form.crop").load(function() {
+    $("form.crop").css({ width: Math.max($("#cropbox").width(), 420) });
+  });
+
+  $("#cropbox").Jcrop({
+    onChange: updateCrop,
+    onSelect: updateCrop,
+    aspectRatio: 1.0
+  });
 });
