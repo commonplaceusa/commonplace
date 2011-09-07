38f40782af4860e4cde49eadc629ff96f05b709c
John Poje <jpojecp@gmail.com>
2011-09-06 17:05:11 -0700

    prettier code for modal dialogs

--- public/shared/views.js
+++ public/shared/views.js
@@ -45,7 +45,21 @@ var EventItemView = CommonPlace.View.extend({
   body: function() { return this.model.get('body'); },
 
   monthAbbrevs: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-                 "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+                 "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"],
+
+  events: {
+    "click a": "editEvent",
+    "change": "render"
+  },
+
+  editEvent: function(e) {
+    e && e.preventDefault();
+    var formview = new EventFormView({
+      model: this.model,
+      template: "feed_page/feed-event-edit-form"
+    });
+    formview.render();
+  }
 
 });
 
@@ -79,8 +93,162 @@ var AnnouncementItemView = CommonPlace.View.extend({
   
   author: function() { return this.model.get('author'); },
   
-  body: function() { return this.model.get('body'); }
+  body: function() { return this.model.get('body'); },
+  
+  events: {
+    "click a": "editAnnouncement",
+    "change": "render"
+  },
 
+  editAnnouncement: function(e) {
+    e && e.preventDefault();
+    var formview = new AnnouncementFormView({
+      model: this.model,
+      template: "feed_page/feed-edit-form"
+    });
+    formview.render();
+  }
+});
+
+var FormView = CommonPlace.View.extend({
+  initialize: function(options) {
+    this.template = this.options.template;
+    this.modal = new ModalView({form: this.el});
+    this.model.bind("create", this.modal.exit, this.modal);
+  },
+
+  afterRender: function() {
+    this.modal.render();
+  },
+
+  events: {
+    "click form a.cancel": "exit",
+    "submit form": "send"
+  },
+
+  send: function(e) {
+    e.preventDefault();
+    this.save();
+  },
+
+  exit: function(e) {
+    e && e.preventDefault();
+    this.modal.exit();
+  }
+});
+
+var MessageFormView = FormView.extend({
+  save: function() {
+    this.model.save({
+      subject: this.$("[name=subject]").val(),
+      body: this.$("[name=body]").val()
+    });
+  }
+});
+
+var AnnouncementFormView = FormView.extend({
+  save: function() {
+    this.model.save({
+      title: this.$("[name=title]").val(),
+      body: this.$("[name=body]").val()
+    });
+  },
+
+  title: function() {
+    return this.model.get("title");
+  },
+
+  body: function() {
+    return this.model.get("body");
+  }
+});
+
+var EventFormView = FormView.extend({
+  save: function() {
+    this.model.save({
+      title: this.$("[name=title]").val(),
+      about: this.$("[name=about]").val(),
+      date: this.$("[name=date]").val(),
+      start: this.$("[name=start]").val(),
+      end: this.$("[name=end]").val(),
+      venue: this.$("[name=venue]").val(),
+      address: this.$("[name=address]").val()
+    });
+  },
+
+  title: function() {
+window.ev = this.model;
+    return this.model.get("title");
+  },
+
+  body: function() {
+    return this.model.get("body");
+  },
+
+  date: function() {
+    return this.model.get("occurs_at");
+  },
+
+  venue: function() {
+    console.log("hi");
+    return this.model.get("venue");
+  },
+
+  address: function() {
+    return this.model.get("address");
+  },
+
+  time_values: function() {
+    var start_value = this.model.get("starts_at");
+    var end_value = this.model.get("ends_at");
+    return _.flatten(_.map(["AM", "PM"],
+      function(half) {
+        return  _.map(_.range(1,13),
+        function(hour) {
+          return _.map(["00", "30"],
+          function(minute) {
+            return String(hour) + ":" + minute + " " + half;
+          });
+        });
+      })
+    );
+  }
+});
+
+var ModalView = CommonPlace.View.extend({
+  template: "feed_page/feed-modal",
+  className: "feed-modal",
+
+  initialize: function(options) {
+    var self = this;
+    this.form = this.options.form;
+  },
+
+  afterRender: function() {
+    $("body").append(this.el);
+    $(".modal-container").append(this.form);
+    this.$("textarea").autoResize();
+    this.centerEl();
+  },
+
+  centerEl: function() {
+    var $el = $(".modal-container");
+    var $window = $(window);
+    
+   
+    $el.css({
+      top: ($window.height() - $el.height()) /2,
+      left: ($window.width() - $el.width()) /2
+    });
+  },
+
+  events: {
+    "click #modal-shadow": "exit"
+  },
+
+  exit: function() {
+    $(this.el).remove();
+  }
 });
 
 var RepliesView = CommonPlace.View.extend({
