23fd00df4d8f08c8e0b7c82da8557018a9a5b317
John Poje <jpojecp@gmail.com>
2011-09-08 13:16:18 -0700

    feed welcome email

---
+++ mail/feed_welcome.rb
@@ -0,0 +1,57 @@
+class FeedWelcome < MailBase
+
+  def initialize(user_id, feed_id)
+    @user = User.find(user_id)
+    @community = user.community
+    @feed = Feed.find(feed_id)
+  end
+
+  def user
+    @user
+  end
+
+  def community
+    @community
+  end
+
+  def feed
+    @feed
+  end
+
+  def town_name
+    community.name
+  end
+
+  def short_user_name
+    user.first_name
+  end
+
+  def feed_name
+    feed.name
+  end
+
+  def feed_url
+    url("/pages/#{feed.slug}")
+  end
+
+  def feed_email_address
+    "#{feed.slug}@CommonPlace.com"
+  end
+
+  def organizer_email
+    community.organizer_email
+  end
+
+  def organizer_name
+    community.organizer_name
+  end
+
+  def subject
+    "Your {feed_name} feed on The {town_name} CommonPlace"
+  end
+
+  def tag
+    "feed_welcome"
+  end
+
+end
