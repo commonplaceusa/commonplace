e766211b87ff8748e7276520c36f90a7b2b6196e
Max Tilford <maxtilford@gmail.com>
2011-09-13 11:43:18 -0700

    use dalli_store for Rails cache in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,6 +3,8 @@ Commonplace::Application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.cache_classes = true
+
+  config.cache_store = :dalli_store
   
   # Full error reports are disabled and caching is turned on
   config.action_controller.consider_all_requests_local = false
