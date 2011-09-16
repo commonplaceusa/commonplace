3ef84ad70606cdc23b5e18511a7103ecf0e021bd
Max Tilford <maxtilford@gmail.com>
2011-09-16 10:02:01 -0700

    Devise and ActiveAdmin cause rake assets:precompile to fail on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 
 Commonplace::Application.routes.draw do
 
-  ActiveAdmin.routes(self)
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  
-  devise_for :users, :controllers => { 
-    :sessions => "user_sessions",
-    :passwords => "password_resets"
-  }
+  begin 
+    ActiveAdmin.routes(self) 
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    
+    devise_for :users, :controllers => { 
+      :sessions => "user_sessions",
+      :passwords => "password_resets"
+    }
+  rescue
+    Rails.logger.warn "ActiveAdmin routes not initialized"
+    Rails.logger.warn "Devise routes not initialized"
+    # ActiveAdmin and Devise try to hit the database on initialization.
+    # That fails when Heroku is compiling assets, so we catch the error here.
+  end
 
   get "facebook_canvas/index"
   match "/facebook_canvas/" => "facebook_canvas#index"
