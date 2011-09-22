058e1e8c670647c7ddf29387008e67b34b369fdf
John Poje <jpojecp@gmail.com>
2011-09-22 15:32:39 -0700

    added info-box profile views
    * added FeedProfileBox
    * added UserProfileBox
    * added GroupProfileBox

---
+++ app/javascripts/info_boxes/feed_profile.js
@@ -0,0 +1,26 @@
+var FeedProfileBox = Profile.extend({
+  template: "main_page/profiles/feed-profile",
+  className: "profile",
+
+  events: {
+    "click button.message": "showMessageForm"
+  },
+
+  avatarUrl: function() { return this.model.get('avatar_url'); },
+  
+  fullName: function() { return this.model.get("name"); },
+  
+  shortName: function() { return this.model.get("first_name"); },
+  
+  about: function() { return this.model.get('about'); },
+  
+  groups: function() { return ""; },
+
+  showMessageForm: function() {
+    var formView = new MessageFormView({
+      model: new Message({ messagable: this.model })
+    });
+    formView.render();
+  }
+  
+});
