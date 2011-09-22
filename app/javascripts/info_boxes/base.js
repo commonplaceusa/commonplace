94df828b6e8d0ccb080cebeaae187b0e51f637a3
John Poje <jpojecp@gmail.com>
2011-09-22 15:35:09 -0700

    added info-box to main page
    * info-box is now static
    * it contains a profile that changes on mousing over a wire item
    * the profile can show a user, a feed, or a group

--- app/javascripts/info_boxes/base.js
+++ app/javascripts/info_boxes/base.js
@@ -1,5 +1,10 @@
 
 var InfoBox = CommonPlace.View.extend({
   id: "info-box",
+  template: "main_page/info-box"
+});
+
+var Profile = CommonPlace.View.extend({
+  id: "profile",
   unbind: function() {}
 });
