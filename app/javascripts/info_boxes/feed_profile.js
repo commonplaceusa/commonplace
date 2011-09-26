c73017c6af74867c15838d369827ca7ba10f6427
John Poje <jpojecp@gmail.com>
2011-09-26 12:56:25 -0700

    messaging users/feeds and prettier lists from their profiles

--- app/javascripts/info_boxes/feed_profile.js
+++ app/javascripts/info_boxes/feed_profile.js
@@ -3,7 +3,7 @@ var FeedProfileBox = Profile.extend({
   className: "profile",
 
   events: {
-    "click button.message": "showMessageForm"
+    "click .message": "showMessageForm"
   },
 
   avatarUrl: function() { return this.model.get('avatar_url'); },
@@ -16,7 +16,8 @@ var FeedProfileBox = Profile.extend({
   
   groups: function() { return ""; },
 
-  showMessageForm: function() {
+  showMessageForm: function(e) {
+    e.preventDefault();
     var formView = new MessageFormView({
       model: new Message({ messagable: this.model })
     });
