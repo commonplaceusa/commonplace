3ecff1e558da017af7c4b400573bf57ffd725d6e
Jason Berlinsky <jberlinsky@gmail.com>
2011-09-26 15:52:38 -0400

    Move data point adding to the API

--- app/controllers/organizer_controller.rb
+++ app/controllers/organizer_controller.rb
@@ -8,45 +8,7 @@ class OrganizerController < ApplicationController
   end
 
   def add
-    @data_point = OrganizerDataPoint.new
-    @data_point.organizer_id = current_user.id
-
-    num = params[:number]
-    if num.include? "-"
-      # Needs to be parsed
-      odds = false
-      evens = false
-      all = true
-
-      if num.include? "O"
-        puts "Odd"
-        odds = true
-        all = false
-        num = num.gsub("O", "")
-      elsif num.include? "E"
-        puts "Even"
-        evens = true
-        all = false
-        num = num.gsub("E", "")
-      end
-
-      range = num.split("-")
-      (range[0].to_i..range[1].to_i).each do |n|
-        if (odds and (n % 2 == 1)) or (evens and (n % 2 == 0)) or all
-          data_point = OrganizerDataPoint.new
-          data_point.organizer_id = current_user.id
-          data_point.address = "#{n} #{params[:address]}"
-          data_point.status = params[:status]
-          data_point.save
-        end
-      end
-    else
-      data_point = OrganizerDataPoint.new
-      data_point.organizer_id = current_user.id
-      data_point.address = params[:address]
-      data_point.status = params[:status]
-      data_point.save
-    end
+    cp_client.add_data_point(current_community,params)
     redirect_to :action => :map
   end
 
