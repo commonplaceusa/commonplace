c3b1347a52e9b8293181493b9f81196a4374a830
Jason Berlinsky <jberlinsky@gmail.com>
2011-09-26 02:28:52 -0400

    Add support for staging for Google Maps

--- app/helpers/organizer_helper.rb
+++ app/helpers/organizer_helper.rb
@@ -1,5 +1,9 @@
 module OrganizerHelper
   def google_api_key()
-    ['ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQB4B98GOFQoxUPdFn0mOBvUSbzOxSwgIAcuwJ7Sux0NCxmE-bx8bLihQ','ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSosDVG8KKPE1-m51RBrvYughuyMxQ-i1QfUnH94QxWIa6N4U6MouMmBA','ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQB4B98GOFQoxUPdFn0mOBvUSbzOxSwgIAcuwJ7Sux0NCxmE-bx8bLihQ'].shuffle.first
+    if ENV['HEROKU_APP'] == 'commonplace'
+      ['ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQB4B98GOFQoxUPdFn0mOBvUSbzOxSwgIAcuwJ7Sux0NCxmE-bx8bLihQ','ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSosDVG8KKPE1-m51RBrvYughuyMxQ-i1QfUnH94QxWIa6N4U6MouMmBA','ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQB4B98GOFQoxUPdFn0mOBvUSbzOxSwgIAcuwJ7Sux0NCxmE-bx8bLihQ'].shuffle.first
+    elsif ENV['HEROKU_APP'] == 'commonplace-staging'
+      'ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQELo9uXCeQpJK50AiwyAkJeaU-fRRR3MsbGRGtnu2SA_mAgRo2lSZC5A'
+    end
   end
 end
