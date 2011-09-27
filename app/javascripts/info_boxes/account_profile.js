661ec364540007219741e0c89b04a3cf7c6be17e
John Poje <jpojecp@gmail.com>
2011-09-27 14:47:09 -0700

    removed 'goods' from account/user profiles

--- app/javascripts/info_boxes/account_profile.js
+++ app/javascripts/info_boxes/account_profile.js
@@ -18,19 +18,15 @@ var AccountProfileBox = Profile.extend({
 
   interests: function() { return _.map(this.model.get('interests'), this.comma); },
 
-  skills: function() { return ["climbing", " falling"]; },
-
-  offers: function() { return _.map(this.model.get("offers"), this.comma); },
+  skills: function() { return _.map(this.model.get("offers"), this.comma); },
 
   subscriptions: function() { return this.model.get('subscriptions'); },
   
   groups: function() { return ""; },
 
   hasInterests: function() { return this.model.get("interests").length > 0; },
- 
-  hasSkills: function() { return true; },
 
-  hasOffers: function() { return this.model.get("offers").length > 0; },
+  hasSkills: function() { return this.model.get("offers").length > 0; },
 
   hasAbout: function() { return this.model.get("about") != undefined; }
   
