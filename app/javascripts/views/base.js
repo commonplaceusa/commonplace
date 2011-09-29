e6388054c18786fe0503781025b5f188806d15c5
Max Tilford <maxtilford@gmail.com>
2011-09-28 22:01:39 -0700

    moves FormView into views/base.js

--- app/javascripts/views/base.js
+++ app/javascripts/views/base.js
@@ -59,3 +59,37 @@ CommonPlace.View = Backbone.View.extend({
 });
 
 
+
+var FormView = CommonPlace.View.extend({
+  initialize: function(options) {
+    this.template = (this.options.template || this.template);
+    this.modal = new ModalView({form: this.el});
+  },
+
+  afterRender: function() {
+    this.modal.render();
+  },
+
+  events: {
+    "click form a.cancel": "exit",
+    "click form a.delete": "deletePost",
+    "submit form": "send"
+  },
+
+  send: function(e) {
+    e.preventDefault();
+    this.save();
+    this.exit();
+  },
+
+  exit: function(e) {
+    e && e.preventDefault();
+    this.modal.exit();
+  },
+
+  deletePost: function(e) {
+    e && e.preventDefault();
+    var self = this;
+    this.remove(function() { self.modal.exit(); });
+  }
+});
