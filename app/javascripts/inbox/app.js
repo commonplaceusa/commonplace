d7115b1cdf9024d64d962e814384ce3017a9cb29
John Poje <jpojecp@gmail.com>
2011-09-30 15:12:05 -0700

    inbox page can now switch between sent and received messages

--- app/javascripts/inbox/app.js
+++ app/javascripts/inbox/app.js
@@ -1,19 +1,29 @@
 
 var InboxRouter = Backbone.Router.extend({
   routes: {
-    "/inbox": "show"
+    "": "showReceived",
+    "received": "showReceived",
+    "sent": "showSent"
   },
 
   initialize: function(options) {
     this.account = options.account;
     this.community = options.community;
-    this.show();
   },
 
-  show: function() {
-    var inboxview = new InboxView({ account: this.account, community: this.community });
+  show: function(type) {
+    var inboxnav = new InboxNavView({ account: this.account, community: this.community });
+    var inboxview = new InboxView({ account: this.account, community: this.community, type: type });
+    inboxnav.render();
     inboxview.render();
+    $("#nav").replaceWith(inboxnav.el);
     $("#inbox").replaceWith(inboxview.el);
-  }
+    $(".nav-tab").removeClass("current");
+    $("." + type).addClass("current");
+  },
+
+  showReceived: function() { this.show("received"); },
+
+  showSent: function() { this.show("sent"); }
 });
 
