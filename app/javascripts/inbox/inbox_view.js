3f04559fde3f4d321110dd28d41016bedb1ec81f
John Poje <jpojecp@gmail.com>
2011-09-29 18:12:33 -0700

    added message inbox page

---
+++ app/javascripts/inbox/inbox_view.js
@@ -0,0 +1,26 @@
+
+var InboxView = CommonPlace.View.extend({
+  template: "inbox/inbox",
+  id: "inbox",
+
+  initialize: function(options) {
+    this.account = options.account;
+    this.community = options.community;
+
+    var self = this;
+    var messages = new Messages([], { uri: "/users/" + this.account.id + "/messages" });
+
+    messages.fetch({
+      success: function(collection) {
+        var listview = new MessageWire({
+          account: self.account,
+          community: self.community,
+          collection: collection
+        });
+        listview.render();
+        self.$("#message-list").replaceWith(listview.el);
+      }
+    });
+  }
+});
+
