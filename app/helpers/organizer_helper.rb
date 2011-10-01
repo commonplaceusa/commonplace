c8484f39afe0d618fb1759ccccb4a24e2887dbb4
Jason Berlinsky <jberlinsky@gmail.com>
2011-10-01 18:32:12 -0400

    Change the Google Maps key

--- app/helpers/organizer_helper.rb
+++ app/helpers/organizer_helper.rb
@@ -3,7 +3,7 @@ module OrganizerHelper
     if ENV['HEROKU_APP'] == 'commonplace'
       ['ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQB4B98GOFQoxUPdFn0mOBvUSbzOxSwgIAcuwJ7Sux0NCxmE-bx8bLihQ','ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSosDVG8KKPE1-m51RBrvYughuyMxQ-i1QfUnH94QxWIa6N4U6MouMmBA','ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQB4B98GOFQoxUPdFn0mOBvUSbzOxSwgIAcuwJ7Sux0NCxmE-bx8bLihQ'].shuffle.first
     elsif ENV['HEROKU_APP'] == 'commonplace-staging'
-      'ABQIAAAAgdPfd74v_0Ttup6W0tJHtxQELo9uXCeQpJK50AiwyAkJeaU-fRRR3MsbGRGtnu2SA_mAgRo2lSZC5A'
+      'ABQIAAAAgdPfd74v_0Ttup6W0tJHtxSeiCNuZ_fCHJEwH48648-0ULan5RRVuO5xxDLGQXIjDZFCu8_6tlc8HQ'
     else
       'unknown_env_key'
     end
