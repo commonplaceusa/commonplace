c25dd913a084a3c95bb9bdfb22acd4afc43c85de
Max Tilford <maxtilford@gmail.com>
2011-10-03 08:38:57 -0700

    Removes old code:
    * RepliesController is no longer needed
    * reply and item haml views are no longer needed
    * helpers:
      * FeedsHelper isn't used
      * MessagesHelper isn't used
      * MapHelper isn't used
      * TextHelper isn't used
      * TimeHelper isn't used
      * UpdateHelper isn't used
      * ApplicationHelper isn't used

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,32 +1,5 @@
  module ApplicationHelper
    
-   def creation_feeds_for(user)
-     ActiveSupport::OrderedHash.new.tap do |collection|
-       ([ user ] + user.managable_feeds.to_a).each do |f|
-         collection[f.name] = dom_id(f)
-       end
-     end
-   end
-
-   def creation_groups_for(community)
-     ActiveSupport::OrderedHash[community.groups.map {|g| [g.name, g.id]}]
-   end
-
-   def html_to_json(&block)
-     @template_format = :html
-     result = block.call
-     @template_format = :json
-     return result.to_json
-   end
-
-   alias_method :h2j, :html_to_json
-
-   def include_javascript_folder(folder)
-     files = Dir.glob("#{ Rails.root }/public/javascripts/#{ folder }/*.js")
-     files.map!{ |f| folder + "/" + File.basename(f) }
-     javascript_include_tag files
-   end
-   
    def tab_to(name, options = {}, html_options = {}, &block)
      options, html_options = name, options if block
 
@@ -39,25 +12,6 @@
      else
        link_to(name, options, html_options)
      end
-   end
-
-   def link_to_add(text, options, html_options = {}) 
-     html_options[:id] ||= ""
-     html_options[:id] += "add"
-     link_to(text, options, html_options)
-   end
-
-   def winnow_button(text, target, html_options = {})
-     html_options['data-remote'] = true
-     tab_to target, html_options do
-       content_tag(:span, text)
-     end
-   end
-
-   def content_for?(name)
-     raise "Delete content_for? in application_helper.rb" if RAILS_GEM_VERSION.to_i > 2
-     ivar = "@content_for_#{name}"
-     instance_variable_get(ivar).present?
    end
 
 end
