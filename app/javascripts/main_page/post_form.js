185ce64c7e26783805f2b6df16ae519a23e2dd57
John Poje <jpojecp@gmail.com>
2011-10-06 16:20:27 -0700

    remove js logging

--- app/javascripts/main_page/post_form.js
+++ app/javascripts/main_page/post_form.js
@@ -36,6 +36,7 @@ var PostForm = CommonPlace.View.extend({
   },
 
   incomplete: function(fields) {
+    console.log(fields);
     var incompleteFields = fields.shift();
     var self = this;
     _.each(fields, function(f) {
