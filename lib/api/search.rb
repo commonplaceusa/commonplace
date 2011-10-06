493ad63639926641c0f7b73c2717ed6a5ad64e3b
John Poje <jpojecp@gmail.com>
2011-10-05 17:51:26 -0700

    cleanup search api

--- lib/api/search.rb
+++ lib/api/search.rb
@@ -3,10 +3,10 @@ class API
 
     helpers do
       
-      def search(klass, query, page)
+      def search(klass, params)
         search = Sunspot.search(klass) do
-          keywords query.split(" ")
-          paginate(:page => page)
+          keywords params["query"].split(" ")
+          paginate(:page => params["page"])
         end
         serialize(search.results)
       end
@@ -20,55 +20,55 @@ class API
     get "/feeds" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(Feed, params["query"], params["page"])
+      search(Feed, params)
     end
 
     get "/groups" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(Group, params["query"], params["page"])
+      search(Group, params)
     end
 
     get "/users" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(User, params["query"], params["page"])
+      search(User, params)
     end
 
     get "/group-like" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search([Feed, Group, User], params["query"], params["page"])
+      search([Feed, Group, User], params)
     end
 
     get "/announcements" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(Announcement, params["query"], params["page"])
+      search(Announcement, params)
     end
 
     get "/events" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(Event, params["query"], params["page"])
+      search(Event, params)
     end
 
     get "/posts" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(Post, params["query"], params["page"])
+      search(Post, params)
     end
 
     get "/group-posts" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search(GroupPost, params["query"], params["page"])
+      search(GroupPost, params)
     end
 
     get "/post-like" do
       halt [200, {}, "[]"] if params["query"].blank?
 
-      search([Announcement, Event, Post, GroupPost], params["query"], params["page"])
+      search([Announcement, Event, Post, GroupPost], params)
     end
 
   end
