cd1c774a4be680d1406fbbf674746d8f46b1ea4f
Max Tilford <maxtilford@gmail.com>
2011-10-11 12:00:00 -0700

    only send Exceptional errors if in production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,10 +4,12 @@ require 'resque-exceptional'
 require 'resque/failure/multiple'
 require 'resque/failure/redis'
 
-Resque::Failure::Exceptional.configure do |config|
-  config.api_key = '0556a141945715c3deb50a0288ec3bea5417f6bf'
-end
+if Rails.env.production?
+  Resque::Failure::Exceptional.configure do |config|
+    config.api_key = '0556a141945715c3deb50a0288ec3bea5417f6bf'
+  end
 
-Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Exceptional]
-Resque::Failure.backend = Resque::Failure::Multiple
+  Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Exceptional]
+  Resque::Failure.backend = Resque::Failure::Multiple
+end
 
