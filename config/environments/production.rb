b6c1a3de6025384662123acaac81c0ae9c9273a3
Max Tilford <maxtilford@gmail.com>
2011-10-11 23:19:52 -0700

    force precompiling assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,6 @@ Commonplace::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-
   # Generate digests for assets URLs
   config.assets.digest = true
 
