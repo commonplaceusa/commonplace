3aa3241feb51a448eb082d3fe76e85438c842a6a
Max Tilford <maxtilford@gmail.com>
2011-10-13 16:06:28 -0700

    remove rack timeout from rails middleware, we include it explicitly

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Commonplace::Application.initialize!
+
+Commonplace::Application.configure do
+  config.middleware.delete Rack::Timeout
+end
