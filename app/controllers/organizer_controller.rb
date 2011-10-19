7c9f555afabef810fe178e956cbb208ec0014251
Jason Berlinsky <jberlinsky@gmail.com>
2011-10-18 20:09:17 -0400

    Organizer App v3

--- app/controllers/organizer_controller.rb
+++ app/controllers/organizer_controller.rb
@@ -4,13 +4,11 @@ class OrganizerController < ApplicationController
     @community_id = current_community.id
   end
 
-  def map
-    @center_zip_code = current_community.zip_code.to_s
-    @community_id = current_community.id
-  end
-
   def add
-    cp_client.add_data_point(current_community,params)
+    puts "(1..#{params[:entry_count]})"
+    (1..(params[:entry_count].to_i)).each do |e|
+      cp_client.add_data_point(current_community, {:number => params["number_#{e}"], :address => params["address_#{e}"]})
+    end
     redirect_to :action => :app
   end
 
