8fa4cf27cf29bca4ee438e242864a29eadf53a9c
John Poje <jpojecp@gmail.com>
2011-10-20 16:39:34 -0700

    info-box scrolling accomodates small screens

--- app/javascripts/info_boxes/base.js
+++ app/javascripts/info_boxes/base.js
@@ -63,19 +63,24 @@ var InfoBox = CommonPlace.View.extend({
 
   setPosition: function() {
     var $el = $(this.el);
+    if ($el.css("position") == "fixed") { $el.css({ top: 0, bottom: "auto" }); }
     var marginTop = parseInt($el.css("margin-top"), 10);
     var $parent = $el.parent();
+    var $footerTop = $("#footer").offset().top;
     var topScroll = $(window).scrollTop();
     var distanceFromTop = $el.offset().top;
     var parentBottomDistanceToTop = $parent.offset().top + $parent.height();
 
     if ($el.css("position") == "relative") {
       if (distanceFromTop < topScroll) {
-        $el.css({ position: "fixed", top: 0 });
+        $el.css({ position: "fixed", top: 0, bottom: "auto" });
       }
     } else {
       if (distanceFromTop < parentBottomDistanceToTop + marginTop) {
         $el.css({ position: "relative" });
+      } else if (distanceFromTop + $el.height() > $footerTop) {
+        var bottom = (topScroll + $(window).height() + 20) - $footerTop;
+        $el.css({ top: "auto", bottom: bottom });
       }
     }
   },
