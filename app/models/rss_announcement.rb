8f74144211ff831c7df7080dd31126db0509d37d
Max Tilford <maxtilford@gmail.com>
2011-10-20 14:51:16 -0700

    refactors RSSImporting

--- app/models/rss_announcement.rb
+++ app/models/rss_announcement.rb
@@ -1,3 +1,36 @@
+require 'rss'
+require 'htmlentities'
+
 class RSSAnnouncement < Announcement
 
+  validates_uniqueness_of :url
+
+  def self.from_rss_item(item, params = {})
+    self.record_timestamps = false
+
+    timestamp = item.date.try(:to_datetime) || DateTime.now
+
+    self.new(params) do |a|
+      a.subject = item.title
+      a.url = item.link
+      a.body = self.extract_rss_body(item.description)
+      a.created_at = timestamp
+      a.updated_at = timestamp
+    end
+  ensure
+    self.record_timestamps = true
+  end
+
+  def self.extract_rss_body(body)
+    body = self.strip_feedflare(body)
+    body = HTMLEntities.new.decode(body)
+    body = McBean.fragment(body).to_markdown
+    
+    body
+  end
+
+  def self.strip_feedflare(html)
+    html.gsub(/<div class=\"feedflare\">(.*)<\/div>/m, "")
+  end
+  
 end
--- lib/rss_importer.rb
+++ lib/rss_importer.rb
@@ -1,35 +1,11 @@
-require 'rss'
-require 'htmlentities'
 
 class RSSImporter
-
-  def self.strip_feedflare(html)
-    HTMLEntities.new.decode(html.gsub(/<div class=\"feedflare\">(.*)<\/div>/m, ""))
-  end
-
   def self.perform
-    RSSAnnouncement.record_timestamps = false    
-
     Feed.find(:all, :conditions => ["feed_url != ?", "" ]).each do |feed|
-      begin
-        RSS::Parser.parse(open(feed.feed_url).read, false).items.each do |item|
-          
-          unless RSSAnnouncement.exists?(:url => item.link)
-            description = RSSImporter.strip_feedflare(item.description)
-            RSSAnnouncement.create(:owner => feed,
-                                   :subject => item.title,
-                                   :url => item.link,
-                                   :community_id => feed.community_id,
-                                   :body => McBean.fragment(description).to_markdown,
-                                   :created_at => item.date.to_datetime,
-                                   :updated_at => item.date.to_datetime)
-            
-          end
-        end
-      rescue
+      Exceptional.rescue do
+        feed.rss_feed.update!
       end
     end
-    RSSAnnouncement.record_timestamps = true
   end
 end
 
