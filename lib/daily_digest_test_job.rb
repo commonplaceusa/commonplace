88a44db5bf7c87c5a90167f8f79a2a72b5373543
Jason Berlinsky <jberlinsky@gmail.com>
2011-10-22 00:21:22 -0400

    Added dummy background job for daily digest

--- lib/daily_digest_test_job.rb
+++ lib/daily_digest_test_job.rb
@@ -1,14 +1,15 @@
-class DailyDigestJob
+class DailyDigestTestJob
   @queue = :daily_digest
 
   def self.perform
     kickoff = KickOff.new
     date = DateTime.now.utc
+    sent = []
     User.where("post_receive_method != 'Never'").find_each do |user|
      Exceptional.rescue do
-        kickoff.deliver_daily_bulletin(user, date)
+        sent << user
      end
     end
+    Resque.redis.rpush("daily_digest_test_results", "#{sent.count}")
   end
-
 end
