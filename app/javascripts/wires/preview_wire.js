8f56f0f8b6a934a29ec616b9bd2ab64066a1c7fe
Max Tilford <maxtilford@gmail.com>
2011-10-23 08:55:54 -0700

    fixes alert when clicking More on a preview wire

--- app/javascripts/wires/preview_wire.js
+++ app/javascripts/wires/preview_wire.js
@@ -3,5 +3,5 @@ var PreviewWire = PaginatingWire.extend({
   template: "wires/preview-wire",
   _defaultPerPage: 3,
   fullWireLink: function() { return this.options.fullWireLink; },
-  showMore: function(){alert('asd');} // override Wire to allow default button behavior.
+  showMore: function() {} // passthrough
 });
