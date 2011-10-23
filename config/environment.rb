31976a736bc8a9d096ddab9011810b498e37bc22
Max Tilford <maxtilford@gmail.com>
2011-10-22 20:30:22 -0700

    Fixes Rack::Timeout in development, probably

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Commonplace::Application.initialize!
-
-Commonplace::Application.configure do
-  config.middleware.delete Rack::Timeout
-end
