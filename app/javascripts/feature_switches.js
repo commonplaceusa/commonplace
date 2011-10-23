040fe0c2068d59e3e7b7d645f4fdfd88fe7c087b
Max Tilford <maxtilford@gmail.com>
2011-10-23 10:45:43 -0700

    make feature switching persistent with the session

--- app/javascripts/feature_switches.js
+++ app/javascripts/feature_switches.js
@@ -1,4 +1,46 @@
+FeatureSwitcher = function(features, backend) {
+  
+  this._features = _(features).keys();
 
-Features = {
-  wireSearch: false
+  this._backend = backend || sessionStorage;
+
+  this.features = function() { return this._features; };
+
+  this.activate = function(feature) {
+    this.set(feature, true);
+  };
+
+  this.deactivate = function(feature) {
+    this.set(feature, false);
+  };
+
+  this.toggle = function(feature) {
+    this.set(feature,
+             !this.isActive(feature));
+  };
+  
+  this.isActive = function(feature) {
+    return this.get(feature);
+  };
+
+  this.set = function(feature, bool) {
+    this._backend.setItem(this.keyFor(feature), JSON.stringify(bool));
+  };
+  
+  this.get = function(feature) {
+    return JSON.parse(this._backend.getItem(this.keyFor(feature))); 
+  };
+  
+  this.keyFor = function(feature) { return "features:" + feature; };
+
+  var self = this;
+  _(features).each(function(bool, feature) {
+    self.set(feature, bool || self.isActive(feature));
+  });
+
+  return this;
 };
+
+Features = new FeatureSwitcher({
+  wireSearch: false
+});
