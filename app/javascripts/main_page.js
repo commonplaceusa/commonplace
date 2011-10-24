07c7720753c4129c7d81da1fb26cdacb831aa529
Max Tilford <maxtilford@gmail.com>
2011-10-23 21:07:07 -0700

    fixedLeftColumn rough (behind a feature switch)

--- app/javascripts/main_page.js
+++ app/javascripts/main_page.js
@@ -26,3 +26,50 @@
 //= require wires
 //= require wire_items
 //= require_tree ./main_page
+
+
+function setPostBoxTop() { 
+  if ($(window).scrollTop() < 60) { 
+    $("#post-box").css({top: 85}); 
+  } else { 
+    $("#post-box").css({top: 15}); 
+  } 
+}
+
+function setProfileBoxBottom() {
+  if ($(document).height() - $(window).scrollTop() - $(window).height() < 65) {
+    $("#info-box").css({bottom: 75});
+  } else {
+    $("#info-box").css({bottom: 15});
+  }
+}
+
+function setProfileBoxTop() {
+  var $postBox = $("#post-box");
+  $("#info-box").css({
+    top: $postBox.outerHeight() + parseInt($postBox.css("top"),10) + 10
+  });
+}
+
+function setProfileBoxInfoUpperHeight() {
+  $("#info-upper").css({
+    height: $("#info-box").height() - 
+      $("#info-box h2").outerHeight() - 
+      $("#info-box form").outerHeight() -
+      $("#info-box ul.filter").outerHeight() - 40
+  });
+}
+
+$(function() {
+
+  if (Features.isActive("fixedLeftColumn")) {
+    $(window).scroll(function() {
+      setPostBoxTop();
+      setProfileBoxBottom();
+      setProfileBoxTop();
+      setProfileBoxInfoUpperHeight();
+    });
+    $("body").addClass("fixedLeftColumn");
+  }
+  
+});
