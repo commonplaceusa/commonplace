b410386528da9375e9eb9175cbee3f93bc6523c9
John Poje <jpojecp@gmail.com>
2011-10-25 16:38:35 -0700

    js validation returns the same format as http responses

--- app/javascripts/models/post.js
+++ app/javascripts/models/post.js
@@ -10,10 +10,20 @@ var Post = Repliable.extend({
   },
 
   validate: function(attribs) {
-    var response = [];
-    if (!attribs.title) { response.push("title"); }
-    if (!attribs.body) { response.push("body"); }
-    if (response.length > 0) { return response; }
+    var missing = [];
+    if (!attribs.title) { missing.push("title"); }
+    if (!attribs.body) { missing.push("body"); }
+    if (missing.length > 0) {
+      var responseText = "Please fill in the " + missing.shift();
+      _.each(missing, function(field) {
+        responseText = responseText + " and " + field;
+      });
+      var response = {
+        status: 400,
+        responseText: responseText + "."
+      };
+      return response;
+    }
   }
 });
 
