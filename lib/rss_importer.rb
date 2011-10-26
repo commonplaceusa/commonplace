4b7ac7bb7e4a59c5b04e49bf02865604f49b98eb
Jason Berlinsky <jberlinsky@gmail.com>
2011-10-26 11:50:14 -0400

    Moving away from McBean

--- lib/rss_importer.rb
+++ lib/rss_importer.rb
@@ -2,8 +2,12 @@
 class RSSImporter
   def self.perform
     Feed.find(:all, :conditions => ["feed_url != ?", "" ]).each do |feed|
-      Exceptional.rescue do
-        feed.rss_feed.update!
+      unless (feed.feed_url =~ URI::regexp).nil?
+        if feed.feed_url.match /^http/
+          Exceptional.rescue do
+            feed.rss_feed.update!
+          end
+        end
       end
     end
   end
