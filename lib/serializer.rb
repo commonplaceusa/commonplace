4e0ec3ac3c6161d82b4cae92abc1c8ec14fb9e2f
Max Tilford <maxtilford@gmail.com>
2011-10-31 16:36:08 -0700

    use {community, feed, group, user}.as_api_response in serializer

--- lib/serializer.rb
+++ lib/serializer.rb
@@ -33,23 +33,7 @@ module Serializer
         "lng" => o.lng
       }
       when User
-        { "id" => o.id,
-        "schema" => "users",
-        "avatar_url" => o.avatar_url(:normal),
-        "url" => "/users/#{o.id}",
-        "name" => o.name,
-        "first_name" => o.first_name,
-        "last_name" => o.last_name,
-        "about" => o.about,
-        "interests" => o.interest_list,
-        "goods" => o.good_list,
-        "skills" => o.skill_list,
-        "subscriptions" => o.feeds,
-        "links" => {
-          "messages" => "/users/#{o.id}/messages",
-          "self" => "/users/#{o.id}"
-        }
-      }
+        o.as_api_response(:default)
       when Post
         { 
         "id" => o.id,
@@ -188,42 +172,7 @@ module Serializer
         }
 
       when Feed
-        { 
-        "id" => o.id,
-        "schema" => "feeds",
-        "user_id" => o.user.id,
-        "url" => "/pages/#{o.slug}",
-        "slug" => o.slug ,
-        "name" => o.name,
-        "about" => o.about,
-        "avatar_url" => o.avatar_url(:normal),
-        "profile_url" => "/feeds/#{o.id}/profile",
-        "rss_url" => o.feed_url,
-        "delete_url" => "/feeds/#{o.id}/delete",
-        "tags" => o.tag_list,
-        "website" => o.website,
-        "phone" => o.phone,
-        "address" => o.address,
-        "kind" => o.kind,
-        "links" => { 
-          "avatar" => {
-            "large" => o.avatar_url(:large),
-            "normal" => o.avatar_url(:normal),
-            "thumb" => o.avatar_url(:thumb)
-          },
-          "announcements" => "/feeds/#{o.id}/announcements",
-          "events" => "/feeds/#{o.id}/events",
-          "invites" => "/feeds/#{o.id}/invites",
-          "messages" => "/feeds/#{o.id}/messages",
-          "edit" => "/feeds/#{o.id}/edit",
-          "subscribers" => "/feeds/#{o.id}/subscribers",
-          "self" => "/feeds/#{o.id}",
-          "owners" => "/feeds/#{o.id}/owners"
-        },
-        "messagable_author_url" => "/feeds/#{o.id}/#{o.user.id}",
-      "messagable_author_name" => o.name
-      }
-
+        o.as_api_response(:default)
       when FeedOwner
         {
         "id" => o.id,
@@ -239,23 +188,7 @@ module Serializer
       }
 
       when Group
-        { 
-        "id" => o.id,
-        "schema" => "groups",
-        "url" => "/groups/#{o.id}",
-        "name" => o.name,
-        "about" => o.about,
-        "avatar_url" => o.avatar_url,
-        "slug" => o.slug,
-        "links" => {
-          "posts" => "/groups/#{o.id}/posts",
-          "members" => "/groups/#{o.id}/members",
-          "announcements" => "/groups/#{o.id}/announcements",
-          "events" => "/groups/#{o.id}/events",
-          "self" => "/groups/#{o.id}"
-        }
-        }
-
+        o.as_api_response(:default)
       when Account
         {
         "id" => o.id,
@@ -294,37 +227,7 @@ module Serializer
         }
 
       when Community
-        community_asset_url = "https://s3.amazonaws.com/commonplace-community-assets/#{o.slug}/"
-        { 
-        "id" => o.id,
-        "schema" => "community",
-        "slug" => o.slug,
-        "name" => o.name,
-        "groups" => o.groups.map {|g| 
-          { "slug" => g.slug, "avatar_url" => g.avatar_url, "id" => g.id, "name" => g.name }
-        },
-        "locale" => o.locale.to_s,
-        "admin_name" => o.organizer_name,
-        "admin_email" => o.organizer_email,
-        "links" => {
-          "launch_letter" => community_asset_url + "launchletter.pdf",
-          "information_sheet" => community_asset_url + "infosheet.pdf",
-          "neighborhood_flyer" => community_asset_url + "neighborflyer.pdf",
-          "all_flyers" => community_asset_url + "archives.zip",
-          "groups" => "/communities/#{o.id}/groups",
-          "feeds" => "/communities/#{o.id}/feeds",
-          "posts" => "/communities/#{o.id}/posts",
-          "events" => "/communities/#{o.id}/events",
-          "announcements" => "/communities/#{o.id}/announcements",
-          "group_posts" => "/communities/#{o.id}/group_posts",
-          "users" => "/communities/#{o.id}/users",
-          "self" => "/communities/#{o.id}",
-          "feeds_search" => "/search/community/#{o.id}/feeds?query=",
-          "users_search" => "/search/community/#{o.id}/users?query=",
-          "groups_search" => "/search/community/#{o.id}/groups?query=",
-          "posts_search" => "/search/community/#{o.id}/posts?query="
-        }
-      }
+        o.as_api_response(:default)
       end
 
     as_json
