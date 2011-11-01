86cb0e89f1cf350236036573deb2427a9bce620d
John Poje <jpojecp@gmail.com>
2011-10-31 17:16:48 -0700

    profile editing page is fully functional

--- app/javascripts/account/app.js
+++ app/javascripts/account/app.js
@@ -7,12 +7,22 @@ var AccountRouter = Backbone.Router.extend({
   initialize: function(options) {
     this.account = options.account;
     this.community = options.community;
+    this.interests = options.interests;
+    this.skills = options.skills;
+    this.goods = options.goods;
   },
   
   show: function() {
-    var editview = new AccountEditView({ account: this.account, community: this.community });
+    var editview = new AccountEditView({
+      account: this.account,
+      community: this.community,
+      interests: this.interests,
+      skills: this.skills,
+      goods: this.goods
+    });
     editview.render();
     $("#account-edit").replaceWith(editview.el);
+    editview.$("select.list").chosen();
   }
 });
 
@@ -21,17 +31,38 @@ var AccountEditView = CommonPlace.View.extend({
   id: "account-edit",
   
   events: {
-    "click .password": "showRedundant",
     "submit": "editAccount"
   },
   
   initialize: function(options) {
     this.account = options.account;
     this.community = options.community;
+    this.interests = options.interests;
+    this.skills = options.skills;
+    this.goods = options.goods;
   },
   
   afterRender: function() {
-    this.$("textarea").autoResize();
+    this.$(".about").autoResize();
+    
+    var get_posts = this.account.get("neighborhood_posts");
+    this.$("[name=get-posts][value=" + get_posts + "]").attr("checked", "checked");
+    
+    var get_bulletin = this.account.get("bulletin");
+    if (get_bulletin) {
+      this.$("[name=get-bulletin]").attr("checked", "checked");
+    }
+    
+    this.populateSelected(this.account.get("interests"), this.$("[name=interests]"));
+    this.populateSelected(this.account.get("skills"), this.$("[name=skills]"));
+    this.populateSelected(this.account.get("goods"), this.$("[name=goods]"));
+  },
+  
+  populateSelected: function(list, el) {
+    _.each(list, function(item) {
+      var option = el.children("[value=" + item + "]");
+      option.attr("selected", "selected");
+    });
   },
   
   editAccount: function(e) {
@@ -41,7 +72,12 @@ var AccountEditView = CommonPlace.View.extend({
     var changes = {
       name: this.$("[name=full_name]").val(),
       about: this.$("[name=about]").val(),
-      email: this.$("[name=email]").val()
+      email: this.$("[name=email]").val(),
+      interests: this.$("[name=interests]").val(),
+      skills: this.$("[name=skills]").val(),
+      goods: this.$("[name=goods]").val(),
+      neighborhood_posts: this.$("[name=get-posts]:checked").val(),
+      bulletin: this.$("[name=get-bulletin]").attr("checked") == "checked"
     }
     
     if (this.$("[name=password]").val()) {
@@ -58,17 +94,24 @@ var AccountEditView = CommonPlace.View.extend({
       }
     }
     
-    console.log(changes);
-    
     this.account.save(changes, {
-      success: function() { self.render(); },
+      success: function() { self.showSuccess(); },
       error: function(m, response) { self.showError(response); }
     });
+  },
+  
+  showSuccess: function() {
+    this.render();
+    this.$("select.list").chosen();
+    this.$(".success").show();
+    this.$(".error").hide();
+    $(window).scrollTop(0);
   },
   
   showError: function(response) {
     this.$(".error").text(response.responseText);
     this.$(".error").show();
+    this.$(".success").hide();
   },
   
   fullName: function() { return this.account.get("name"); },
@@ -81,8 +124,10 @@ var AccountEditView = CommonPlace.View.extend({
   
   avatarUrl: function() { return this.account.get("avatar_url"); },
   
-  showRedundant: function(e) {
-    this.$(".password-redundant").show();
-  }
+  interests: function() { return this.interests; },
+  
+  skills: function() { return this.skills; },
+  
+  goods: function() { return this.goods; }
 });
 
