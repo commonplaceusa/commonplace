e47155cd3a195ed50e9116fe6396b03d6f9d59d8
John Poje <jpojecp@gmail.com>
2011-11-03 15:08:03 -0700

    fixed sending query when fetching more of a wire

--- app/javascripts/wires/paginating_wire.js
+++ app/javascripts/wires/paginating_wire.js
@@ -31,7 +31,7 @@ var PaginatingWire = Wire.extend({
 
   search: function(event) {
     event.preventDefault();
-    this.scope['query'] = this.$("form.search input").val();
+    this.currentQuery = this.$("form.search input").val();
     this.$("ul").empty();
     var self = this;
     this.fetchPage(function() { self.appendPage(); });
