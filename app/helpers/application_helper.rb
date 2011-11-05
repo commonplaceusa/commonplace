8cf813c4c1e30c9f23510f38f429a992e9ca51fe
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2011-11-04 21:50:37 -0400

    refactor common_head, add tracking to some new pages, standardize tracking in head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,81 @@
      end
    end
 
+   def include_mixpanel
+     key = Rails.env.production? ? $MixpanelAPIToken : 'staging/testing key'
+raw <<script
+<script type='text/javascript'>
+//<![CDATA[
+if (window['mpq'] != 'undefined'){
+  var mpq = [];
+  mpq.push(["init", "#{$MixpanelAPIToken}"]);
+  (function(){var b,a,e,d,c;b=document.createElement("script");b.type="text/javascript";b.async=true;b.src=(document.location.protocol==="https:"?"https:":"http:")+"//api.mixpanel.com/site_media/js/api/mixpanel.js";a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a);e=function(f){return function(){mpq.push([f].concat(Array.prototype.slice.call(arguments,0)))}};d=["init","track","track_links","track_forms","register","register_once","identify","name_tag","set_config"];for(c=0;c<d.length;c++){mpq[d[c]]=e(d[c])}})();
+}
+//]]>
+</script>
+script
+   end
+
+   def include_ga
+     key = Rails.env.production? ? 'UA-12807510-2' : 'staging/testing key'
+raw <<script
+<script type='text/javascript'>
+//<![CDATA[
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12807510-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+//]]>
+</script>
+script
+   end
+
+   def include_exceptional
+     key = Rails.env.production? ? '0556a141945715c3deb50a0288ec3bea5417f6bf' : 'staging/testing key'
+raw <<script
+<script type='text/javascript' src="https://exceptional-js.heroku.com/exceptional.js"></script>
+<script type='text/javascript'>
+//<![CDATA[
+if(window['Exceptional'] !== undefined){
+  Exceptional.setHost('exceptional-api.heroku.com');
+  Exceptional.setKey('#{key}');
+}
+//]]>
+</script>
+script
+   end
+
+   def include_commonplace(title = '')
+raw <<script
+<script type='text/javascript'>
+//<![CDATA[
+  if(window['CommonPlace'] == undefined){
+    CommonPlace = {};
+  }
+  CommonPlace.auth_token = "#{form_authenticity_token}";
+  CommonPlace.env = '#{Rails.env}';
+  CommonPlace.page = '#{title}';
+//]]>
+</script>
+script
+   end
+
+   def populate_commonplace
+raw <<script
+<script type='text/javascript'>
+//<![CDATA[
+if(window['Community'] !== undefined && window['Account'] !== undefined){
+  CommonPlace.community = new Community(#{serialize(current_community)});
+  window.current_account = new Account(#{serialize(Account.new(current_user))});
+}
+//]]>
+</script>
+script
+   end
+
 end
