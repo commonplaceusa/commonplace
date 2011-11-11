2f3632adebcad7c538527d9f55eba168f6a24787
Jason Berlinsky <jberlinsky@gmail.com>
2011-11-11 02:11:13 -0500

    E-mail text fix

--- mail/n_subscribers_feed_notification.rb
+++ mail/n_subscribers_feed_notification.rb
@@ -29,7 +29,7 @@ class NSubscribersFeedNotification < MailBase
   end
 
   def invitation_link
-    link_to 'http://www.ourcommonplace.com/invite', 'Click Here to Invite Your Neighbors'
+    "http://#{community.slug}.ourcommonplace.com/invite"
   end
 
   def number_of_subscribers
