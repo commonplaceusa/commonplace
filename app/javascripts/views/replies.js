56cff5937ccc30fb4fbe9e4b194ce446cede96ea
Max Tilford <maxtilford@gmail.com>
2011-11-17 14:29:19 -0800

    always show all replies"

--- app/javascripts/views/replies.js
+++ app/javascripts/views/replies.js
@@ -17,34 +17,21 @@ var RepliesView = CommonPlace.View.extend({
   events: {
     "keydown form textarea": "sendReply",
     "focus form textarea": "showHint",
-    "blur form textarea": "hideHint",
-    "click .replies-more": "showMoreReplies"
+    "blur form textarea": "hideHint"
   },
 
   appendReplies: function() {
     var self = this;
-    var $ul = this.$("ul.reply-list");
-
-    this.collection.each(function(reply, index) {
-      var replyview = new ReplyWireItem({ model: reply, account: self.account });
-
-      $ul.append(replyview.render().el);
-      if (index < self.hiddenReplyCount() ){
-        $(replyview.el).hide();
-      }
-
+    var elements = this.collection.map(function(reply) {
+      var view = new ReplyWireItem({ model: reply, account: self.account });
+      view.render();
+      return view.el; 
     });
+    this.$("ul.reply-list").append(elements);
   },
 
   pluralizedReplies: function(){
       return (this.hiddenReplyCount() > 1) ? 'replies' : 'reply';
-  },
-
-  hiddenReplyCount: function() {
-    if (!this._hiddenReplyCount) {
-      this._hiddenReplyCount = (this.collection.length > 2) ? this.collection.length - 3 : 0;
-    }
-    return this._hiddenReplyCount;
   },
 
   sendReply: function(e) {
@@ -59,12 +46,6 @@ var RepliesView = CommonPlace.View.extend({
         this.collection.create({ body: this.$("[name=body]").val()});
       }
     }
-  },
-
-  showMoreReplies: function(event){
-    event.preventDefault();
-    this.$('.reply-item').show();
-    this.$('.replies-more').hide();
   },
   
   showHint: function(e) { this.$(".enter-hint").show(); },
