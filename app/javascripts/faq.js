ade5d019b7c530baeef53557672d5b487b9661e3
Max Tilford <maxtilford@gmail.com>
2011-12-08 00:44:33 -0800

    refactor: faq page

--- app/javascripts/faq.js
+++ app/javascripts/faq.js
@@ -1,19 +1,20 @@
-$(document).ready(function(){   
+$(function(){ 
+  if ($("body").hasClass("faq")) {
     var sidebar = $('#sections_picker');
     var top = sidebar.offset().top;
     var main;
     $(window).resize(function (event){
-        var ypos = $(this).scrollTop();
-        main = $('#main').offset().left;
-        main += $('#main').width();
-        main -= sidebar.width();
-        main += 80;
+      var ypos = $(this).scrollTop();
+      main = $('#main').offset().left;
+      main += $('#main').width();
+      main -= sidebar.width();
+      main += 80;
       if(ypos <= 3772){
         if (ypos+40 >= top) {
-            sidebar.css({position: "fixed", left: main, top: "30px"});
+          sidebar.css({position: "fixed", left: main, top: "30px"});
         }
         else{
-            sidebar.css({position: "absolute", left: "", top: "30px"});
+          sidebar.css({position: "absolute", left: "", top: "30px"});
         }
       }
       else{
@@ -21,25 +22,22 @@ $(document).ready(function(){
       } 
     });
     $(window).scroll(function (event) {
-        var ypos = $(this).scrollTop();
-        main = $('#main').offset().left;
-        main += $('#main').width();
-        main -= sidebar.width();
-        main += 80;
+      var ypos = $(this).scrollTop();
+      main = $('#main').offset().left;
+      main += $('#main').width();
+      main -= sidebar.width();
+      main += 80;
       if(ypos <= 3772){
         if (ypos+40 >= top) {
-            sidebar.css({position: "fixed", left: main, top: "30px"});
+          sidebar.css({position: "fixed", left: main, top: "30px"});
         }
         else{
-            sidebar.css({position: "absolute", left: "", top: "30px"});
+          sidebar.css({position: "absolute", left: "", top: "30px"});
         }
       }
       else{
         sidebar.css({position: "absolute", top: "3637px", left:""});
       }
     });
- 
-    $.localScroll();
-     
- 
+  } 
 });
