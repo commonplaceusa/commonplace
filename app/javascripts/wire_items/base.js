417c3a1c1fd5cba61b28760c6969e94fd0589df5
Jason Berlinsky <jberlinsky@gmail.com>
2011-12-30 20:59:04 -0500

    s/mode/model

--- app/javascripts/wire_items/base.js
+++ app/javascripts/wire_items/base.js
@@ -2,7 +2,7 @@ var WireItem = CommonPlace.View.extend({
   thank: function() {
     var self = this;
     $.ajax({
-      url: "/api/" + this.mode.get("schema") + "/" + this.model.get("id") + "/thank",
+      url: "/api/" + this.model.get("schema") + "/" + this.model.get("id") + "/thank",
       type: "POST",
       success: function() {
         self.$(".thank_count").html(self.numThanks() + 1);
