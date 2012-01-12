b848d06b5e25c6d4ad4dddde99eea4864d7e8ac1
Jason Berlinsky <jberlinsky@gmail.com>
2012-01-12 13:56:15 -0500

    Fixes URLs for sharing

--- app/javascripts/views/share.js
+++ app/javascripts/views/share.js
@@ -9,6 +9,10 @@ var ShareView = CommonPlace.View.extend({
   afterRender: function() {
   }, 
 
+  share_url: function() {
+    return(CommonPlace.community.get("links")["base"] + "/show/" + this.model.get("schema") + "/" + this.model.get("id"));
+  },
+
   events: {
     "click .share-e": "showEmailShare",
     "click .share-f": "shareFacebook",
