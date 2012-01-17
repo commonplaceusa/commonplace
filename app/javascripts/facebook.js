e915e9f4b568b2fa93dc2e0ac5247e80d029eac4
Jason Berlinsky <jberlinsky@gmail.com>
2012-01-17 09:20:27 -0500

    Ensure that the FB API has been initiated before trying to post-connect

--- app/javascripts/facebook.js
+++ app/javascripts/facebook.js
@@ -10,23 +10,17 @@ function fbEnsureInit(callback) {
 
 function facebook_connect_post_registration() {
   // Pulls an avatar and access token
-  FB.login(function(response) {
-    if (response.authResponse) {
-      FB.api("/me", function(response) {
-        // Do something with the data
-        console.log(response);
-        var avatar_url = 'https://graph.facebook.com/' + response.username + '/picture';
-        console.log("Downloading avatar from " + avatar_url);
-        $.getImageData({
-          url: avatar_url,
-          success: function(image) {
-            console.log(image);
-          },
-          error: function(xhr, text_status) {
-            console.log(xhr);
-            console.log("ERROR: " + text_status);
-          }});
-      });
-    }
-  }, { scope: 'read_friendlists,offline_access' });
+  fbEnsureInit(function() {
+    FB.login(function(response) {
+      if (response.authResponse) {
+        FB.api("/me", function(response) {
+          // Do something with the data
+          console.log(response);
+          var avatar_url = 'https://graph.facebook.com/' + response.username + '/picture';
+          // Set the avatar in the API
+          // Store the offline access token via the API
+        });
+      }
+    }, { scope: 'read_friendlists,offline_access' });
+  });
 }
