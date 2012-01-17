16f9983648c09fc7584390cd8110041a6096b95f
Jason Berlinsky <jberlinsky@gmail.com>
2012-01-17 01:22:35 -0500

    Test implementation of FB-post-reg

--- app/javascripts/facebook.js
+++ app/javascripts/facebook.js
@@ -27,3 +27,25 @@ function facebook_invite() {
       });
 }
 
+function facebook_connect_post_registration() {
+  // Pulls an avatar and access token
+  FB.login(function(response) {
+    if (response.authResponse) {
+      FB.api("/me", function(response) {
+        // Do something with the data
+        console.log(response);
+        var avatar_url = 'https://graph.facebook.com/' + response.username + '/picture';
+        console.log("Downloading avatar from " + avatar_url);
+        $.getImageData({
+          url: avatar_url,
+          success: function(image) {
+            console.log(image);
+          },
+          error: function(xhr, text_status) {
+            console.log(xhr);
+            console.log("ERROR: " + text_status);
+          }});
+      });
+    }
+  }, { scope: 'read_friendlists,offline_access' });
+}
