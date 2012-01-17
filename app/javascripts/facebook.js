5566691b7692bcb56f661dbb56b7228aa9f02cb1
Jason Berlinsky <jberlinsky@gmail.com>
2012-01-17 10:34:44 -0500

    Save data collected from Facebook

--- app/javascripts/facebook.js
+++ app/javascripts/facebook.js
@@ -8,17 +8,30 @@ function fbEnsureInit(callback) {
     }
 }
 
-function facebook_connect_post_registration() {
+function facebook_connect_post_registration(success, failure) {
   // Pulls an avatar and access token
   fbEnsureInit(function() {
     FB.login(function(response) {
       if (response.authResponse) {
+        var auth_token = response.authResponse.accessToken;
         FB.api("/me", function(response) {
-          // Do something with the data
-          console.log(response);
-          var avatar_url = 'https://graph.facebook.com/' + response.username + '/picture';
           // Set the avatar in the API
           // Store the offline access token via the API
+          $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "/api/users/" + CommonPlace.account.get("id") + "/update_avatar_and_fb_auth",
+            data: JSON.stringify({
+              fb_username: response.username,
+              fb_auth_token: auth_token
+            }),
+            success: function() {
+              success();
+            },
+            failure: function() {
+              failure();
+            }
+          });
         });
       }
     }, { scope: 'read_friendlists,offline_access' });
