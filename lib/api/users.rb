842bd2630ef452bf1b1b7830d8027e11bd9e6c9f
Jason Berlinsky <jberlinsky@gmail.com>
2012-01-18 17:50:45 -0500

    Move FB API stuff to /accounts

--- lib/api/users.rb
+++ lib/api/users.rb
@@ -16,19 +16,6 @@ class API
       end
     end
 
-    post "/:id/update_avatar_and_fb_auth" do |id|
-      user = User.find(id)
-      halt [401, "wrong community"] unless in_comm(user.community.id)
-      # Update the user object
-      user.metadata['fb_access_token'] = request_body['fb_auth_token']
-      user.facebook_uid = request_body['fb_username']
-      if user.save
-        [200, ""]
-      else
-        [400, "errors"]
-      end
-    end
-
     get "/user_by_email/:email" do |email|
       user = User.find_by_email(email)
       halt [401, "wrong community"] unless current_user.admin
