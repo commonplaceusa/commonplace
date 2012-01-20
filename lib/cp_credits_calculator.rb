483dbb5d66811f2cfb08383c55cceb127df81897
Jason Berlinsky <jberlinsky@gmail.com>
2012-01-20 11:26:23 -0500

    Forgot to re-validate

--- lib/cp_credits_calculator.rb
+++ lib/cp_credits_calculator.rb
@@ -5,6 +5,7 @@ class CpCreditsCalculator
   def self.perform
     User.where("cp_credits_are_valid = false").each do |user|
       user.calculated_cp_credits = user.all_cpcredits
+      user.cp_credits_are_valid = true
       user.save
     end
   end
