0bd05c3e697b4ac57f256782b6c359389892a7fe
John Poje <jpojecp@gmail.com>
2012-01-24 13:49:59 -0500

    fixed switching between resources on feed/group pages

--- app/javascripts/feed_page/feed_nav_view.js
+++ app/javascripts/feed_page/feed_nav_view.js
@@ -8,12 +8,13 @@ var FeedNavView = CommonPlace.View.extend({
 
   initialize: function(options) {
     this.current = options.current || "showAnnouncements";
+    this.switchTab = options.switchTab;
   },
   
   navigate: function(e) {
     e.preventDefault();
     this.current = $(e.target).attr('data-tab');
-    this.trigger('switchTab', this.current);
+    this.switchTab(this.current);
     this.render();
   },
 
