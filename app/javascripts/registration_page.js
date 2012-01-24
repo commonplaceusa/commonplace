c74f1851c90f9c60ee000984e4f0146240b4bfd3
Josh Lewis <josh@joshmlewis.com>
2012-01-24 18:11:56 -0500

    Changed sign up button

--- app/javascripts/registration_page.js
+++ app/javascripts/registration_page.js
@@ -11,7 +11,7 @@ $(function() {
   // Register
   $('#sign_in_button').click(function() {
     $(this).toggleClass("open");
-    $(this).siblings("#sign_in_form").children("form").slideToggle();
+    $(this).siblings("#sign_in_form").children("form").toggle();
   });
 
   // Add more info
