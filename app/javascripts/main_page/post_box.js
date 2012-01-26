a72a5da89d28d888ef277008a1435e19e4ec9ab0
John Poje <jpojecp@gmail.com>
2012-01-26 14:34:30 -0500

    integrating feed management into postbox, header

--- app/javascripts/main_page/post_box.js
+++ app/javascripts/main_page/post_box.js
@@ -40,7 +40,7 @@ var PostBox = CommonPlace.View.extend({
     if (tab == "group_post") { tab = "group"; }
     
     this.$("li." + tab).addClass("current");
-    view = this.tabs[tab](this);
+    view = this.tabs(tab);
     
     view.render();
     this.$("form").replaceWith(view.el);
@@ -58,34 +58,45 @@ var PostBox = CommonPlace.View.extend({
     if (view.onFormFocus) { view.onFormFocus(); }
   },
   
-  tabs: {
-    nothing: function() { return new PostForm(); },
-    event: function() { return new EventForm(); },
-    post: function() { return new PostForm({
-      category: "neighborhood",
-      template: "main_page.forms.post-neighborhood-form"
-    }); },
-    publicity: function() { return new PostForm({
-      category: "publicity",
-      template: "main_page.forms.post-publicity-form"
-    }); },
-    offers: function() { return new PostForm({
-      category: "offers",
-      template: "main_page.forms.post-offer-form"
-    }); },
-    help: function() { return new PostForm({
-      category: "help",
-      template: "main_page.forms.post-help-form"
-    }); },
-    other: function() { return new PostForm({
-      category: "other",
-      template: "main_page.forms.post-form"
-    }); },
-    group: function() { return new GroupPostForm(); },
-    meetups: function() { return new PostForm({
-      category: "meetups",
-      template: "main_page.forms.post-meetup-form"
-    }); }
+  tabs: function(tab) {
+    var view;
+    var constant = {
+      nothing: function() { return new PostForm(); },
+      event: function() { return new EventForm(); },
+      post: function() { return new PostForm({
+        category: "neighborhood",
+        template: "main_page.forms.post-neighborhood-form"
+      }); },
+      publicity: function() { return new PostForm({
+        category: "publicity",
+        template: "main_page.forms.post-publicity-form"
+      }); },
+      offers: function() { return new PostForm({
+        category: "offers",
+        template: "main_page.forms.post-offer-form"
+      }); },
+      help: function() { return new PostForm({
+        category: "help",
+        template: "main_page.forms.post-help-form"
+      }); },
+      other: function() { return new PostForm({
+        category: "other",
+        template: "main_page.forms.post-form"
+      }); },
+      group: function() { return new GroupPostForm(); },
+      meetups: function() { return new PostForm({
+        category: "meetups",
+        template: "main_page.forms.post-meetup-form"
+      }); }
+    };
+    
+    if (!constant[tab]) {
+      view = new AnnouncementForm({
+        feed_id: tab.split("-").pop()
+      });
+    } else { view = constant[tab](); }
+    
+    return view;
   },
   
   groups: function() {
@@ -94,13 +105,16 @@ var PostBox = CommonPlace.View.extend({
   
   showWire: function(tab) {
     if (tab != "nothing") {
+      if (tab.search("feed") > -1) { tab = "announcements"; }
       var wire = $(".resources .sub-navigation." + tab);
       if (wire.length) {
         var offset = wire.offset().top;
         $(window).scrollTo(offset - parseInt($("#community-resources .sticky").css("top")));
       }
     }
-  }
+  },
+  
+  feeds: function() { return CommonPlace.account.get("feeds"); }
   
   
 });
