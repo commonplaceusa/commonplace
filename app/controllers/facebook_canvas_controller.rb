8b8aa74e01968aff2f40846af20c5a26ca11e2bf
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-04 15:36:55 -0500

    Move Facebook canvas stuff from JS to Ruby

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -3,9 +3,11 @@ require 'open-uri'
 class FacebookCanvasController < ApplicationController
   
   def index
-    @request_id = params[:request_ids]
+    request_id = params[:request_ids]
     url = "https://graph.facebook.com/oauth/access_token?&client_id=#{$FacebookConfig['app_id']}&client_secret=#{$FacebookConfig['app_secret']}&grant_type=client_credentials"
-    @application_token = open(url).read.gsub("access_token=","")
+    application_token = open(url).read.gsub("access_token=","")
+    graph_url = "https://graph.facebook.com/#{request_id}?access_token=#{application_token}"
+    @slug = ActiveSupport::JSON.decode(open(URI.encode(graph_url)).read)
     render :layout => nil
   end
 
