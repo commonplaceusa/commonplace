bebe155b99f15fe9c7ef13e9f69f8c6e77309934
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-04 15:57:32 -0500

    Do not use array, use string

--- app/controllers/facebook_canvas_controller.rb
+++ app/controllers/facebook_canvas_controller.rb
@@ -7,7 +7,7 @@ class FacebookCanvasController < ApplicationController
     url = "https://graph.facebook.com/oauth/access_token?&client_id=#{$FacebookConfig['app_id']}&client_secret=#{$FacebookConfig['app_secret']}&grant_type=client_credentials"
     application_token = open(url).read.gsub("access_token=","")
     graph_url = "https://graph.facebook.com/#{request_id}?access_token=#{application_token}"
-    @slug = ActiveSupport::JSON.decode(open(URI.encode(graph_url)).read)
+    @slug = ActiveSupport::JSON.decode(open(URI.encode(graph_url)).read)["data"]
     render :layout => nil
   end
 
