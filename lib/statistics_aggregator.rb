db7f0071ce58a3be00d622d8c15339d05e057170
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-06 09:58:23 -0500

    Adds statistics API and front-end

--- lib/statistics_aggregator.rb
+++ lib/statistics_aggregator.rb
@@ -1,46 +1,33 @@
 class StatisticsAggregator
-
-  AVERAGE_DAYS = 7.0
-  HISTORICAL_DAYS = 13
-  DATA_UNAVAILABLE_MESSAGE = "N/A"
+  require 'csv'
 
   MailGunStatistics = RestClient::Resource.new "https://api:#{$MailgunAPIToken}@api.mailgun.net/v2/#{$MailgunAPIDomain}/stats"
 
-  NETWORK_SIZE = User.count
-
-  def self.historical_statistics
-    ActiveSupport::JSON.decode(Resque.redis.get "statistics:historical")
-  end
-
-  def self.generate_user_statistics_csv_for_community(c)
-    csv = "Date,Users"
-    today = DateTime.now
-    launch = c.users.sort{ |a,b| a.created_at <=> b.created_at }.first.created_at.to_date
-    launch.upto(today).each do |day|
-      csv = "#{csv}\n#{day},#{c.users.between(launch.to_datetime,day.to_datetime).count}"
-    end
-    csv
-  end
-
   def self.generate_statistics_csv_for_community(c)
     unless Resque.redis.get("statistics:csv:#{c.slug}").present?
-      csv = "Date,Users,Posts,Events,Announcements,Private Messages,Group Posts"
+      csv = "Date,UsersTotal,PostsTotal,EventsTotal,AnnouncementsTotal,PrivateMessagesTotal,GroupPostsTotal,UsersLoggedInOverPast3Months,UsersActiveOverPast30Days,UsersPostingOverPast3Months,UsersGainedDaily"
       today = DateTime.now
       community = c
       launch = community.launch_date.to_date || community.users.sort{ |a,b| a.created_at <=> b.created_at }.first.created_at.to_date
       launch.upto(today).each do |day|
-        reply_count = 0
-        replies = community.repliables
-        replies.each do |reply_set|
-          reply_count += reply_set.select{ |repliable| repliable.between?(launch.to_datetime, day.to_datetime) }.count
-        end
+        #reply_count = 0
+        #replies = community.repliables
+        #replies.each do |reply_set|
+        #  reply_count += reply_set.select{ |repliable| repliable.between?(launch.to_datetime, day.to_datetime) }.count
+        #end
         user_count = community.users.between(launch.to_datetime, day.to_datetime).count
+        logged_in_in_past_30_days = community.users.select { |u| u.last_sign_in_at and u.last_sign_in_at < day.to_datetime and u.last_sign_in_at > day.to_datetime - 30.days }.count
+        daily_bulletin_opens_on_date = 0
+        users_engaged_in_past_30_days = 0
+        users_posted_in_past_30_days = 0
+        users_gained = community.users.between(day.to_datetime.beginning_of_day, day.to_datetime.end_of_day).count
         post_count = community.posts.between(launch.to_datetime, day.to_datetime).count
         event_count = community.events.between(launch.to_datetime, day.to_datetime).count
         announcement_count = community.announcements.between(launch.to_datetime, day.to_datetime).count
         private_message_count = community.private_messages.select { |m| m.between?(launch.to_datetime, day.to_datetime) }.count
         group_post_count = community.group_posts.select { |p| p.between?(launch.to_datetime, day.to_datetime) }.count
-        csv = "#{csv}\n#{day},#{user_count},#{post_count},#{event_count},#{announcement_count},#{private_message_count},#{group_post_count},#{reply_count}"
+        #csv = "#{csv}\n#{day.strftime("%m/%d/%Y")},#{user_count},#{post_count},#{event_count},#{announcement_count},#{private_message_count},#{group_post_count},#{reply_count}"
+        csv = "#{csv}\n#{day.strftime("%m/%d/%Y")},#{user_count},#{post_count},#{event_count},#{announcement_count},#{private_message_count},#{group_post_count},#{logged_in_in_past_30_days},#{users_engaged_in_past_30_days},#{users_posted_in_past_30_days},#{users_gained}"
       end
       Resque.redis.set("statistics:csv:#{c.slug}", csv)
       csv
@@ -49,6 +36,23 @@ class StatisticsAggregator
     end
   end
 
+  def self.generate_hashed_statistics_for_community(c)
+    unless Resque.redis.get("statistics:hashed:#{c.slug}").present?
+      #csv = CSV.new(StatisticsAggregator.csv_statistics_for_community(c), {})
+      data = CSV.parse(StatisticsAggregator.csv_statistics_for_community(c))
+      headers = data.shift.map &:to_s
+      string_data = data.map { |row| row.map(&:to_s) }
+      array_of_hashes = string_data.map { |row| Hash[*headers.zip(row).flatten] }
+      Resque.redis.set("statistics:hashed:#{c.slug}", ActiveSupport::JSON.encode(array_of_hashes))
+      array_of_hashes
+    else
+      Resque.redis.get("statistics:hashed:#{c.slug}")
+    end
+  end
+
+  def self.csv_statistics_for_community(community)
+    Resque.redis.get("statistics:csv:#{community.slug}")
+  end
 
   def self.statistics_for_community_between_days_ago(c, yday, tday)
     community_average_days = StatisticsAggregator.average_days(c) || AVERAGE_DAYS
@@ -78,97 +82,4 @@ class StatisticsAggregator
     
     result
   end
-
-  def self.historical_statistics_for_community(community, days)
-    result = []
-    day_counter = 0
-    # We only need the first day's results as long as a result set exists
-    if StatisticsAggregator.historical_statistics.count >= days
-      result = StatisticsAggregator.historical_statistics[community.name]
-      result.delete(result.last)
-      result.insert(0, StatisticsAggregator.statistics_for_community_between_days_ago(community, 1, 0))
-    else
-      while day_counter <= days do
-        # Generate statistics between day_counter+1.days.ago and day_counter.days.ago
-        result[day_counter] = StatisticsAggregator.statistics_for_community_between_days_ago(community, day_counter + 1, day_counter)
-        day_counter += 1
-      end
-    end
-    result
-  end
-
-  def self.perform
-    # Prepare to access Google Analytics
-    Garb::Session.login($GoogleAnalyticsAPILogin, $GoogleAnalyticsAPIPassword)
-    profile = Garb::Management::Profile.all.detect {|p| p.web_property_id == $GoogleAnalyticsPropertyID}
-    # Aggregate platform-wide statistics (for every community)
-    communities = {}
-    historical = {}
-    overall = {}
-    Community.all.sort.select{|c| c.core and c.posts.present?}.each do |c|
-      historical[c.name] = StatisticsAggregator.historical_statistics_for_community(c, HISTORICAL_DAYS)
-      communities[c.name] = historical[c.name][0]
-    end
-
-    overall[:page_views_today] = DATA_UNAVAILABLE_MESSAGE
-    overall[:page_views_this_week] = DATA_UNAVAILABLE_MESSAGE
-    overall[:average_page_views_per_day] = DATA_UNAVAILABLE_MESSAGE
-
-    overall[:unique_platform_visits_today] = DATA_UNAVAILABLE_MESSAGE
-    overall[:average_number_of_visitors_daily] = DATA_UNAVAILABLE_MESSAGE
-
-    overall[:network_engagement_daily] = DATA_UNAVAILABLE_MESSAGE
-    overall[:network_engagement_weekly] = DATA_UNAVAILABLE_MESSAGE
-    overall[:average_time_spent_on_platform] = DATA_UNAVAILABLE_MESSAGE
-
-    overall[:average_percent_of_emails_read] = (StatisticsAggregator.email_open_pctg())
-
-    overall[:percent_of_network_adding_data] = DATA_UNAVAILABLE_MESSAGE
-
-    overall[:average_number_of_profile_adds_daily] = DATA_UNAVAILABLE_MESSAGE
-    overall[:percentage_with_profile_photos] = DATA_UNAVAILABLE_MESSAGE
-    overall[:percentage_with_tags_in_profile] = DATA_UNAVAILABLE_MESSAGE
-
-    overall[:number_of_profile_clicks] = DATA_UNAVAILABLE_MESSAGE
-    overall[:average_profile_clicks_per_visitor] = DATA_UNAVAILABLE_MESSAGE
-    overall[:number_of_searches] = DATA_UNAVAILABLE_MESSAGE
-    overall[:average_number_of_searches_per_visitor] = DATA_UNAVAILABLE_MESSAGE
-
-    overall[:average_load_time] = DATA_UNAVAILABLE_MESSAGE
-
-    Resque.redis.set COMMUNITY_STATISTICS_BUCKET, ActiveSupport::JSON.encode(communities)
-    Resque.redis.set OVERALL_STATISTICS_BUCKET, ActiveSupport::JSON.encode(overall)
-    Resque.redis.set HISTORICAL_STATISTICS_BUCKET, ActiveSupport::JSON.encode(historical)
-  end
-
-  def self.emails_opened
-    emails_opened = 0
-    params = {}
-    params[:event] = 'opened'
-    result = ActiveSupport::JSON.decode(MailGunStatistics.get({:params => params}))
-    result['items'].each do |obj|
-      emails_opened += obj['total_count'].to_i
-    end
-    return emails_opened
-  end
-
-  def self.emails_sent
-    emails_sent = 0
-    params = {}
-    params[:event] = 'sent'
-    result = ActiveSupport::JSON.decode(MailGunStatistics.get({:params => params}))
-    result['items'].each do |obj|
-      emails_sent += obj['total_count'].to_i
-    end
-    return emails_sent
-  end
-
-  def self.email_open_pctg
-    return "#{(100 * StatisticsAggregator.emails_opened.to_f / StatisticsAggregator.emails_sent.to_f).round(2)}%"
-  end
-
-  def self.average_days(community)
-    (DateTime.now - community.launch_date.to_date).to_i
-  end
-
 end
