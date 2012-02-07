36d94ff25f5c489d9dc4753dedc529d2acb2e7a1
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-07 10:27:32 -0500

    Add global stats and content type graphs

--- lib/statistics_csv_generator.rb
+++ lib/statistics_csv_generator.rb
@@ -6,11 +6,15 @@ class StatisticsCsvGenerator
       Resque.redis.set("statistics:csv:#{c.slug}", nil)
       Resque.redis.set("statistics:hashed:#{c.slug}", nil)
     end
+    Resque.redis.set("statistics:csv:global", nil)
+    Resque.redis.set("statistics:hashed:global", nil)
     Resque.redis.set("statistics:csv:meta:date", Date.today.to_s)
     Resque.redis.set("statistics:hashed:meta:date", Date.today.to_s)
     Community.all.each do |c|
       StatisticsAggregator.generate_statistics_csv_for_community(c)
       StatisticsAggregator.generate_hashed_statistics_for_community(c)
     end
+    StatisticsAggregator.csv_statistics_globally
+    StatisticsAggregator.generate_hashed_statistics_globally
   end
 end
