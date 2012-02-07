5482abc2e5e0d69f379e4ceef24871f9832b95fa
John Poje <jpojecp@gmail.com>
2012-02-07 14:11:22 -0500

    only 3 announcements in landingwire

--- app/models/community_wire.rb
+++ app/models/community_wire.rb
@@ -28,7 +28,7 @@ class CommunityWire
 
   def publicity
     [ @community.posts.where(category: 'publicity').limit(3),
-      @community.announcements.reorder("GREATEST(replied_at, created_at) DESC").limit(3) ].flatten.sort {|a,b| [b.replied_at, b.created_at].compact.max <=> [a.replied_at, a.created_at].compact.max }
+      @community.announcements.reorder("GREATEST(replied_at, created_at) DESC").limit(3) ].flatten.sort {|a,b| [b.replied_at, b.created_at].compact.max <=> [a.replied_at, a.created_at].compact.max }.take(3)
   end
   
   def meetups
