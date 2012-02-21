5aa0d5e09b9c80ca11354828d7cd266197052a31
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-21 10:20:29 -0500

    Show a warning message

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -3,6 +3,8 @@ if ENV['MONGOHQ_HOST']
   MongoMapper.database = ENV['MONGOHQ_DATABASE']
   MongoMapper.database.authenticate(ENV['MONGOHQ_USERNAME'], ENV['MONGOHQ_PASSWORD'])
 else
+  puts "Please make sure that you have MongoDB installed and running locally"
+  puts "Otherwise, bad things will happen."
   MongoMapper.connection = Mongo::Connection.new('localhost')
   MongoMapper.database = "commonplace_stats_#{Rails.env}"
 end
