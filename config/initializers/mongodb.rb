c9d09c4576015bf245ff593e13ff688436f6120e
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-21 10:04:48 -0500

    Use different environment variables for MongoHQ

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,2 +1,8 @@
-MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'] || 'localhost')
-MongoMapper.database = "commonplace_stats_#{Rails.env}"
+if ENV['MONGOHQ_HOST']
+  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_HOST'], ENV['MONGOHQ_PORT'])
+  MongoMapper.database = ENV['MONGOHQ_DATABASE']
+  MongoMapper.database.authenticate(ENV['MONGOHQ_USERNAME'], ENV['MONGOHQ_PASSWORD'])
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost')
+  MongoMapper.database = "commonplace_stats_#{Rails.env}"
+end
