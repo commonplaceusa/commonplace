d6a3b1c8277fd53000ed46c8ced729c01062b82a
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-25 17:26:05 -0500

    Automatically scale up and down for daily digests

---
+++ lib/heroku_resque.rb
@@ -0,0 +1,15 @@
+module HerokuResque
+  class WorkerScaler
+    @queue = :server_management
+
+    def self.perform(num_dynos)
+      Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASSWORD']).ps_scale(ENV['HEROKU_APP'], :type => 'worker', :qty => num_dynos)
+    end
+  end
+
+  class WebScaler < WorkerScaler
+    def self.perform(num_dynos)
+      Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASSWORD']).ps_scale(ENV['HEROKU_APP'], :type => 'web', :qty => num_dynos)
+    end
+  end
+end
