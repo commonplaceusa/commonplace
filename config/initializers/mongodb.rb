97dc78634c3818a89f9bf181102ae6eca31f6c22
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-25 18:51:01 -0500

    Change MongoDB provider to MongoLab

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,10 +1,6 @@
-if ENV['MONGOHQ_HOST']
-  MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_HOST'], ENV['MONGOHQ_PORT'])
-  MongoMapper.database = ENV['MONGOHQ_DATABASE']
-  MongoMapper.database.authenticate(ENV['MONGOHQ_USERNAME'], ENV['MONGOHQ_PASSWORD'])
-else
-  puts "Please make sure that you have MongoDB installed and running locally"
-  puts "Otherwise, bad things will happen."
-  MongoMapper.connection = Mongo::Connection.new('localhost')
-  MongoMapper.database = "commonplace_stats_#{Rails.env}"
-end
+MongoMapper.config = {
+  Rails.env => {
+    'uri' => ENV['MONGOLAB_URI'] || 'mongodb://localhost/commonplace_stats'
+  }
+}
+MongoMapper.connect(Rails.env)
