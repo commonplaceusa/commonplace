a46a3cf800ea3a18f8313f33d1623d70fe79f9a4
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-27 12:35:47 -0500

    Stop storing entire email bodies and subjects; store class name instead

--- app/models/sent_email.rb
+++ app/models/sent_email.rb
@@ -2,12 +2,10 @@ class SentEmail
   include MongoMapper::Document
 
   key :recipient_email, String, :required => true
-  key :subject, String, :required => true
-  key :tag_list, String
   key :main_tag, String
   key :status, String
-  key :body, String
   key :originating_community_id, Integer
+  key :email_type, String
 
   timestamps!
 end
