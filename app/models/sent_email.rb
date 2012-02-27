0bc485961a63cce1a50fbad4cf5e909e105512e7
Jason Berlinsky <jberlinsky@gmail.com>
2012-02-27 11:51:44 -0500

    Add main tag to sent emails

--- app/models/sent_email.rb
+++ app/models/sent_email.rb
@@ -4,6 +4,7 @@ class SentEmail
   key :recipient_email, String, :required => true
   key :subject, String, :required => true
   key :tag_list, String
+  key :main_tag, String
   key :status, String
   key :body, String
   key :originating_community_id, Integer
