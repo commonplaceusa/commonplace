59ad2e721408bbb5020bc5eac4c5023156758dcb
Max Tilford <maxtilford@gmail.com>
2012-02-29 16:03:20 -0500

    Spring Cleaning: removing unused code and files
    Helpers:
    
    * removes unused OrganizerHelper
    * removes unused AccountsHelper
    * removes unused method tab_to from ApplicationHelper
    
    Locale:
    
    * locale is always :en
    * removes college locale versions of text
    * Community#locale is always :en
    * removes unused text in app/text/en
    
    Views:
    
    * removes unused view registrations/facebook_new.haml
    * removes used facebook_wall_post partial and text
    * removes unused partial bootstraps/_exceptional.haml
    * removes admin_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,5 @@
 module ApplicationHelper
 
-  def tab_to(name, options = {}, html_options = {}, &block)
-    options, html_options = name, options if block
-
-    html_options[:class] ||= ""
-    if current_page?(options) || current_page?(url_for(options) + ".json")
-      html_options[:class] += " selected_nav"
-    end
-    if block
-      link_to(options, html_options, &block)
-    else
-      link_to(name, options, html_options)
-    end
-  end
-
   def include_dummy_console
     raw <<script
 <script type='text/javascript'>
