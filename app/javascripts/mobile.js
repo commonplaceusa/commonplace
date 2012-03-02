04bed0bba8ca57e175d44f621d6d6b6d0b4e0e75
Max Tilford <maxtilford@gmail.com>
2012-03-02 16:32:49 -0500

    create basic mobile site structure

---
+++ app/javascripts/mobile.js
@@ -0,0 +1,11 @@
+//= require json2
+//= require jquery
+//= require underscore
+//= require mustache
+//= require backbone
+
+//= require views
+
+//= require_tree ../templates/mobile
+
+//= require_tree ./mobile
