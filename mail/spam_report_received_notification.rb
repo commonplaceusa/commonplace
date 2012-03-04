03f554b0a3df6f2f683b3882228d329d334ec23a
Jason Berlinsky <jberlinsky@gmail.com>
2012-03-03 23:27:16 -0500

    Add SpamReportReceivedNotification

---
+++ mail/spam_report_received_notification.rb
@@ -0,0 +1,39 @@
+class SpamReportReceivedNotification < MailBase
+
+  def initialize(user_id)
+    @user = User.find(user_id)
+  end
+
+  def subject
+    "Spam Report Received"
+  end
+
+  def reply_to
+    "postmaster@ourcommonplace.com"
+  end
+
+  def user
+    @user
+  end
+
+  def community_name
+    community.name
+  end
+
+  def community
+    user.community
+  end
+
+  def user_name
+    user.name
+  end
+
+  def short_user_name
+    user.first_name
+  end
+
+  def tag
+    'spam_report'
+  end
+
+end
