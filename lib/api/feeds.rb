92450f2f1630c2ed740032df65a85409b0925cb7
John Poje <jpojecp@gmail.com>
2012-03-06 13:54:48 -0500

    Refactor and Document the API

--- lib/api/feeds.rb
+++ lib/api/feeds.rb
@@ -1,45 +1,51 @@
 class API
-  class Feeds < Unauthorized
+  class Feeds < Base
 
-    before "/:feed_id/*" do |feed_id, stuff|
-      feed = feed_id =~ /[^\d]/ ? Feed.find_by_slug(feed_id) : Feed.find(feed_id)
-      halt [401, "wrong community"] unless in_comm(feed.community.id)
-    end
-    
     helpers do
 
-      def auth(feed)
-        authorize!
-        feed.get_feed_owner(current_account) or current_account.admin
+      # Finds the feed by params[:id] or halts with 404
+      def find_feed
+        @feed ||= case params[:id]
+                  when /^\d+/
+                    Feed.find_by_id(params[:id])
+                  else
+                    Feed.find_by_slug(params[:id])
+                  end
+        @feed || (halt 404)
       end
 
     end
     
-    put "/:feed_id" do |feed_id|
-      feed = Feed.find(feed_id)
-      halt [401, "unauthorized"] unless auth(feed)
-      halt [404, "errors"] unless feed.present?
+    # Updates the feed attributes
+    #
+    # Requires ownership
+    put "/:id" do
+      control_access :owner, find_feed
       
-      feed.name = request_body["name"]
-      feed.about = request_body["about"]
-      feed.kind = request_body["kind"]
-      feed.phone = request_body["phone"]
-      feed.website = request_body["website"]
-      feed.address = request_body["address"]
-      feed.slug = request_body["slug"]
-      feed.feed_url = request_body["rss"]
+      find_feed.name = request_body["name"]
+      find_feed.about = request_body["about"]
+      find_feed.kind = request_body["kind"]
+      find_feed.phone = request_body["phone"]
+      find_feed.website = request_body["website"]
+      find_feed.address = request_body["address"]
+      find_feed.slug = request_body["slug"]
+      find_feed.feed_url = request_body["rss"]
       
       if feed.save
         serialize(feed)
       else
-        [500, "could not save"]
+        [400, "could not save"]
       end
     end
 
-    post "/:feed_id/announcements" do |feed_id|
-      halt [401, "unauthorized"] unless auth(Feed.find(feed_id))
+    # Creates a feed announcement
+    # 
+    # Requires feed ownership
+    post "/:id/announcements" do
+      control_access :owner, find_feed
+      
       announcement = Announcement.new(:owner_type => "Feed",
-                                      :owner_id => feed_id,
+                                      :owner_id => find_feed.id,
                                       :subject => request_body['title'],
                                       :body => request_body['body'],
                                       :community => current_account.community,
@@ -52,15 +58,22 @@ class API
       end
     end
 
-    get "/:feed_id/announcements" do |feed_id|
+    # Gets a list of the feed's announcements
+    get "/:id/announcements" do
+      control_access :public
+      
       scope = Announcement.where("owner_id = ? AND owner_type = ?", feed_id, "Feed")
       serialize(paginate(scope.includes(:replies, :owner).reorder("GREATEST(replied_at,created_at) DESC")))
     end
 
-    post "/:feed_id/events" do |feed_id|
-      halt [401, "unauthorized"] unless auth(Feed.find(feed_id))
+    # Creates a feed event
+    # 
+    # Requires feed ownership
+    post "/:id/events" do
+      control_access :owner, find_feed
+
       event = Event.new(:owner_type => "Feed",
-                        :owner_id => feed_id,
+                        :owner_id => find_feed.id,
                         :name => request_body['title'],
                         :description => request_body['about'],
                         :date => request_body['date'],
@@ -78,14 +91,21 @@ class API
       end
     end
 
-    get "/:feed_id/events" do |feed_id|
+    # Gets the feed's events
+    get "/:id/events" do
+      control_access :public
+
       scope = Event.where("owner_id = ? AND owner_type = ?",feed_id, "Feed")
       serialize(paginate(scope.upcoming.includes(:replies).reorder("date ASC")))
     end
     
-    post "/:feed_id/essays" do |feed_id|
-      halt [401, "unauthorized"] unless auth(Feed.find(feed_id))
-      essay = Essay.new(:feed_id => feed_id,
+    # Creates a feed essay
+    # 
+    # Requires feed ownership
+    post "/:id/essays" do
+      control_access :owner, find_feed
+
+      essay = Essay.new(:feed_id => find_feed.id,
                         :user_id => current_user.id,
                         :subject => request_body["title"],
                         :body => request_body["body"])
@@ -96,26 +116,35 @@ class API
       end
     end
     
-    get "/:feed_id/essays" do |feed_id|
-      serialize(paginate(Feed.find(feed_id).essays))
+    # Gets the feed essays
+    get "/:id/essays" do
+      control_access :public
+
+      serialize(paginate(find_feed.essays))
     end
 
-    get "/:feed_id/subscribers" do |feed_id|
-      serialize(paginate(Feed.find(feed_id).subscribers))
+    # Gets the feed subscribers
+    get "/:id/subscribers" do
+      control_access :public
+
+      serialize(paginate(find_feed.subscribers))
     end
 
-    post "/:feed_id/invites" do |feed_id|
-      halt [401, "unauthorized"] unless auth(Feed.find(feed_id))
-      kickoff.deliver_feed_invite(request_body['emails'], Feed.find(feed_id))
+    # Send invites from the feed
+    post "/:id/invites" do
+      control_access :owner, find_feed
+
+      kickoff.deliver_feed_invite(request_body['emails'], find_feed)
       [200, ""]
     end
 
-    post "/:feed_id/messages" do |feed_id|
-      authorize!
+    # Send a message to the feed
+    post "/:id/messages" do
+      control_access :community_member, find_feed.community
       message = Message.new(:subject => request_body['subject'],
                             :body => request_body['body'],
                             :messagable_type => "Feed",
-                            :messagable_id => feed_id,
+                            :messagable_id => find_feed.id,
                             :user => current_account)
       if message.save
         [200, ""]
@@ -124,31 +153,37 @@ class API
       end
     end
     
-    get "/:feed_id" do |feed_id|
-      serialize(feed_id =~ /[^\d]/ ? Feed.find_by_slug(feed_id) : Feed.find(feed_id))
+    # Returns the serialized feed info
+    get "/:id" do
+      control_access :public
+      serialize find_feed
     end
 
-    get "/:feed_id/owners" do |feed_id|
-      authorize!
-      if Feed.find(feed_id).get_feed_owner(current_user)
-        serialize(Feed.find(feed_id).feed_owners)
+    # Gets the list of feed owners
+    get "/:id/owners" do
+      control_access :owner, find_feed
+
+      if find_feed.get_feed_owner(current_user)
+        serialize(find_feed.feed_owners)
       else
         [401, "unauthorized"]
       end
     end
     
-    get "/:feed_id/swipes" do |feed_id|
-      serialize(paginate(Feed.find(feed_id).swipes))
+    # Get the feed's swipes
+    get "/:id/swipes" do
+      control_access :public
+      serialize(paginate(find_feed.swipes))
     end
 
-    post "/:feed_id/owners" do |feed_id|
-      feed = Feed.find(feed_id)
-      halt [401, "unauthorized"] unless auth(feed)
+    # Add a new feed owner
+    post "/:id/owners" do
+      control_access :owner, find_feed
       params["emails"].split(",").each do |email|
         user = User.find_by_email(email.gsub(" ",""))
-        existing_owner = feed.get_feed_owner(user)
+        existing_owner = find_feed.get_feed_owner(user)
         if user and !existing_owner
-          owner = FeedOwner.new(:feed => feed,
+          owner = FeedOwner.new(:feed => find_feed,
                                 :user => user)
           owner.save
         end
@@ -156,27 +191,31 @@ class API
       [200, ""]
     end
 
-    delete "/:feed_id/owners/:id" do |feed_id, id|
-      halt [401, "unauthorized"] unless auth(Feed.find(feed_id))
-      owner = FeedOwner.find(id)
+    # Remove a feed owner
+    delete "/:id/owners/:owner_id" do
+      control_access :owner, find_feed
+
+      owner = FeedOwner.find(params[:owner_id])
       owner.destroy
     end
-    
-    post "/:feed_id/avatar" do |feed_id|
-      feed = Feed.find(feed_id)
-      halt [401, "unauthorized"] unless auth(feed)
-      feed.avatar = params[:avatar][:tempfile]
-      feed.avatar.instance_write(:filename, params[:avatar][:filename])
-      feed.save
-      serialize feed
+
+    # Create the feed avatar
+    post "/:id/avatar" do
+      control_access :owner, find_feed
+
+      find_feed.avatar = params[:avatar][:tempfile]
+      find_feed.avatar.instance_write(:filename, params[:avatar][:filename])
+      find_feed.save
+      serialize find_feed
     end
-    
-    delete "/:feed_id/avatar" do |feed_id|
-      feed = Feed.find(feed_id)
-      halt [401, "unauthorized"] unless auth(feed)
-      feed.avatar = nil
-      feed.save
-      serialize feed
+
+    # Remove the feed avatar
+    delete "/:id/avatar" do
+      control_access :owner, find_feed
+
+      find_feed.avatar = nil
+      find_feed.save
+      serialize find_feed
     end
 
   end
--- lib/api/integration.rb
+++ lib/api/integration.rb
@@ -1,33 +1,35 @@
 require 'openssl'
 class API
 
-  class Integration < Unauthorized
+  class Integration < Base
 
     helpers do 
-      def authentic_mailgun?(token, timestamp, signature)
-        return signature == OpenSSL::HMAC.hexdigest(
+
+      # Tells us whether the current_request came from Mailgun
+      #
+      # See http://documentation.mailgun.net/user_manual.html#securing-webhooks
+      def authentic_mailgun?
+        return params[:signature] == OpenSSL::HMAC.hexdigest(
           OpenSSL::Digest::Digest.new('sha256'),
           $MailgunAPIToken,
-          [timestamp, token].join)
+          [params[:timestamp], params[:token]].join)
       end
     end
-    
-    before /^\/mailgun/ do
-      if Rails.env.production? && 
-          !authentic_mailgun?(params[:token], params[:timestamp], params[:signature])
-        halt 200
-      end
-    end
-    
+
+    # Receive incoming replies from mailgun
     post "/mailgun/posts" do
+      authentic_mailgun? || (halt 200)
       begin
         MailgunPost.new(request_body).save
       ensure
         halt 200
       end
     end
-    
+
+    # When a user marks us as spam, Mailgun lets us know, and we don't
+    # send them any more emails
     post "/mailgun/disabled_emails" do
+      authentic_mailgun? || (halt 200)
       begin
         User.find_by_email(params[:recipient])
           .update_attribute(:post_receive_method, "Never")
