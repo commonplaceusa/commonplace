40c78620aec855f4c552f9459b8548f2c1349b90
Max Tilford <maxtilford@gmail.com>
2012-03-09 17:59:21 -0500

    add metadata to communities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120308214137) do
+ActiveRecord::Schema.define(:version => 20120309224352) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(:version => 20120308214137) do
     t.string   "google_docs_url"
     t.text     "discount_businesses"
     t.text     "feature_switches"
+    t.text     "metadata"
   end
 
   create_table "delayed_jobs", :force => true do |t|
