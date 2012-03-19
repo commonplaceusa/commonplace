86cf4171c2a93160d34b9f3bfc4b66331fca3b36
Max Tilford <maxtilford@gmail.com>
2012-03-19 14:51:31 -0400

    normalize routes in bootstrapper.rb

--- app/bootstrapper.rb
+++ app/bootstrapper.rb
@@ -2,7 +2,43 @@ class Bootstrapper < Sinatra::Base
 
   set :views, Rails.root.join("app", "bootstrapper")
 
+  helpers do
+    # Redirect various in-use domains to https://www.ourcommonplace.com/:community
+    def normalize_domains
+      return unless Rails.env.production?
+      return if request.host == "commonplace.herokuapp.com"
+      case request.host
+        
+      when %r{^www\.ourcommonplace\.com$}
+        return
+        
+      when %r{^assets\.}
+        return
+        
+      when %r{^ourcommonplace\.com$}
+        redirect "https://www.ourcommonplace.com#{request.path}"
+        return
+        
+      when %r{^(?:www\.)?([a-zA-Z]+)\.ourcommonplace\.com$}
+        if request.path == "/" || request.path == ""
+          redirect "https://www.ourcommonplace.com/#{$1}"
+        else
+          redirect "https://www.ourcommonplace.com#{request.path}"
+        end
+        
+      when %r{^(?:www\.)?commonplaceusa.com$}
+        case request.path
+        when %r{^/$}
+          redirect "https://www.ourcommonplace.com/about"
+        else
+          redirect "https://www.ourcommonplace.com#{request.path}"
+        end
+      end
+    end
+  end
+
   before do
+    normalize_domains
     @account = env["warden"].user(:user)
   end
 
