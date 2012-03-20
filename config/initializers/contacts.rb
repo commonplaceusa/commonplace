fd7106f92cf8cf18c85c72f2687de94eb873ec23
Jason Berlinsky <jberlinsky@gmail.com>
2012-03-20 17:49:22 -0400

    Add Windows Live support

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,6 +1,11 @@
 Contacts::configure({
   :yahoo => {
-    :consumer_key => ENV['yahoo_api_consumer_key'] || "***REMOVED***",
-    :consumer_secret => ENV['yahoo_api_consumer_secret'] || "***REMOVED***"
+    :consumer_key => ENV['yahoo_consumer_key'] || "***REMOVED***",
+    :consumer_secret => ENV['yahoo_consumer_secret'] || "***REMOVED***"
+  },
+  :windows_live => {
+    :application_id => ENV['windows_live_application_id'] || "***REMOVED***",
+    :secret_key => ENV['windows_live_secret_key'] || "***REMOVED***",
+    :privacy_policy_url => "http://www.ourcommonplace.com/"
   }
 })
