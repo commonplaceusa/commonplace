ac66eb5d912ad54bd983a204d513422dff938517
Jason Berlinsky <jberlinsky@gmail.com>
2012-03-20 22:00:38 -0400

    Add confirmation flash header back

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,11 +5,10 @@ class PasswordResetsController < Devise::PasswordsController
     user = User.find_by_email(params[:user][:email])
     if user
       user.send_reset_password_instructions
-      flash[:notice] = "An email will be sent to #{user.email} containing password reset instructions"
+      flash[:notice] = "An email will be sent to #{user.email} containing password reset instructions."
       redirect_to new_user_session_url
     else
       render :new
     end
   end
-
 end
