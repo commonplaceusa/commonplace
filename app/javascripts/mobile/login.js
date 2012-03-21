736ee657dd66a425dcdda9e5607e0574342b3492
Lexi Ross <the.circ@gmail.com>
2012-03-21 16:57:09 -0400

    Added API calls for registration, login, and Facebook login. Added form validation for registration and login. Created registration view and template.

--- app/javascripts/mobile/login.js
+++ app/javascripts/mobile/login.js
@@ -1,29 +1,55 @@
 var LoginView = CommonPlace.View.extend({
     template: "mobile.login",
-    events: {"submit form":"login"},
+    events: {
+        "click a#register":"register",
+        "submit form":"login",
+    },
+
+    register: function(e) {
+                  e.preventDefault();
+                  var RegisterView = new RegisterView({el:$('#main')});
+                  RegisterView.render();
+              },
 
     login: function(e) {
-                // prevent default
-               // authenticate
-        
-                var lat = null;
-                var lng = null;
-                if (geo_position_js.init()) {
-                    geo_position_js.getCurrentPosition(function(loc) {
-                        lat = loc.coords.latitude;
-                        lng = loc.coords.longitude;
-                    });
-                }
+                e.preventDefault();
+                var email = $("#email").val();
+                var password = $("#password").val();
+                $.ajax({ 
+                    url: "/api/sessions", 
+                    contentType: "application/json", 
+                    data: JSON.stringify({ email: email, password: password }), 
+                    dataType: "json", 
+                    success: function(response) { 
+                        window.full_name = response.full_name;
+                        var lat = null;
+                        var lng = null;
+                        if (geo_position_js.init()) {
+                            geo_position_js.getCurrentPosition(function(loc) {
+                                lat = loc.coords.latitude;
+                                lng = loc.coords.longitude;
+                            });
+                        }
 
-                new Recommendations([]).fetch({
-                    data: {
-                              latitude: lat,
-                              longittude: lng
-                          },
-                    success: function(recommendations) {
-                                recommendationsView = new RecommendationsView({el:$('#main'),collection:recommendations});
-                                recommendationsView.render();
-                             }
+                        new Recommendations([]).fetch({
+                            data: {
+                                      latitude: lat,
+                                      longitude: lng
+                                  },
+                            success: function(recommendations) {
+                                        recommendationsView = new RecommendationsView({el:$('#main'),collection:recommendations});
+                                        recommendationsView.render();
+                                     }
+                        });
+                    
+                    }, 
+                    error: function() { 
+                                console.log("Error!");
+                                $("#message").show();
+                           } 
                 });
+        
     }
+
+
 });
