987a855ee6f19d927040ade6a6e27003855fce0d
Jason Berlinsky <jberlinsky@gmail.com>
2012-03-28 12:05:43 -0400

    Seems to fix forgot password issues. Monkey-patches in newer Devise password reset controller.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,4 +11,17 @@ class PasswordResetsController < Devise::PasswordsController
       render :new
     end
   end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(params[resource_name])
+
+    if resource.errors.empty?
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message(:notice, flash_message) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with resource, :location => after_sign_in_path_for(resource)
+    else
+      respond_with resource
+    end
+  end
 end
