149dc65394eb356cf95eff00e73664e9fdfce2c3
Jason Berlinsky <jberlinsky@gmail.com>
2012-03-29 14:05:53 -0400

    Finalize revert

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,8 +1,3 @@
 class UserSessionsController < Devise::SessionsController
   layout "sign_in"
-  before_filter :load_communities
-
-  def load_communities
-    @communities = Community.public.sort_by &:name
-  end
 end
--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -9,6 +9,7 @@
 //= require mustache
 //= require backbone
 //= require autoresize
+//= require dropkick
 //= require truncator
 //= require ajaxupload
 //= require placeholder
@@ -130,6 +131,7 @@ var Application = Backbone.Router.extend({
 
   find_neighbors: function(c) { this.showPage("find_neighbors"); },
   find_neighbors_with_callback: function(c, token, verifier) {
+    console.log("Finding with callback");
     this.pages.find_neighbors.is_callback = true;
     this.pages.find_neighbors.callback_token = token;
     this.pages.find_neighbors.callback_verifier = verifier;
