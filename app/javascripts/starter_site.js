e58a906b723855c115a69aab7494069a0492607e
Max Tilford <maxtilford@gmail.com>
2012-03-30 19:18:33 -0700

    fixes starter_site header

--- app/javascripts/starter_site.js
+++ app/javascripts/starter_site.js
@@ -1,16 +1,17 @@
-//= require starter_site/application
-//= require starter_site/easing
-//= require starter_site/bubbles
-//= require starter_site/tabs
-//= require starter_site/slideshow
+//= require jquery
 //= require dropkick
 //= require json2
-//= require jquery
 //= require underscore
 //= require postjson
 //= require mustache
 //= require backbone
 //= require views
 //= require models
+//= require starter_site/application
+//= require starter_site/easing
+//= require starter_site/bubbles
+//= require starter_site/tabs
+//= require starter_site/slideshow
+
 //= require_tree ./shared
 //= require_tree ../templates
