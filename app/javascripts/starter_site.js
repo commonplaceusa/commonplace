099d763c6f3ce7b66ba92c0528361f2ca0a2e709
Jason Berlinsky <jberlinsky@gmail.com>
2012-04-05 15:38:53 -0400

    Merge conflicts with backbone-login

--- app/javascripts/starter_site.js
+++ app/javascripts/starter_site.js
@@ -1,6 +1,17 @@
+//= require jquery
+//= require dropkick
+//= require json2
+//= require underscore
+//= require postjson
+//= require mustache
+//= require backbone
+//= require views
+//= require models
 //= require starter_site/application
 //= require starter_site/easing
 //= require starter_site/bubbles
 //= require starter_site/tabs
 //= require starter_site/slideshow
-//= require dropkick
+
+//= require_tree ./shared
+//= require_tree ../templates
