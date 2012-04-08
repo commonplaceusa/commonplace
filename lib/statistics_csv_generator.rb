2996ab4f61fc912b7131f7d153685a7a9381a4f4
Jason Berlinsky <jberlinsky@gmail.com>
2012-04-08 15:51:02 -0400

    Add remote worker

--- lib/statistics_csv_generator.rb
+++ lib/statistics_csv_generator.rb
@@ -3,22 +3,47 @@ class StatisticsCsvGenerator
   #
   @queue = :statistics
 
-  def self.perform(days = 30, globally = true)
-    Resque.redis.set("statistics:meta:available", false)
+  def self.perform_remotely
+    # Spin up EC2 instance
+    #   The instance should run the remote connection on spin-up
+    #   And should shut itself down automatically
+    ec2 = AWS::EC2::Base.new(:access_key_id => ENV['EC2_KEY_ID'], :secret_access_key => ENV['EC2_KEY_SECRET'])
+    ec2.start_instances({
+      :instance_id => [ENV['EC2_STATS_CRUNCHER_INSTANCE_ID']]
+    })
+  end
+
+  def self.perform(days = 30, globally = true, redis = Resque.redis)
+    redis.set("statistics:meta:available", false)
     Community.all.each do |c|
-      Resque.redis.set("statistics:csv:#{c.slug}", nil)
-      Resque.redis.set("statistics:hashed:#{c.slug}", nil)
+      redis.set("statistics:csv:#{c.slug}", nil)
+      redis.set("statistics:hashed:#{c.slug}", nil)
     end
-    Resque.redis.set("statistics:csv:global", nil) if globally
-    Resque.redis.set("statistics:hashed:global", nil) if globally
-    Resque.redis.set("statistics:csv:meta:date", Date.today.to_s)
-    Resque.redis.set("statistics:hashed:meta:date", Date.today.to_s)
-    Community.all.select { |c| c.core }.each do |c|
-      StatisticsAggregator.generate_statistics_csv_for_community(c, days)
-      StatisticsAggregator.generate_hashed_statistics_for_community(c)
+    redis.set("statistics:csv:global", nil) if globally
+    redis.set("statistics:hashed:global", nil) if globally
+    redis.set("statistics:meta:date", Date.today.to_s)
+    Community.where("core = true").each do |c|
+      StatisticsAggregator.generate_statistics_csv_for_community(c, days, redis)
+      StatisticsAggregator.generate_hashed_statistics_for_community(c, redis)
     end
-    StatisticsAggregator.csv_statistics_globally(days) if globally
-    StatisticsAggregator.generate_hashed_statistics_globally if globally
-    Resque.redis.set("statistics:meta:available", true)
+    StatisticsAggregator.csv_statistics_globally(days, redis) if globally
+    StatisticsAggregator.generate_hashed_statistics_globally(redis) if globally
+    redis.set("statistics:meta:available", true)
+  end
+
+  def self.copy_redis_values(original_redis, new_redis)
+    keys = [
+            "statistics:csv:global",
+            "statistics:hashed:global",
+            "statistics:meta:date",
+            "statistics:meta:available"
+    ]
+    Community.where("core = true").each do |c|
+      keys.prepend "statistcs:csv:#{c.slug}"
+      keys.prepend "statistics:hashed:#{c.slug}"
+    end
+    keys.each do |redis_key|
+      new_redis.set(redis_key, original_redis.get(redis_key))
+    end
   end
 end
