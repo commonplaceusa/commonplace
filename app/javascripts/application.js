f4f15b73bb3c308c3166317d2c4c2045400afa26
Jason Berlinsky <jberlinsky@gmail.com>
2012-04-16 19:14:11 -0400

    Should fix message wire loading

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -180,6 +180,7 @@ var Application = Backbone.Router.extend({
 
   messageUser: function(c, id) {
     this.showPage("community");
+    this.pages.community.lists.switchTab("all_posts");
     var user = new User({ links: { self: "/users/" + id } });
     user.fetch({
       success: function() {
