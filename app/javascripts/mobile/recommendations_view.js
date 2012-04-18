471359da8d1f1e17cafa34b920701ae0f29591b8
Lexi Ross <the.circ@gmail.com>
2012-04-18 17:27:17 -0400

    added id attribute to recommendations

--- app/javascripts/mobile/recommendations_view.js
+++ app/javascripts/mobile/recommendations_view.js
@@ -8,7 +8,8 @@ var RecommendationsView = CommonPlace.View.extend({
   aroundRender: function(render) {
     var self = this;
     geo_position_js.getCurrentPosition(function(loc) {
-      self.location = loc;
+      window.loc = loc;
+      console.log(loc);
       render();
     });
 
@@ -23,10 +24,12 @@ var RecommendationsView = CommonPlace.View.extend({
     },
 
     distanceTo: function(recommendation) {
-      var lat1 = self.location.coords.latitude;
-      var lng1 = self.location.coords.longitude;
+        console.log(window.loc);
+      var lat1 = window.loc.coords.latitude;
+      var lng1 = window.loc.coords.longitude;
       var lat2 = recommendation.latitude;
-      var lng2 = recommendation.longtitude;
+      var lng2 = recommendation.longitude;
+      console.log("lat1="+lat1+",lat2="+lat2+"lng1="+lng1+"lng2="+lng2);
       var p1 = new LatLon(lat1,lng1);
       var p2 = new LatLon(lat2,lng2);
       var dist_km = p1.distanceTo(p2);
@@ -38,12 +41,14 @@ var RecommendationsView = CommonPlace.View.extend({
     },
 
     events: {
-                "click .item":function(e) {
+                "click .recommendation":function(e) {
                     e.preventDefault();
+                    console.log($(e.currentTarget).data('id'));
                     var id = $(e.currentTarget).data('id');
                     var rec = this.collection.get(id);
-                    var ImInView = new ImInView({el:$('#main'), model:rec});
-                    ImInView.render();
+                    console.log("rec:"+rec);
+                    var imInView = new ImInView({el:$('#main'), model:rec});
+                    imInView.render();
                 }
             }
 });
