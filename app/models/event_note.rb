1dfc5c36447e4a56db3f2f8c2bec4e42338a6b3d
Max Tilford <maxtilford@gmail.com>
2012-04-18 13:28:12 -0700

    add POST /api/events/:id/notes and GET /api/events/:id/notes

--- app/models/event_note.rb
+++ app/models/event_note.rb
@@ -1,7 +1,24 @@
 class EventNote < ActiveRecord::Base
 
+  extend ActsAsApi::Base
+
+  def model_name
+    "event_note"
+  end
+
   belongs_to :user
   belongs_to :event
   
+  def author
+    user.name
+  end
+
+  acts_as_api
+
+  api_accessible :default do |t|
+    t.add :body
+    t.add :author
+    t.add :created_at, :as => :timestamp
+  end
   
 end
