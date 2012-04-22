7b75b1b211334f2a5c9b53a27a9530d6131fc4d9
Jason Berlinsky <jberlinsky@gmail.com>
2012-04-22 15:52:21 -0400

    Clear private message badge

--- app/javascripts/inbox_page.js
+++ app/javascripts/inbox_page.js
@@ -24,6 +24,7 @@ var InboxPage = CommonPlace.View.extend({
         });
         this.$("div#message-list.loading").removeClass("loading");
         this.listview.render();
+        window.HeaderNav.clearUnreadMessages();
       }, this)
     });
   },
--- app/javascripts/shared/header_nav.js
+++ app/javascripts/shared/header_nav.js
@@ -2,10 +2,16 @@ var HeaderNav = CommonPlace.View.extend({
   template: "shared.header-nav",
   className: "nav",
 
+  unreadMessageCount: 0,
+
   afterRender: function() {
-    if (this.hasUnreadMessages()) {
-      this.$(".inbox .file").badger("" + this.unreadMessages());
-    }
+    this.updateUnreadMessages();
+    this.updateUnreadMessagesBadge();
+    var self = this;
+    CommonPlace.account.on("sync", function() {
+      self.updateUnreadMessages();
+      self.updateUnreadMessagesBadge();
+    });
   },
 
   slug: function() {
@@ -15,29 +21,44 @@ var HeaderNav = CommonPlace.View.extend({
       return CommonPlace.community.get("slug");
     }
   },
-  
+
   invite_url: function() { return "/" + this.slug() + "/invite"; },
-  
+
   inbox_url: function() { return "/" + this.slug() + "/inbox"; },
-  
+
   avatar_url: function() { return CommonPlace.account.get("avatar_url"); },
-  
+
   account_url: function() { return "/" + this.slug() + "/account"; },
-  
+
   faq_url: function() { return "/" + this.slug() + "/faq"; },
-  
+
   short_name: function() { return CommonPlace.account.get("short_name"); },
-  
+
   neighbor_discount_url: function() {},
-  
+
   privacy_url: function() { return "/" + this.slug() + "/privacy"; },
-  
+
   feeds: function() { return CommonPlace.account.get("feeds"); },
-  
+
   activityIsEnabled: function() { return this.isActive("CPCreditsHeaderView"); },
 
-  unreadMessages: function() { return CommonPlace.account.get("unread"); },
+  unreadMessages: function() { return this.unreadMessageCount; },
 
-  hasUnreadMessages: function() { return this.unreadMessages() !== 0; }
+  updateUnreadMessages: function() { this.unreadMessageCount = CommonPlace.account.get("unread"); },
+
+  hasUnreadMessages: function() { return this.unreadMessages() !== 0; },
+
+  clearUnreadMessages: function() {
+    this.unreadMessageCount = 0;
+    this.updateUnreadMessagesBadge();
+  },
+
+  updateUnreadMessagesBadge: function() {
+    if (this.hasUnreadMessages()) {
+      this.$(".inbox .file").badger("" + this.unreadMessages());
+    } else {
+      this.$(".inbox .file").badger("");
+    }
+  }
 
 });
