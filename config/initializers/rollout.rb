d71173e68afdf49bbb7c373e430088b25805a5dc
Jason Berlinsky <jberlinsky@gmail.com>
2012-04-25 18:55:52 -0400

    Make Rollout work with Mocked Redis

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,35 +1,38 @@
-uri = if Rails.env.development? || Rails.env.test?
+uri = if Rails.env.development?
         URI.parse("localhost:6379")
       elsif ENV["REDISTOGO_URL"].present?
         URI.parse(ENV["REDISTOGO_URL"])
+      else
+        ""
       end
 
-if uri
+if uri.present?
   $rollout = Rollout.new(Redis.new(:host => uri.host, 
                                    :port => uri.port, 
                                    :password => uri.password, 
                                    :thread_safe => true))
-
-
   $rollout.define_group(:alpha) do |user|
     user.community.slug == "CommonPlace" or user.community.slug == "test"
   end
-  
+
   $rollout.define_group(:harrisonburg) do |user|
     user.community.slug == "harrisonburg"
   end
-  
+
   $rollout.define_group(:fallschurch) do |user|
     user.community.slug == "fallschurch" 
   end
-  
+
   $rollout.define_group(:discount) do |user|
     if user == nil or user.community == nil
       return false
     end
     user.present? and user.community.present? and user.community.slug == "Vienna" or user.community.slug == "test"
   end
-  
+
   $rollout.activate_group(:facebook_invite, :alpha)
   $rollout.activate_group(:good_neighbor_discount, :discount)
+
+else
+  $rollout = Rollout.new(MockRedis.new)
 end
