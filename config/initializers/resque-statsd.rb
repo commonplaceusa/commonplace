52851c63cfa73988e29f463c394d820bfe5426b1
Jason Berlinsky <jberlinsky@gmail.com>
2012-05-03 11:31:34 -0400

    Add init

---
+++ config/initializers/resque-statsd.rb
@@ -0,0 +1,8 @@
+require 'enqueue_time'
+
+module Resque
+  include Resque::EnqueueTime
+end
+
+StatsdHelper.graphite_host = ENV['GRAPHITE_HOST']
+StatsdHelper.namespace = "#{ENV['NEW_RELIC_APPNAME']}.#{Rails.env}.resque"
