6f8785fc1a88acb71c371a2b9a7f3f2c55dd255c
Jason Berlinsky <jberlinsky@gmail.com>
2012-05-04 01:04:20 -0400

    Fix namespace

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,4 +1,4 @@
 if Rails.env.production?
   $statsd = Statsd.new(ENV['GRAPHITE_HOST'] || 'localhost', ENV['GRAPHITE_PORT'] || 8125)
-  $statsd.namespace = "data.stats.commonplace.#{Rails.env}"
+  $statsd.namespace = "commonplace.#{Rails.env}"
 end
