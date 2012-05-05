5cd927e925d095af8d4d3bfb551c0e7e005c1661
Jason Berlinsky <jberlinsky@gmail.com>
2012-05-04 23:34:39 -0400

    Auto-scale NotificationWorkers

--- lib/heroku_resque.rb
+++ lib/heroku_resque.rb
@@ -7,6 +7,12 @@ module HerokuResque
     end
   end
 
+  class NotificationWorkerScaler < WorkerScaler
+    def self.perform(num_dynos)
+      Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASSWORD']).ps_scale(ENV['HEROKU_APP'], :type => 'notification_worker', :qty => num_dynos)
+    end
+  end
+
   class WebScaler < WorkerScaler
     def self.perform(num_dynos)
       Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PASSWORD']).ps_scale(ENV['HEROKU_APP'], :type => 'web', :qty => num_dynos)
