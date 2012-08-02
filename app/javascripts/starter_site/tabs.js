eef28441319709436a0527f369e2ef2aabeadfd5
Jason Berlinsky <jberlinsky@gmail.com>
2012-08-02 07:53:33 -0400

    Refactor about page tab logic, allowing Rails to change the active tab

--- app/javascripts/starter_site/tabs.js
+++ app/javascripts/starter_site/tabs.js
@@ -1,27 +1,21 @@
 $(document).ready(function(){
   $('.nav li a').click(function(){
-    
+
     $('.nav li').removeClass('selected');
-    
+
     $(this).parent().addClass('selected');
-    
+
     $('.slide').hide();
-    
-    if($(this).attr('href')=="our-mission"){
-      $('#our-mission').show();
-    }
-    else if($(this).attr('href')=="our-story"){
-      $('#our-story').show();
-    }
-    else if($(this).attr('href')=="our-platform"){
-      $('#our-platform').show();
-    }
-    else if($(this).attr('href')=="press"){
-      $('#press').show();
-    }
-    else if($(this).attr('href')=="nominate"){
-      $('#nominate').show();
-    }
+
+    activateTab($(this).attr('href'));
     return false;
   });
 });
+
+function activateTab(tab_name) {
+  $(".nav li").removeClass('selected');
+  $("a[href=" + tab_name + "]").parent().addClass('selected');
+  $(".slide").hide();
+  $('#' + tab_name).show();
+  return false;
+}
