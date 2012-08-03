0322fcadf8ae434e63d672d734f5730424d26ec1
Jason Berlinsky <jberlinsky@gmail.com>
2012-08-02 23:42:34 -0400

    Remove old JS files

--- app/javascripts/starter_site.js
+++ app/javascripts/starter_site.js
@@ -8,10 +8,7 @@
 //= require views
 //= require models
 //= require starter_site/application
-//= require starter_site/easing
-//= require starter_site/bubbles
 //= require starter_site/tabs
-//= require starter_site/slideshow
 
 //= require_tree ./shared
 //= require_tree ../templates
