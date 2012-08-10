f6425f8d632fd787d17a6ff9255995271c11a1b2
Bryan Kerrigan <bryankerrigan@gmail.com>
2012-08-10 15:38:36 -0700

    Revert "Fixing the placeholder text in the nominate a civic hero form for browsers that don't support it"
    Rverting this commit because Jason has a fix that takes care of all the tabs.
    
    This reverts commit adc58894f004772bbe1c350cec7414d94b3b24e6.

--- app/javascripts/registration/nominate_civic_hero_form.js
+++ app/javascripts/registration/nominate_civic_hero_form.js
@@ -2,47 +2,16 @@ var NominateCivicHeroForm = CommonPlace.View.extend({
   template: "registration.nominate_civic_hero",
 
   events: {
-    "click button.next-button": "submit",
-    "blur :input": "inputBlur",
-    "focus :input": "inputFocus"
+    "click button.next-button": "submit"
   },
 
   initialize: function() {
     this.data = {};
-    this.placeholder_support = false;
-    var test = document.createElement("input");
-    if('placeholder' in test) this.placeholder_support = true;
-  },
-
-  afterRender: function () {
-    $(":input").blur();
-  },
-
-  inputFocus: function (e) {
-    if (e) { e.preventDefault(); }
-
-    if (!this.placeholder_support) {
-      if ($(e.target).attr('placeholder') != "" && $(e.target).val() == $(e.target).attr('placeholder')) {
-        $(e.target).val("").removeClass("hasPlaceholder");
-      }
-    }
-  },
-      
-  inputBlur: function (e) {  
-    if (e) { e.preventDefault(); }
-
-    if (!this.placeholder_support) {
-
-      if ($(e.target).attr('placeholder') != '' && ($(e.target).val() == '' || $(e.target).val() == $(e.target).attr('placeholder'))) {
-        $(e.target).val($(e.target).attr('placeholder')).addClass('hasPlaceholder');
-      }
-    }
   },
 
   submit: function(e) {
     if (e) { e.preventDefault(); }
 
-    $(e.target).find('.hasPlaceholder').each(function() { $(e.target).val(''); });
     var self = this;
     _.each(["nominee_name", "nominee_email", "nomination_reason", "nominator_name", "nominator_email"], function(data_point) {
       self.data[data_point] = self.$("[name=" + data_point + "]").val();
