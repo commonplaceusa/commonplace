97494f754ecf16a9d0f7483dafcdaa996d76e1ee
Jason Berlinsky <jberlinsky@gmail.com>
2012-08-10 19:00:03 -0400

    Remove validations in civic hero nominations and civic leader applications

--- app/models/civic_hero_nomination.rb
+++ app/models/civic_hero_nomination.rb
@@ -1,5 +1,3 @@
 class CivicHeroNomination < ActiveRecord::Base
   attr_accessible :nominator_email, :nominator_name, :nominee_email, :nominee_name, :reason, :community_id
-
-  validates_presence_of :nominator_email, :nominator_name, :nominee_email, :nominee_name, :reason, :community_id
 end
--- app/models/civic_leader_application.rb
+++ app/models/civic_leader_application.rb
@@ -1,5 +1,3 @@
 class CivicLeaderApplication < ActiveRecord::Base
   attr_accessible :email, :name, :reason, :community_id
-
-  validates_presence_of :email, :name, :reason, :community_id
 end
