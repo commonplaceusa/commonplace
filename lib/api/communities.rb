9ac7d76d768ce37f0a1381299473cdf413a1c779
Jeffrey Lu <Ziruga@gmail.com>
2012-08-29 18:34:04 -0400

    Take care of the strange case in facebook connect where a town is not found in address verification

--- lib/api/communities.rb
+++ lib/api/communities.rb
@@ -737,6 +737,9 @@ CONDITION
     # TODO: Instead of loosening the similarity cut-off,
     # make sure they type in a "proper" address (ie begins with some #)
     get "/:id/address_approximate" do
+      if find_community.nil? || Community.find_by_name("Lexington").nil?
+        return serialize([-1])
+      end
       if find_community.launch_date < Community.find_by_name("Lexington").launch_date
         return serialize([-1])
       end
