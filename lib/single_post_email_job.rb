9cde6e8f62bfa0997ad3f1446fd8ac045bffde8e
Bryan Kerrigan <bryankerrigan@gmail.com>
2012-09-24 15:06:04 -0700

    Adding the single post emails to the Resque scheduler

--- lib/single_post_email_job.rb
+++ lib/single_post_email_job.rb
@@ -2,11 +2,13 @@ class SinglePostEmailJob
   @queue = :single_post_email
 
   def self.perform
-    date = DateTime.now.utc.to_s(:db)
+    now = DateTime.now.utc
+    end_date = now.to_s(:db)
+    start_date = now.ago(5*60*60).to_s(:db)  # go back 5 hours
 
     Community.all.each do |community|
       Exceptional.rescue do
-        Resque.enqueue(CommunitySinglePostEmailJob, community.id, date)
+        Resque.enqueue(CommunitySinglePostEmailJob, community.id, start_date, end_date)
       end
     end
   end
