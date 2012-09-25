ef3c09a7fbbf6c92d1506cbb50cf33f2b3941971
markmalazarte <mark@markmalazarte.com>
2012-09-25 14:11:39 -0400

    Integrating Tilford's email templates

--- mail/reply_notification.rb
+++ mail/reply_notification.rb
@@ -1,27 +1,18 @@
-class ReplyNotification < MailBase
-  
+class ReplyNotification < PostNotification
+
+  self.template_file = PostNotification.template_file  
+
   def initialize(reply_id, user_id)
     @reply, @user = Reply.find(reply_id), User.find(user_id)
+    @post = @reply.repliable
   end
 
   def subject
     subject_line
   end
 
-  def community
-    replier.community
-  end
-
-  def community_name
-    community.name
-  end
-
   def reply
     @reply
-  end
-
-  def repliable
-    reply.repliable
   end
 
   def user
@@ -29,68 +20,61 @@ class ReplyNotification < MailBase
   end
   
   def reply_to
-    "reply+#{repliable.class.name.downcase}_#{repliable.id}@ourcommonplace.com"
+    "reply+#{@post.class.name.downcase}_#{@post.id}@ourcommonplace.com"
   end
 
   def user_name
     user.name
   end
 
-  def replier
+  def author
     reply.user
   end
 
-  def reply_body
-    markdown reply.body
+  def body
+    reply.body
   end
 
-  def replier_avatar_url
-    replier.avatar_url
+  def title
+    @post.subject
   end
 
-  def repliable_subject
-    repliable.subject
-  end
-  
   def new_message_url
-    message_user_url(replier.id)
+    message_user_url(author.id)
   end
 
   def repliable_url
-    case repliable
-    when Event then show_event_url(repliable.id)
-    when GroupPost then show_group_post_url(repliable.id)
-    when Announcement then show_announcement_url(repliable.id)
-    when Post then show_post_url(repliable.id)
-    when Message then show_message_url(repliable.id)
+    case @post
+    when Event then show_event_url(@post.id)
+    when GroupPost then show_group_post_url(@post.id)
+    when Announcement then show_announcement_url(@post.id)
+    when Post then show_post_url(@post.id)
+    when Message then show_message_url(@post.id)
     end
   end
 
-  def replier_name
-    replier.name
-  end
-
-  def short_replier_name
-    replier.first_name
-  end
-
-  def repliable_is_a_message?
-    repliable.is_a? Message
-  end
-  
   def subject_line
-    if @user.community.is_college
-      post_subject = "a hall board post"
-    else
-      post_subject = "a post"
-    end
-    post_type = case repliable
-                when Message then "a private message"
-                when Post then post_subject
-                when Event then "an event"
-                when Announcement then "an announcement"
-                end
-    "#{replier_name} just replied to #{post_type} on CommonPlace."
+
+    adverb = author == @post.user ? "just" : "also"
+    possesive = author == @post.user ? "your" : @post.user.name + "'s"
+    post_text = 
+      case @post
+      when Message then "private message"
+      when Post 
+        case @post.category
+        when "offers" then "offer"
+        when "neighborhood" then "neighborhood update"
+        when "meetups" then "meet-up proposal"
+        when "help" then "request"
+        when "publicity" "post"
+        when "other" then "post"
+        end
+      when Event then "event"
+      when Announcement then "an announcement"
+      when GroupPost then "a post on the #{community_name} #{@post.group.name} Group"
+      end
+    
+    "#{author_name} #{adverb} replied to #{possesive} #{post_text} on CommonPlace."
   end
 
   def tag
