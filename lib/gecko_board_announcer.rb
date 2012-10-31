4af64d7d0a1728f2d9a29be19d0b42bf330209d2
Jason Berlinsky <jberlinsky@gmail.com>
2012-10-31 11:17:29 -0400

    Add a few more statistics to Leftronic

--- lib/gecko_board_announcer.rb
+++ lib/gecko_board_announcer.rb
@@ -9,10 +9,11 @@ class GeckoBoardAnnouncer
       populations << { community.name => community.users.count }
     end
     dashboard.leaderboard("Users by Community", populations)
-    # dashboard.number("Users on Network in #{community.name}", community.users.count)
     dashboard.number("Daily Bulletins Sent Today", DailyStatistic.value("daily_bulletins_sent"))
-    dashboard.number("Single Post Emails Sent Today", DailyStatistic.value("single_posts_sent"))
     dashboard.number("Daily Bulletins Opened Today", DailyStatistic.value("daily_bulletins_opened"))
+    dashboard.number("Daily Bulletin Open Rate Today", DailyStatistic.value("daily_bulletins_opened").to_f / DailyStatistic.value("daily_bulletins_sent").to_f)
+    dashboard.number("Single Post Emails Sent Today", DailyStatistic.value("single_posts_sent"))
     dashboard.number("Single Post Emails Opened Today", DailyStatistic.value("single_posts_opened"))
+    dashboard.number("Single Post Email Open Rate Today", DailyStatistic.value("single_posts_opened").to_f / DailyStatistic.value("single_posts_sent").to_f)
   end
 end
