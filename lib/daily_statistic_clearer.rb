9b34db516e75cf09f32f2546434e69ea9c5b53bc
Jason Berlinsky <jberlinsky@gmail.com>
2012-11-03 21:36:24 -0400

    Fix statistic cleanup

--- lib/daily_statistic_clearer.rb
+++ lib/daily_statistic_clearer.rb
@@ -1,9 +1,9 @@
 class DailyStatisticClearer
   @queue = :statistics_cleansing
 
-  def self.perform!(redis = Resque.redis)
+  def self.perform(redis = Resque.redis)
     redis.keys(StatisticsIncrementor::NAMESPACE + "*").each do |key|
-      redis.set(StatisticsIncrementor::NAMESPACE + key, 0)
+      redis.set(key, 0)
     end
   end
 end
