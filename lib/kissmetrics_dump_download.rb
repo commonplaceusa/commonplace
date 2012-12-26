826393a9c5efb490bf7d02c8a949f3378180f918
Jason Berlinsky <jberlinsky@gmail.com>
2012-12-26 01:12:58 -0500

    Update dump downloader

--- lib/kissmetrics_dump_download.rb
+++ lib/kissmetrics_dump_download.rb
@@ -15,10 +15,42 @@ class KissmetricsDumpDownload
 
     AWS::S3::Base.establish_connection!(conn)
 
-    AWS::S3::Bucket.find('cpkissmetrics').objects.each do |obj|
+    bucket = AWS::S3::Bucket.find("cpkissmetrics")
+    files = [bucket['index.csv']]
+
+    files.each do |obj|
       filename = obj.key.gsub("/", "_")
       File.open("km_dump/#{filename}", 'w') { |f| f.write(obj.value) }
       puts "Processed #{filename}"
+    end
+
+    unless File.exists? 'km_dump/index.csv'
+      raise "Did not download index manifest. Aborting..."
+    end
+
+    index_file = File.read('km_dump/index.csv').split("\n")
+    index_file.shift(1)
+
+    index_file.each do |index_file_line|
+
+      original_filename = index_file_line.split(",").first
+      filename = original_filename.gsub("/", "_")
+      puts "Processing #{original_filename} (#{filename})..."
+      obj = bucket[original_filename]
+      unless obj.present?
+        obj = AWS::S3::S3Object.find(original_filename, 'cpkissmetrics')
+        unless obj.present?
+          puts "Object is nill. Skipping."
+          next
+        end
+      end
+      File.open("km_dump/#{filename}", 'w') { |f| f.write(obj.value) }
+      puts "Processed #{filename}"
+    end
+
+    last_json_file = index_file.last.split(",").first.split("/").last
+    unless File.exists? "km_dump/#{last_json_file}"
+      raise "Kissmetrics dump download was corrupted, since #{last_json_file} was not downloaded. Aborting."
     end
 
     puts "Done"
