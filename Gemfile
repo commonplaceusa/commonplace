93b443da8b7e8c70e0fa1c3d474a50f3772651fd
Jason Berlinsky <jberlinsky@gmail.com>
2012-12-31 02:11:54 -0500

    Beef up Kissmetrics import

--- Gemfile
+++ Gemfile
@@ -90,7 +90,7 @@ gem 'require_all', '~> 1.2.1' # require all ruby files in a directory
 
 # Analytics
 gem 'km'
-gem 'km-db', :git => "https://github.com/Jberlinsky/km-db.git", :ref => "1dbf2dbf60b34a2b6fa2c473368f4ee5b2eef8ae"
+gem 'km-db', :git => "https://github.com/Jberlinsky/km-db.git", :ref => "f530c8b67223cde2e076c3516ef875cfed32c025"
 gem 'mysql2'
 
 group :assets do
--- lib/kissmetrics_dump_download.rb
+++ lib/kissmetrics_dump_download.rb
@@ -1,11 +1,13 @@
 class KissmetricsDumpDownload
   @queue = :statistics
 
+  REDIS_LAST_PROCESSED_FILE = "kissmetrics:processing:last_processed_file"
+
   def self.perform
+    require 'progressbar'
     require 'net/http'
     require 'rexml/document'
 
-    `rm -rf km_dump`
     `mkdir km_dump`
 
     conn = {
@@ -28,29 +30,63 @@ class KissmetricsDumpDownload
       raise "Did not download index manifest. Aborting..."
     end
 
+    last_processed_file = Resque.redis.get(REDIS_LAST_PROCESSED_FILE)
+    if last_processed_file.nil?
+      last_processed_file = "1.json"
+      Resque.redis.set(REDIS_LAST_PROCESSED_FILE, last_processed_file)
+    end
+
     index_file = File.read('km_dump/index.csv').split("\n")
     index_file.shift(1)
+    index_file = index_file.sort { |l|
+      l.split(",").first.split("/").last.split(".").first.to_i
+    }.reverse
+    while index_file.first.split(",").first.split("/").last != last_processed_file
+      # puts "Already processed #{index_file.first.split(",").first.split("/").last}. Skipping"
+      index_file.shift(1)
+    end
+    progress = ProgressBar.new("Download", index_file.count)
 
     index_file.each do |index_file_line|
       original_filename = index_file_line.split(",").first
       filename = original_filename.gsub("/", "_")
-      puts "Processing #{original_filename} (#{filename})..."
+      # puts "Processing #{original_filename} (#{filename})..."
       obj = bucket[original_filename]
       unless obj.present?
-        obj = AWS::S3::S3Object.find(original_filename, 'cpkissmetrics')
-        unless obj.present?
-          puts "Object is nill. Skipping."
+        begin
+          obj = AWS::S3::S3Object.find(original_filename, 'cpkissmetrics')
+          unless obj.present?
+            # puts "Object is nill. Skipping."
+            next
+          end
+        rescue
+          puts "Cycling #{index_file}"
+          index_file << index_file_line
           next
         end
       end
+      if File.exists? "km_dump/#{filename}"
+        `rm -rf km_dump/#{filename}`
+      end
       File.open("km_dump/#{filename}", 'w') { |f| f.write(obj.value) }
+      progress.inc
     end
 
-    last_json_file = index_file.last.split(",").first.split("/").last
-    unless File.exists? "km_dump/#{last_json_file}"
-      raise "Kissmetrics dump download was corrupted, since #{last_json_file} was not downloaded. Aborting."
-    end
+    progress.finish
 
-    puts "Done"
+    file_nums = index_file.map { |l| l.split(",").first.split("/").last.split(".").first.to_i }
+    progress = ProgressBar.new("Verification", file_nums.count)
+    max_file_num = file_nums.max
+    min_file_num = file_nums.min
+    (min_file_num..max_file_num).each do |file_num|
+      unless File.exists? "km_dump/revisions_#{file_num.to_s}.json"
+        puts "Kissmetrics dump download was corrupted, since #{file_num} was not downloaded. Aborting."
+        exit 1
+      end
+      progress.inc
+    end
+    progress.finish
+
+    Resque.redis.set(REDIS_LAST_PROCESSED_FILE, "#{max_file_num}.json")
   end
 end
