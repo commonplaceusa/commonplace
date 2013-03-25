9e184fd3af32a083dbeac5227f4e697b99db79d6
Jason Berlinsky <jberlinsky@gmail.com>
2013-03-25 02:12:40 -0400

    Wrap Contacts initialization script in begin/rescue

--- config/initializers/contacts.rb
+++ config/initializers/contacts.rb
@@ -1,11 +1,15 @@
-Contacts::configure({
-  :yahoo => {
-    :consumer_key => ENV['yahoo_consumer_key'] || "***REMOVED***",
-    :consumer_secret => ENV['yahoo_consumer_secret'] || "***REMOVED***"
-  },
-  :windows_live => {
-    :application_id => ENV['windows_live_application_id'] || "***REMOVED***",
-    :secret_key => ENV['windows_live_secret_key'] || "***REMOVED***",
-    :privacy_policy_url => "http://www.ourcommonplace.com/"
-  }
-})
+begin
+  Contacts::configure({
+    :yahoo => {
+      :consumer_key => ENV['yahoo_consumer_key'] || "***REMOVED***",
+      :consumer_secret => ENV['yahoo_consumer_secret'] || "***REMOVED***"
+    },
+    :windows_live => {
+      :application_id => ENV['windows_live_application_id'] || "***REMOVED***",
+      :secret_key => ENV['windows_live_secret_key'] || "***REMOVED***",
+      :privacy_policy_url => "http://www.ourcommonplace.com/"
+    }
+  })
+rescue
+  puts "Could not initialize Contacts gem"
+end
