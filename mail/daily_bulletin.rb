83b652b993d0c549710e2f2e9d85a5ea7f9a0214
Jason Berlinsky <jberlinsky@gmail.com>
2013-04-14 13:06:56 -0400

    Say hello instead of good morning

--- mail/daily_bulletin.rb
+++ mail/daily_bulletin.rb
@@ -59,7 +59,7 @@ class DailyBulletin < MailBase
   end
 
   def message_text
-    "Good morning #{community_name}! It's currently #{current_temp} degrees outside with a high today of #{high_today} degrees#{rain?}. In the past week, #{new_user_count} of your neighbors have joined OurCommonPlace #{community_name}, making the network #{community_user_count} people strong. In the past day, the community has shared #{post_count} neighborhood posts and #{announcement_count} organization announcements. Enjoy!"
+    "Hello #{community_name}! It's currently #{current_temp} degrees outside with a high today of #{high_today} degrees#{rain?}. In the past week, #{new_user_count} of your neighbors have joined OurCommonPlace #{community_name}, making the network #{community_user_count} people strong. In the past day, the community has shared #{post_count} neighborhood posts and #{announcement_count} organization announcements. Enjoy!"
   end
 
   def from
